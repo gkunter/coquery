2840efc70aca6d2cd5cf622ceda2bfd4fcecd17f
gkunter <coquery@users.strandwall.de>
2015-07-06 00:42:32 +0200

    Add a button that starts the MySQL server setup guide.

--- coquery/gui/MySQLOptions.py
+++ coquery/gui/MySQLOptions.py
@@ -2,8 +2,9 @@ from __future__ import division
 from __future__ import unicode_literals
 
 from pyqt_compat import QtCore, QtGui
-#import options
+import argparse
 import MySQLOptionsUi
+import mysql_guide
 import sys
 sys.path.append("/home/kunibert/Dev/coquery/coquery")
 import sqlwrap
@@ -29,6 +30,8 @@ class MySQLOptions(QtGui.QDialog):
         self.ui.port.valueChanged.connect(self.check_connection)
         self.ui.radio_local.clicked.connect(self.check_connection)
         self.ui.radio_remote.clicked.connect(self.check_connection)
+        self.ui.buttonBox.button(QtGui.QDialogButtonBox.Help).clicked.connect(self.start_mysql_guide)
+        self.ui.buttonBox.button(QtGui.QDialogButtonBox.Help).setText("MySQL server guide...")
 
     def check_connection(self):
         #self.ui.update()
@@ -50,21 +53,36 @@ class MySQLOptions(QtGui.QDialog):
         except SQLInitializationError:
             self.ui.label_connection.setText("Not connected")
             self.ui.button_status.setStyleSheet('QPushButton {background-color: red; color: red;}')
+            self.ui.buttonBox.button(QtGui.QDialogButtonBox.Ok).setEnabled(False)
+            self.ui.buttonBox.button(QtGui.QDialogButtonBox.Help).setEnabled(True)
         else:
             self.ui.button_status.setStyleSheet('QPushButton {background-color: green; color: green;}')
             self.ui.label_connection.setText("Connected ({})".format(x[0]))
+            self.ui.buttonBox.button(QtGui.QDialogButtonBox.Ok).setEnabled(True)
+            self.ui.buttonBox.button(QtGui.QDialogButtonBox.Help).setEnabled(False)
+        
+    def start_mysql_guide(self):
+        mysql_guide.MySqlGuide(self)
+        
         
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_Escape:
             self.accept()
             
     @staticmethod
-    def set(path, parent=None):
-        dialog = MySQLOptions(parent)
+    def set(parent=None):
+        dialog = MySQLOptions(parent=parent)
         result = dialog.exec_()
-        return None
-    
-            
+        if result:
+            namespace = argparse.Namespace()
+            namespace.db_user = dialog.ui.user.text()
+            namespace.db_password = dialog.ui.password.text()
+            namespace.db_host = dialog.ui.hostname.text()
+            namespace.db_port = dialog.ui.port.value()
+            return namespace
+        else:
+            return None
+
 def main():
     app = QtGui.QApplication(sys.argv)
     viewer = MySQLOptions()
