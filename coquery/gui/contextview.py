86444ba75e06852244536585019b06c344285080
gkunter <coquery@users.strandwall.de>
2015-07-20 00:37:01 +0200

    Add context viewer

---
+++ coquery/gui/contextview.py
@@ -0,0 +1,123 @@
+from __future__ import division
+from __future__ import unicode_literals
+
+from pyqt_compat import QtCore, QtGui
+
+import sys
+import contextviewUi
+import options
+
+class ContextView(QtGui.QDialog):
+    def __init__(self, corpus, token_id, source_id, token_width, parent=None):
+        
+        super(ContextView, self).__init__(parent)
+        
+        self.corpus = corpus
+        self.token_id = token_id
+        self.source_id = source_id
+        self.token_width = token_width
+        
+        self.ui = contextviewUi.Ui_ContextView()
+        self.ui.setupUi(self)
+
+        self.ui.spin_context_width.valueChanged.connect(self.spin_changed)
+        self.ui.slider_context_width.valueChanged.connect(self.slider_changed)
+        self.ui.slider_context_width.setTracking(True)
+        
+        self.add_source_label("Token ID", token_id)
+        self.add_source_label("Source ID", source_id)
+        if corpus:
+            rc_table = "{}_table".format(options.cfg.context_source_id.split("_")[0])
+            
+            print(corpus)
+            
+        self.update_context()
+        
+    def add_source_label(self, name, content):
+        """ Add the label 'name' with value 'content' to the context viewer.
+        """
+        layout_row = self.ui.form_information.count()
+        self.ui.source_name = QtGui.QLabel(self.ui.box_context)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.ui.source_name.sizePolicy().hasHeightForWidth())
+        self.ui.source_name.setSizePolicy(sizePolicy)
+        self.ui.source_name.setAlignment(QtCore.Qt.AlignRight|QtCore.Qt.AlignTop|QtCore.Qt.AlignTrailing)
+        self.ui.source_name.setTextInteractionFlags(QtCore.Qt.LinksAccessibleByMouse|QtCore.Qt.TextSelectableByKeyboard|QtCore.Qt.TextSelectableByMouse)
+        self.ui.form_information.setWidget(layout_row, QtGui.QFormLayout.LabelRole, self.ui.source_name)
+        self.ui.source_content = QtGui.QLabel(self.ui.box_context)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.ui.source_content.sizePolicy().hasHeightForWidth())
+        self.ui.source_content.setSizePolicy(sizePolicy)
+        self.ui.source_content.setAlignment(QtCore.Qt.AlignLeading|QtCore.Qt.AlignLeft|QtCore.Qt.AlignTop)
+        self.ui.source_content.setTextInteractionFlags(QtCore.Qt.LinksAccessibleByMouse|QtCore.Qt.TextSelectableByKeyboard|QtCore.Qt.TextSelectableByMouse)
+        self.ui.form_information.setWidget(layout_row, QtGui.QFormLayout.FieldRole, self.ui.source_content)
+        
+        if name:
+            name = str(name).strip()
+            if not name.endswith(":"):
+                name += ":"
+            self.ui.source_name.setText(name)
+        if content:
+            content = str(content).strip()
+            self.ui.source_content.setText(content)
+        
+    def spin_changed(self):
+        self.ui.slider_context_width.valueChanged.disconnect(self.slider_changed)
+        self.ui.slider_context_width.setValue(self.ui.spin_context_width.value())
+        self.update_context()
+        self.ui.slider_context_width.valueChanged.connect(self.slider_changed)
+    
+    def slider_changed(self):
+        self.ui.spin_context_width.valueChanged.disconnect(self.spin_changed)
+        self.ui.spin_context_width.setValue(self.ui.slider_context_width.value())
+        self.update_context()
+        self.ui.spin_context_width.valueChanged.connect(self.spin_changed)
+    
+    def update_context(self):
+        if self.corpus:
+            self.corpus.render_context(
+                self.token_id, 
+                self.source_id, 
+                self.token_width,
+                self.ui.slider_context_width.value(), self)
+        
+    def keyPressEvent(self, e):
+        if e.key() == QtCore.Qt.Key_Escape:
+            self.accept()
+            
+    def done(self, *args):
+        options.cfg.context_view_height = self.height()
+        options.cfg.context_view_width = self.width()
+        options.cfg.context_view_words = self.ui.slider_context_width.value()
+        super(ContextView, self).done(*args)
+
+    @staticmethod
+    def display(resource, token_id, source_id, token_width, parent=None):
+        dialog = ContextView(resource, token_id, source_id, token_width, parent=parent)        
+        try:
+            dialog.ui.slider_context_width.setValue(options.cfg.context_view_words)
+        except AttributeError:
+            pass
+        try:
+            dialog.resize(dialog.width(), options.cfg.context_view_height)
+        except AttributeError:
+            pass
+        try:
+            dialog.resize(options.cfg.context_view_width, dialog.height())
+        except AttributeError:
+            pass
+
+        result = dialog.show()
+
+def main():
+    app = QtGui.QApplication(sys.argv)
+    viewer = ContextView(None, None, None, None, 0)
+    viewer.exec_()
+    
+if __name__ == "__main__":
+    main()
+    
---
+++ coquery/gui/contextviewUi.py
@@ -0,0 +1,116 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'contextview.ui'
+#
+# Created: Sun Jul 19 13:02:45 2015
+#      by: PyQt4 UI code generator 4.10.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from pyqt_compat import QtCore, QtGui
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_ContextView(object):
+    def setupUi(self, ContextView):
+        ContextView.setObjectName(_fromUtf8("ContextView"))
+        ContextView.resize(578, 412)
+        ContextView.setSizeGripEnabled(False)
+        self.verticalLayout_2 = QtGui.QVBoxLayout(ContextView)
+        self.verticalLayout_2.setContentsMargins(4, 4, 4, -1)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.box_context = QtGui.QFrame(ContextView)
+        self.box_context.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.box_context.setFrameShadow(QtGui.QFrame.Raised)
+        self.box_context.setObjectName(_fromUtf8("box_context"))
+        self.verticalLayout = QtGui.QVBoxLayout(self.box_context)
+        self.verticalLayout.setMargin(10)
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.form_information = QtGui.QFormLayout()
+        self.form_information.setFieldGrowthPolicy(QtGui.QFormLayout.ExpandingFieldsGrow)
+        self.form_information.setHorizontalSpacing(10)
+        self.form_information.setObjectName(_fromUtf8("form_information"))
+        self.verticalLayout.addLayout(self.form_information)
+        self.line = QtGui.QFrame(self.box_context)
+        self.line.setFrameShape(QtGui.QFrame.HLine)
+        self.line.setFrameShadow(QtGui.QFrame.Sunken)
+        self.line.setObjectName(_fromUtf8("line"))
+        self.verticalLayout.addWidget(self.line)
+        self.context_area = QtGui.QTextBrowser(self.box_context)
+        self.context_area.setAutoFillBackground(False)
+        self.context_area.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.context_area.setFrameShadow(QtGui.QFrame.Sunken)
+        self.context_area.setTextInteractionFlags(QtCore.Qt.LinksAccessibleByKeyboard|QtCore.Qt.LinksAccessibleByMouse|QtCore.Qt.TextBrowserInteraction|QtCore.Qt.TextSelectableByKeyboard|QtCore.Qt.TextSelectableByMouse)
+        self.context_area.setObjectName(_fromUtf8("context_area"))
+        self.verticalLayout.addWidget(self.context_area)
+        self.verticalLayout_2.addWidget(self.box_context)
+        self.box_context_width = QtGui.QFrame(ContextView)
+        self.box_context_width.setFrameShape(QtGui.QFrame.StyledPanel)
+        self.box_context_width.setFrameShadow(QtGui.QFrame.Raised)
+        self.box_context_width.setObjectName(_fromUtf8("box_context_width"))
+        self.horizontalLayout = QtGui.QHBoxLayout(self.box_context_width)
+        self.horizontalLayout.setSpacing(10)
+        self.horizontalLayout.setMargin(10)
+        self.horizontalLayout.setObjectName(_fromUtf8("horizontalLayout"))
+        self.label_3 = QtGui.QLabel(self.box_context_width)
+        self.label_3.setObjectName(_fromUtf8("label_3"))
+        self.horizontalLayout.addWidget(self.label_3)
+        self.spin_context_width = QtGui.QSpinBox(self.box_context_width)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.spin_context_width.sizePolicy().hasHeightForWidth())
+        self.spin_context_width.setSizePolicy(sizePolicy)
+        self.spin_context_width.setFrame(True)
+        self.spin_context_width.setButtonSymbols(QtGui.QAbstractSpinBox.UpDownArrows)
+        self.spin_context_width.setMaximum(500)
+        self.spin_context_width.setProperty("value", 5)
+        self.spin_context_width.setObjectName(_fromUtf8("spin_context_width"))
+        self.horizontalLayout.addWidget(self.spin_context_width)
+        self.slider_context_width = QtGui.QSlider(self.box_context_width)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.slider_context_width.sizePolicy().hasHeightForWidth())
+        self.slider_context_width.setSizePolicy(sizePolicy)
+        self.slider_context_width.setMinimum(0)
+        self.slider_context_width.setMaximum(500)
+        self.slider_context_width.setProperty("value", 10)
+        self.slider_context_width.setSliderPosition(10)
+        self.slider_context_width.setOrientation(QtCore.Qt.Horizontal)
+        self.slider_context_width.setInvertedAppearance(False)
+        self.slider_context_width.setInvertedControls(False)
+        self.slider_context_width.setTickPosition(QtGui.QSlider.TicksAbove)
+        self.slider_context_width.setTickInterval(10)
+        self.slider_context_width.setObjectName(_fromUtf8("slider_context_width"))
+        self.horizontalLayout.addWidget(self.slider_context_width)
+        self.verticalLayout_2.addWidget(self.box_context_width)
+        self.buttonBox = QtGui.QDialogButtonBox(ContextView)
+        self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
+        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Ok)
+        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.verticalLayout_2.addWidget(self.buttonBox)
+
+        self.retranslateUi(ContextView)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), ContextView.accept)
+        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), ContextView.reject)
+        QtCore.QMetaObject.connectSlotsByName(ContextView)
+
+    def retranslateUi(self, ContextView):
+        ContextView.setWindowTitle(_translate("ContextView", "Dialog", None))
+        self.label_3.setText(_translate("ContextView", "Context size:", None))
+        self.spin_context_width.setSpecialValueText(_translate("ContextView", "none", None))
+        self.spin_context_width.setSuffix(_translate("ContextView", " words", None))
+
