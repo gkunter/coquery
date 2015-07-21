38e28ca38fc3d62c40b4b7b2816652e1f35e33df
gkunter <gero.kunter@uni-duesseldorf.de>
2015-07-21 18:54:01 +0200

    Add white background

--- coquery/gui/visualizerUi.py
+++ coquery/gui/visualizerUi.py
@@ -2,8 +2,7 @@
 
 # Form implementation generated from reading ui file 'visualizer.ui'
 #
-# Created: Mon Jul 20 23:31:54 2015
-#      by: PyQt4 UI code generator 4.10.4
+# Created by: PyQt4 UI code generator 4.10.3
 #
 # WARNING! All changes made in this file will be lost!
 
@@ -36,6 +35,17 @@ class Ui_visualizer(object):
         self.verticalLayout = QtGui.QVBoxLayout(self.box_visualize)
         self.verticalLayout.setMargin(10)
         self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.graph_area = QtGui.QFrame(self.box_visualize)
+        self.graph_area.setAutoFillBackground(False)
+        self.graph_area.setStyleSheet(_fromUtf8("background: rgb(255, 255, 255)"))
+        self.graph_area.setFrameShape(QtGui.QFrame.NoFrame)
+        self.graph_area.setFrameShadow(QtGui.QFrame.Sunken)
+        self.graph_area.setLineWidth(0)
+        self.graph_area.setObjectName(_fromUtf8("graph_area"))
+        self.visualization_layout = QtGui.QVBoxLayout(self.graph_area)
+        self.visualization_layout.setMargin(0)
+        self.visualization_layout.setObjectName(_fromUtf8("visualization_layout"))
+        self.verticalLayout.addWidget(self.graph_area)
         self.verticalLayout_2.addWidget(self.box_visualize)
         self.buttonBox = QtGui.QDialogButtonBox(visualizer)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
