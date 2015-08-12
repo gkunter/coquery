4a77744efa5433ec33161c7f2245886ee5358c95
gkunter <coquery@users.strandwall.de>
2015-08-12 19:54:17 +0200

    Move to seaborn/pandas.

--- coquery/visualizations/barcodeplot.py
+++ coquery/visualizations/barcodeplot.py
@@ -7,68 +7,49 @@ import unittest
 import options
 from defines import *
 
-from visualizer import Visualizer
+import visualizer as vis
+import seaborn as sns
+import pandas as pd
+import matplotlib as mpl
 
-class BarcodeVisualizer(Visualizer):
+class BarcodeVisualizer(vis.Visualizer):
     visualize_frequency = False
 
     def setup_figure(self):
-        #super(BarcodeVisualizer, self).setup_figure()
-        #self.subplot = self.figure.add_subplot(111)
-        #self.subplot.set_ylim(0, 1)
-        #self.subplot.set_xlim(0, options.cfg.main_window.Session.Corpus.get_corpus_size())
+        super(BarcodeVisualizer, self).setup_figure()
 
-        self.figure = self.get_default_figure()
-        
-        self.subplot_dict = {}
-        
-        self.table_structure = self.get_table_levels()
+        # get the last factor in the current table. 
+        try:
+            col_factor = [x for x in self._table.columns if not x.startswith("coquery_invisible")][-1]
+        except IndexError:
+            col_factor = None
+        # get the penultimate factor in the current table. 
+        try:
+            row_factor = [x for x in self._table.columns if not x.startswith("coquery_invisible")][-2]
+            col_wrap=None
+        except IndexError:
+            # only a single factor in the table, use column wrapping:
+            row_factor=None
+            _, col_wrap = self.get_grid_layout(len(pd.unique(self._table[col_factor])))
 
-        # prod contains a list of lists, where each inner list represents one
-        # possible combination of factor levels from the current table
-        # structure.
-        prod = [self.get_ordered_row(x) for x in dict_product(self.table_structure)]
+        mpl.rc("font",
+               {"family": "normal", "weight": "bold", "size": 22})
 
-        # Try to choose a suitable grid layout for the figure. If there are
-        # more than just a single factor, there is one column for each factor 
-        # level of the last column in the data table. If there is only one 
-        # factor, the levels are layed out so that there is an appropriate
-        # number of rows and columns.
-        
-        if len(self.table_structure) > 1:
-            # Get the number of the factor levels in the last column of the
-            # data table:
-            ncols = len(self.table_structure[self.table_structure.keys()[-1]])
-            nrows = len(prod) // ncols
-        else:
-            nrows, ncols = self.get_grid_layout(len(prod))
-
-        # Create a subplot for each factor level combination in the table
-        # structure. The subplots are accessible by tuples of factor levels.
-        for i, row in enumerate(prod):
-            key = tuple(row)
-            self.subplot_dict[key] = self.figure.add_subplot(
-                nrows, ncols, i + 1)
-            self.subplot_dict[key].set_ylim(0, 1)
-            self.subplot_dict[key].set_xlim(0, options.cfg.main_window.Session.Corpus.get_corpus_size())
-            self.subplot_dict[key].set_xlabel(", ".join(key))
+        self.g = sns.FacetGrid(self._table, 
+                               xlim=(0, options.cfg.main_window.Session.Corpus.get_corpus_size()),
+                               ylim=(0, 1),
+                               col_wrap=col_wrap,
+                               #row=row_factor,
+                               col=col_factor, sharex=True, sharey=True)
+        self.figure = self.g.fig
 
     def draw(self):
         """ Plot a vertical line for each token in the current data table.
-        The line is drawn in the subplot matching the factor level 
+        The line is drawn in a subplot matching the factor level 
         combination in that row. The horizontal position corresponds to the
         token id so that tokens that occur in the same part of the corpus
         will also have lines that are placed close to each other. """
         
-        #bins = collections.Counter()
-        #for x in self._content:
-            #bins[x["coquery_invisible_corpus_id"] // 10000] += 1
-        #largest_bin = max(bins.values())
-        #for x in bins:
-            #self.subplot.bar([x * 10000 for x in bins.keys()], bins.values(), width=9500)
-            
-        for row in self._model.content:
-            l = tuple(self.get_ordered_row(row))
-            self.subplot_dict[l].vlines(row["coquery_invisible_corpus_id"], 0, 1, colors=[(0, 0, 0, 0.25)])
-            
-        self.figure.tight_layout()
+        sns.despine(self.g.fig, left=True, right=True)
+        with sns.axes_style("white"):
+            self.g.map(sns.rugplot, "coquery_invisible_corpus_id", height=1, color=sns.color_palette("Paired", 1)[0])
