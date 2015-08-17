7f0ab2f01ab68bd91f7b151fa55ead2431aac66e
gkunter <coquery@users.strandwall.de>
2015-08-18 00:56:40 +0200

    Move to seaborn facetting

--- coquery/visualizations/heatmap.py
+++ coquery/visualizations/heatmap.py
@@ -15,28 +15,27 @@ import matplotlib.pyplot as plt
 
 class HeatmapVisualizer(vis.Visualizer):
     visualize_frequency = True
-
+    dimensionality=2
+    
     def setup_figure(self):
-        with sns.plotting_context(self.get_plot_context(), font_scale=self.get_font_scale()):
+        with sns.axes_style("white"):
             super(HeatmapVisualizer, self).setup_figure()
-        print(self.figure)
-        print(dir(self.figure))
 
-    def update_data(self):
-        super(HeatmapVisualizer, self).update_data()
-        # get the last factor in the current table. 
-        columns = [x for x in self._table.columns if not x.startswith("coquery_invisible")]
-        if len(columns) == 1:
-            self.col_factor = columns[0]
-            self.row_factor = None
-            self.col_wrap = True
-        elif len(columns) >= 2:
-            self.col_factor = columns[-2]
-            self.row_factor = columns[-1]
-            self.col_wrap = False
-        else:
-            self.col_factor = None
-            self.row_factor = None
+    #def update_data(self):
+        #super(HeatmapVisualizer, self).update_data()
+        ## get the last factor in the current table. 
+        #columns = [x for x in self._table.columns if not x.startswith("coquery_invisible")]
+        #if len(columns) == 1:
+            #self.col_factor = columns[0]
+            #self.row_factor = None
+            #self.col_wrap = True
+        #elif len(columns) >= 2:
+            #self.col_factor = columns[-2]
+            #self.row_factor = columns[-1]
+            #self.col_wrap = False
+        #else:
+            #self.col_factor = None
+            #self.row_factor = None
 
     def draw(self):
         """ Plot a vertical line for each token in the current data table.
@@ -45,11 +44,36 @@ class HeatmapVisualizer(vis.Visualizer):
         token id so that tokens that occur in the same part of the corpus
         will also have lines that are placed close to each other. """
         
-        #self.setup_axis("Y")
-        sns.heatmap(pd.crosstab(
-            self._table[self.row_factor], 
-            self._table[self.col_factor]), ax=self.subplot)
+        def get_crosstab(data, row_fact,col_fact, row_names, col_names):
+            ct = pd.crosstab(data[row_fact], data[col_fact])
+            ct = ct.reindex_axis(row_names, axis=0).fillna(0)
+            ct = ct.reindex_axis(col_names, axis=1).fillna(0)
+            return ct
+        
+        if len(self._groupby) < 2:
+            tmp = pd.Series([""] * len(self._table[self._groupby[0]]), name="")
+            tab = pd.crosstab(tmp, self._table[self._groupby[0]])
+        else:
+            FUN = lambda data, color: sns.heatmap(
+                get_crosstab(
+                    data, 
+                    self._groupby[0], 
+                    self._groupby[1], 
+                    self._levels[0], 
+                    self._levels[1]),
+                robust=True,
+                annot=True,
+                cbar=False,
+                vmax=vmax,
+                linewidths=1)
+
+        vmax = pd.crosstab(
+            [self._table[x] for x in [self._row_factor, self._groupby[0]] if x <> None],
+            [self._table[x] for x in [self._col_factor, self._groupby[1]] if x <> None]).values.max()
+
+        self.g.map_dataframe(FUN)
+
         self.setup_axis("Y")
         self.setup_axis("X")
-        #self.figure.autofmt_xdate()
-        self.figure.tight_layout()
+        
+        self.g.fig.tight_layout()
