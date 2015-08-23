69e13f93f91af2a9a9a8593de442c92df16f9df4
gkunter <coquery@users.strandwall.de>
2015-08-23 17:11:21 +0200

    Clean up code

--- coquery/sqlwrap.py
+++ coquery/sqlwrap.py
@@ -1,30 +1,8 @@
 # -*- coding: utf-8 -*-
-"""
-FILENAME: sqlwrap.py -- part of Coquery corpus query tool
+""" FILENAME: sqlwrap.py -- part of Coquery corpus query tool
 
-This module defines a wrapper to access a MySQL database.
+This module defines a wrapper to access a MySQL database. """
 
-LICENSE:
-Copyright (c) 2015 Gero Kunter
-
-Permission is hereby granted, free of charge, to any person obtaining a copy
-of this software and associated documentation files (the "Software"), to deal
-in the Software without restriction, including without limitation the rights
-to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-copies of the Software, and to permit persons to whom the Software is
-furnished to do so, subject to the following conditions:
-
-The above copyright notice and this permission notice shall be included in
-all copies or substantial portions of the Software.
-
-THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-THE SOFTWARE.
-"""
 from __future__ import unicode_literals
 import __init__
 
@@ -48,23 +26,11 @@ try:
 except ImportError:
     raise DependencyError(["MySQLdb", "pymysql"])
 
-ExecutedLastCommand = False
-
-ModifyingCommands = ["ALTER", "CREATE", "DELETE", "DROP", "INSERT", 
-                     "LOAD", "UPDATE"]
-NonModifyingCommands = ["SELECT", "SHOW", "DESCRIBE", "SET", "RESET"]
-
 class SqlDB (object):
     """ A wrapper for MySQL. """
     def __init__(self, Host, Port, User, Password, Database=None, encoding="utf8"):
         self.Con = None
         self.Cur = None
-        self.Host = Host
-        self.Port = Port
-        self.User = User
-        self.Password = Password
-        self.Database = Database
-        self.LastQuery = None
         
         try:
             if Database:
