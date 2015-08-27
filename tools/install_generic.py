f42435ccc4e3b539afa95d244d225d2ed6cdf57a
gkunter <gero.kunter@uni-duesseldorf.de>
2015-08-27 10:21:42 +0200

    Add dummy URL

--- tools/install_generic.py
+++ tools/install_generic.py
@@ -10,6 +10,8 @@ class GenericCorpusBuilder(BaseCorpusBuilder):
         # specify which features are provided by this corpus and lexicon:
         self.lexicon_features = ["LEX_WORDID", "LEX_LEMMA", "LEX_ORTH", "LEX_POS"]
         self.corpus_features = ["CORP_CONTEXT", "CORP_FILENAME", "CORP_STATISTICS"]
+        
+        self.documentation_url = "(no URL availabe)"
 
         # add table descriptions for the tables used in this database.
         #
