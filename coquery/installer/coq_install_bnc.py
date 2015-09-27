b9a49cbc418c8a683534a2550532e05042795843
gkunter <coquery@users.strandwall.de>
2015-09-27 19:26:20 +0200

    Add GPL note

--- coquery/installer/coq_install_bnc.py
+++ coquery/installer/coq_install_bnc.py
@@ -1,5 +1,15 @@
 # -*- coding: utf-8 -*-
 
+"""
+coq_install_bnc.py is part of Coquery.
+
+Copyright (c) 2015 Gero Kunter (gero.kunter@coquery.org)
+
+Coquery is released under the terms of the GNU General Public License.
+For details, see the file LICENSE that you should have received along 
+with Coquery. If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from __future__ import unicode_literals 
 
 from corpusbuilder import *
