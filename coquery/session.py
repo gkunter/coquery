8c7be89b55c3b32564955143582cd0e61d44f82f
gkunter <gero.kunter@uni-duesseldorf.de>
2015-10-02 10:37:15 +0200

    Improve handling of sub-queries. Disable obsolete code.

--- coquery/session.py
+++ coquery/session.py
@@ -29,12 +29,10 @@ from defines import *
 import queries
 import tokens
 
-
 class Session(object):
     def __init__(self):
         self.header = None
         self.max_number_of_input_columns = 0
-        self.max_number_of_tokens = 0
         self.query_list = []
         self.requested_fields = []
             
@@ -65,89 +63,99 @@ class Session(object):
         self.output_order = []
         self.header_shown = False
         self.input_columns = []
-
-    def expand_header(self):
+        
+    def get_max_token_count(self):
         """
-        Session.expand_header() ensures that the list Session.header 
-        contains all column labels that are required for the current 
-        session. The set of labels depends on the command line flags. For 
-        example, -p adds one or more numbered part-of-speech labels to the 
-        header. The number of labels depends on the maximum number of query 
-        tokens in this session. """
+        Return the maximal number of tokens that may be produced by the 
+        queries from this session.
+        """
+        maximum = 0
+        for query in self.query_list:
+            maximum = max(maximum, query.get_max_tokens())
+        return maximum
 
-        return []
+    #def expand_header(self):
+        #"""
+        #Session.expand_header() ensures that the list Session.header 
+        #contains all column labels that are required for the current 
+        #session. The set of labels depends on the command line flags. For 
+        #example, -p adds one or more numbered part-of-speech labels to the 
+        #header. The number of labels depends on the maximum number of query 
+        #tokens in this session. """
+        #print("expand_header is not used anymore.")
+        #return []
 
-        if options.cfg.MODE == QUERY_MODE_COLLOCATIONS:
-            self.header = ["coquery_query_string"]
-            lexicon_features = self.Corpus.resource.get_lexicon_features()
-            for rc_feature in options.cfg.selected_features:
-                if rc_feature in [x for x, _ in lexicon_features]:
-                    self.header.append("coq_collocate_{}".format(rc_feature))
-            self.header.append("coq_frequency")
-            self.header.append("coq_collocate_frequency")
-            self.header.append("coq_collocate_frequency_left")
-            self.header.append("coq_collocate_frequency_right")
-            self.header.append("coq_mutual_information")
-            self.header.append("coq_conditional_probability")
-            self.header.append("coquery_invisible_corpus_id")
-            self.header.append("coquery_invisible_origin_id")
-            self.header.append("coquery_invisible_number_of_tokens")
-            return
+        #if options.cfg.MODE == QUERY_MODE_COLLOCATIONS:
+            #self.header = ["coquery_query_string"]
+            #lexicon_features = self.Corpus.resource.get_lexicon_features()
+            #for rc_feature in options.cfg.selected_features:
+                #if rc_feature in [x for x, _ in lexicon_features]:
+                    #self.header.append("coq_collocate_{}".format(rc_feature))
+            #self.header.append("coq_frequency")
+            #self.header.append("coq_collocate_frequency")
+            #self.header.append("coq_collocate_frequency_left")
+            #self.header.append("coq_collocate_frequency_right")
+            #self.header.append("coq_mutual_information")
+            #self.header.append("coq_conditional_probability")
+            #self.header.append("coquery_invisible_corpus_id")
+            #self.header.append("coquery_invisible_origin_id")
+            #self.header.append("coquery_invisible_number_of_tokens")
+            #return
             
-        corpus_features = [x for x, _ in self.Corpus.resource.get_corpus_features() if x in options.cfg.selected_features]
-        lexicon_features = [x for x, _ in self.Corpus.resource.get_lexicon_features() if x in options.cfg.selected_features]
-        corpus_names = [x for _, x in self.Corpus.resource.get_corpus_features() if _ in options.cfg.selected_features]
-        lexicon_names = [x for _, x in self.Corpus.resource.get_lexicon_features() if _ in options.cfg.selected_features]
-        h = []
-        for rc_feature in self.Corpus.resource.get_preferred_output_order():
-            if rc_feature in options.cfg.selected_features and rc_feature in lexicon_features:
-                h += ["{}{}".format(lexicon_names[lexicon_features.index(rc_feature)], i+1) for i in range(self.max_number_of_tokens)]
-        for rc_feature in self.Corpus.resource.get_preferred_output_order():
-            if rc_feature in options.cfg.selected_features and rc_feature in corpus_features:
-                h += [corpus_names[corpus_features.index(rc_feature)]]
-        for rc_feature in options.cfg.selected_features:
-            if rc_feature.startswith("coquery"):
-                name = self.Corpus.resource.__getattribute__(rc_feature)
-                h.append(name)
-        self.header = h
-        if options.cfg.MODE == QUERY_MODE_FREQUENCIES:
-            self.header.append (options.cfg.freq_label)
-        if options.cfg.context_left:
-            self.header.append("Left_context")
-        if options.cfg.context_right:
-            self.header.append("Right_context")
-        # if a GUI is used, include source features so the entries in the
-        # result table can be made clickable to show the context:
-        if options.cfg.MODE != QUERY_MODE_FREQUENCIES and (options.cfg.gui):
-            self.header.append("coq_invisible_token_id")
-            self.header.append("coq_invisible_source_id")
-            self.header.append("coq_invisible_number_of_tokens")
+        #corpus_features = [x for x, _ in self.Corpus.resource.get_corpus_features() if x in options.cfg.selected_features]
+        #lexicon_features = [x for x, _ in self.Corpus.resource.get_lexicon_features() if x in options.cfg.selected_features]
+        #corpus_names = [x for _, x in self.Corpus.resource.get_corpus_features() if _ in options.cfg.selected_features]
+        #lexicon_names = [x for _, x in self.Corpus.resource.get_lexicon_features() if _ in options.cfg.selected_features]
+        #h = []
+        #for rc_feature in self.Corpus.resource.get_preferred_output_order():
+            #if rc_feature in options.cfg.selected_features and rc_feature in lexicon_features:
+                #h += ["{}{}".format(lexicon_names[lexicon_features.index(rc_feature)], i+1) for i in range(self.max_number_of_tokens)]
+        #for rc_feature in self.Corpus.resource.get_preferred_output_order():
+            #if rc_feature in options.cfg.selected_features and rc_feature in corpus_features:
+                #h += [corpus_names[corpus_features.index(rc_feature)]]
+        #for rc_feature in options.cfg.selected_features:
+            #if rc_feature.startswith("coquery"):
+                #name = self.Corpus.resource.__getattribute__(rc_feature)
+                #h.append(name)
+        #self.header = h
+        #if options.cfg.MODE == QUERY_MODE_FREQUENCIES:
+            #self.header.append (options.cfg.freq_label)
+        #if options.cfg.context_left:
+            #self.header.append("Left_context")
+        #if options.cfg.context_right:
+            #self.header.append("Right_context")
+        ## if a GUI is used, include source features so the entries in the
+        ## result table can be made clickable to show the context:
+        #if options.cfg.MODE != QUERY_MODE_FREQUENCIES and (options.cfg.gui):
+            #self.header.append("coq_invisible_token_id")
+            #self.header.append("coq_invisible_source_id")
+            #self.header.append("coq_invisible_number_of_tokens")
 
-    def get_expected_column_number(self, max_number_of_tokens):
-        """ Return the expected number of columns, based on the maximum 
-        number of tokens in all query strings from the current session. The 
-        number is calculated by multiplying the maximum number of tokens by 
-        the number of lexicon features that were selected, and adding the 
-        number of selected corpus features."""
-        corpus_features = [x for x, _ in self.Corpus.resource.get_corpus_features() if x in options.cfg.selected_features]
-        lexicon_features = [x for x, _ in self.Corpus.resource.get_lexicon_features() if x in options.cfg.selected_features]
-        length = len(corpus_features) + max_number_of_tokens * len(lexicon_features)
-        if options.cfg.context_columns:
-            length += options.cfg.context_left
-            length += options.cfg.context_right
-            length += max_number_of_tokens
-        elif options.cfg.context_span:
-            length += 1
-        if options.cfg.MODE == QUERY_MODE_FREQUENCIES:
-            length += 1
-        for rc_feature in options.cfg.selected_features:
-            if rc_feature.startswith("coquery"):
-                length += 1
-        # if a GUI is used, include source features so the entries in the
-        # result table can be made clickable to show the context:
-        if options.cfg.MODE != QUERY_MODE_FREQUENCIES and (options.cfg.gui):
-            length += 3
-        return length
+    #def get_expected_column_number(self, max_number_of_tokens):
+        #""" Return the expected number of columns, based on the maximum 
+        #number of tokens in all query strings from the current session. The 
+        #number is calculated by multiplying the maximum number of tokens by 
+        #the number of lexicon features that were selected, and adding the 
+        #number of selected corpus features."""
+        #corpus_features = [x for x, _ in self.Corpus.resource.get_corpus_features() if x in options.cfg.selected_features]
+        #lexicon_features = [x for x, _ in self.Corpus.resource.get_lexicon_features() if x in options.cfg.selected_features]
+        #length = len(corpus_features) + max_number_of_tokens * len(lexicon_features)
+        #if options.cfg.context_columns:
+            #length += options.cfg.context_left
+            #length += options.cfg.context_right
+            #length += max_number_of_tokens
+        #elif options.cfg.context_span:
+            #length += 1
+        #if options.cfg.MODE == QUERY_MODE_FREQUENCIES:
+            #length += 1
+        #for rc_feature in options.cfg.selected_features:
+            #if rc_feature.startswith("coquery"):
+                #length += 1
+        ## if a GUI is used, include source features so the entries in the
+        ## result table can be made clickable to show the context:
+        #if options.cfg.MODE != QUERY_MODE_FREQUENCIES and (options.cfg.gui):
+            #length += 3
+        #return length
 
     def open_output_file(self):
         if options.cfg.gui:
@@ -167,7 +175,7 @@ class Session(object):
         """ Process all queries. For each query, go through the entries in 
         query_list() and yield the results for that subquery. Then, write
         all results to the output file. """
-        self.expand_header()
+        #self.expand_header()
         self._queries = {}
         self._results = {}
 
@@ -192,35 +200,18 @@ class Session(object):
 
         for current_query in self.query_list:
             self.literal_query_string = current_query.query_string
-            if len(current_query.query_list) > 1:
-                start_time = time.time()
-                any_result = False
-                for sub_query in current_query.query_list:
-                    query_results = []
-                    for current_result in self.Corpus.yield_query_results(sub_query):
-                        query_results.append(current_result)
-                    sub_query.Results = query_results
-                    if query_results:
-                        any_result = True
-                        sub_query.write_results(self.output_object)
-                if not any_result:
-                    current_query.write_results(self.output_object)
-                logger.info("Query executed (%.3f seconds)" % (time.time() - start_time))
-                
-            else:
-                start_time = time.time()
-                logger.info("Start query: '{}'".format(current_query))
-
-                if current_query.tokens:
-                    current_query.Results = self.Corpus.yield_query_results(current_query)
-                start_time = time.time()
+            start_time = time.time()
+            logger.info("Start query: '{}'".format(current_query.query_string))
+            for sub_query in current_query.query_list:
+                current_query._current_number_of_tokens = len(sub_query)
+                current_query.Results = self.Corpus.yield_query_results(current_query, sub_query)
                 current_query.write_results(self.output_object)
-                logger.info("Query executed (%.3f seconds)" % (time.time() - start_time))
+            logger.info("Query executed (%.3f seconds)" % (time.time() - start_time))
         self.end_time = datetime.datetime.now()
         if not options.cfg.gui:
             self.output_object.close()
         else:
-            self.output_object.index = range(1, 1 + len(self.output_object))
+            self.output_object.reset_index(drop=True, inplace=True)
 
 class StatisticsSession(Session):
     def __init__(self):
@@ -228,10 +219,6 @@ class StatisticsSession(Session):
         self.query_list.append(queries.StatisticsQuery(self.Corpus, self))
         self.header = ["Variable", "Value"]
         self.output_order = self.header
-    
-    def expand_header(self):
-        self.header = ["Variable", "Value"]
-        self.output_odrer = self.header
 
 class SessionCommandLine(Session):
     def __init__(self):
@@ -243,7 +230,6 @@ class SessionCommandLine(Session):
             else: 
                 raise CorpusUnavailableQueryTypeError(options.cfg.corpus, options.cfg.MODE)
             self.query_list.append(new_query)
-            self.max_number_of_tokens = max(new_query.max_number_of_tokens, self.max_number_of_tokens)
         self.max_number_of_input_columns = 0
 
 class SessionInputFile(Session):
@@ -276,7 +262,6 @@ class SessionInputFile(Session):
                     new_query.input_frame = pd.DataFrame(
                         [current_line], columns=input_header)
                     self.query_list.append(new_query)
-                    self.max_number_of_tokens = max(new_query.max_number_of_tokens, self.max_number_of_tokens)
                 self.max_number_of_input_columns = max(len(current_line), self.max_number_of_input_columns)
                 read_lines += 1
             
@@ -304,7 +289,6 @@ class SessionStdIn(Session):
                         new_query = self.query_type(query_string, self, tokens.COCAToken)
                         new_query.InputLine = copy.copy(current_line)
                         self.query_list.append(new_query)
-                        self.max_number_of_tokens = max(new_query.max_number_of_tokens, self.max_number_of_tokens)
                 self.max_number_of_input_columns = max(len(current_line), self.max_number_of_input_columns)
             read_lines += 1
         logger.info("Reading standard input ({} {})".format(len(self.query_list), "query" if len(self.query_list) == 1 else "queries"))            
