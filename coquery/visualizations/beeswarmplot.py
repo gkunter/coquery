983a398a4d671f4e5919ad4e9123efbbd3051897
gkunter <coquery@users.strandwall.de>
2015-11-21 13:17:19 +0100

    Clean up code

--- coquery/visualizations/beeswarmplot.py
+++ coquery/visualizations/beeswarmplot.py
@@ -16,64 +16,22 @@ import matplotlib.pyplot as plt
 from beeswarm import *
 
 class Visualizer(vis.BaseVisualizer):
-    visualize_frequency = True
     dimensionality = 1
 
     def setup_figure(self):
         with sns.axes_style("whitegrid"):
             super(Visualizer, self).setup_figure()
-        ## get the last factor in the current table. 
-        #try:
-            #self.col_factor = [x for x in self._table.columns if not x.startswith("coquery_invisible")][-1]
-        #except IndexError:
-            #self.col_factor = None
-        ## get the penultimate factor in the current table. 
-        #try:
-            #self.row_factor = [x for x in self._table.columns if not x.startswith("coquery_invisible")][-2]
-            #col_wrap=None
-        #except IndexError:
-            ## only a single factor in the table, use column wrapping:
-            #self.row_factor=None
-            #_, col_wrap = self.get_grid_layout(len(pd.unique(self._table[self.col_factor])))
-
-        ##mpl.rc("font",
-               ##{"family": "normal", "weight": "bold", "size": 22})
-
-        ##with sns.plotting_context(self.get_plot_context(), font_scale=self.get_font_scale()):
-            ##with sns.axes_style("white"):
-                ##self.g = sns.FacetGrid(self._table, 
-                                    ###xlim=(0, options.cfg.main_window.Session.Corpus.get_corpus_size()),
-                                    ###ylim=(0, 1),
-                                    ##col_wrap=col_wrap,
-                                    ###row=row_factor,
-                                    ##col=self.col_factor, sharex=True, sharey=True)
-        ##self.figure = self.g.fig
-
+ 
     def draw(self):
-        """ Plot a vertical line for each token in the current data table.
-        The line is drawn in a subplot matching the factor level 
-        combination in that row. The horizontal position corresponds to the
-        token id so that tokens that occur in the same part of the corpus
-        will also have lines that are placed close to each other. """
-
         def plot_facet(data, color):
-            print(color)
             #col = self.get_colors_for_factor(self.col_factor, rgb_string=True)
             
             values = [data[data[self._groupby[-1]] == x]["coquery_invisible_corpus_id"].values for x in self._levels[-1]]
             
             beeswarm(
                 values=values,
-                #positions=range(len(self._levels[-1])),
+                positions=range(len(self._levels[-1])),
                 ax=plt.gca())
-        
-        #grouped = self._table.groupby(self.col_factor)
-        #val=[self._table.iloc[grouped.groups[x]]["coquery_invisible_corpus_id"] for x in grouped.groups]
-        
-        #beeswarm(
-            #values=val,
-            ##positions=self._table["coquery_invisible_corpus_id"],
-            #col=col.values(), method="hex", labels=col.keys(), ax = self.subplot)
         
         self.g.map_dataframe(plot_facet)
         self.g.set_axis_labels(self._groupby[-1], "Corpus position")
