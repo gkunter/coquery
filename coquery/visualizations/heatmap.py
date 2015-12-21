10f15f51d3ee769167407b53e6bbe1913839af2c
gkunter <coquery@users.strandwall.de>
2015-12-22 00:58:59 +0100

    Add license notice.

--- coquery/visualizations/heatmap.py
+++ coquery/visualizations/heatmap.py
@@ -1,4 +1,13 @@
-""" Heatmap visualization """
+# -*- coding: utf-8 -*-
+""" 
+heatmap.py is part of Coquery.
+
+Copyright (c) 2015 Gero Kunter (gero.kunter@coquery.org)
+
+Coquery is released under the terms of the GNU General Public License.
+For details, see the file LICENSE that you should have received along 
+with Coquery. If not, see <http://www.gnu.org/licenses/>.
+"""
 
 import visualizer as vis
 import seaborn as sns
