fc2ab9d037b6ece2fde862ecc449bec48c3ac9a0
gkunter <gero.kunter@uni-duesseldorf.de>
2016-01-08 18:47:59 +0100

    Update license note. Use list of CoqDetailBox for installers

--- coquery/gui/corpusmanager.py
+++ coquery/gui/corpusmanager.py
@@ -3,18 +3,21 @@
 """
 corpusmanager.py is part of Coquery.
 
-Copyright (c) 2015 Gero Kunter (gero.kunter@coquery.org)
+Copyright (c) 2016 Gero Kunter (gero.kunter@coquery.org)
 
-Coquery is released under the terms of the GNU General Public License.
+Coquery is released under the terms of the GNU General Public License. A 
+Coquery exception applies under GNU GPL version 3 section 7.
+
 For details, see the file LICENSE that you should have received along 
-with Coquery. If not, see <http://www.gnu.org/licenses/>.
+with Coquery. If not, see <http://www.gnu.org/licenses/>. For the Coquery 
+exception, see <http://www.coquery.org/license/>.
 """
 
 from __future__ import unicode_literals
 from __future__ import division
 
-
 from pyqt_compat import QtCore, QtGui
+import classes
 
 import sys
 import corpusManagerUi
@@ -33,8 +36,8 @@ from defines import *
 
 class CoqAccordionEntry(QtGui.QWidget):
     """ Define a QWidget that can be used as an entry in a accordion list."""
-    def __init__(self, stack=None, *args):
-        super(CoqAccordionEntry, self).__init__(*args)
+    def __init__(self, stack=None, *args, **kwargs):
+        super(CoqAccordionEntry, self).__init__(*args, **kwargs)
         self._title = ""
         self._text = ""
         self._url = ""
@@ -45,11 +48,14 @@ class CoqAccordionEntry(QtGui.QWidget):
         self._checksum = ""
         self._validation = ""
         self._modules = []
-        self.stack=stack
+        self._stack = stack
         
         self.verticalLayout_2 = QtGui.QVBoxLayout(self)
         self.corpus_description_frame = QtGui.QFrame(self)
-        self.corpus_description_frame.setStyleSheet("QFrame { background-color: rgb(250, 250, 251); }")
+        
+        button_col = options.cfg.app.palette().color(QtGui.QPalette().Button)
+        style = "QFrame {{ background-color: rgb({}, {}, {}); }}".format(button_col.red(), button_col.green(), button_col.blue())
+        self.corpus_description_frame.setStyleSheet(style)
         self.corpus_description_frame.setFrameShape(QtGui.QFrame.StyledPanel)
         self.corpus_description_frame.setFrameShadow(QtGui.QFrame.Sunken)
         self.verticalLayout_3 = QtGui.QVBoxLayout(self.corpus_description_frame)
@@ -64,35 +70,39 @@ class CoqAccordionEntry(QtGui.QWidget):
         self.verticalLayout_3.addWidget(self.corpus_description)
         
         self.button_layout = QtGui.QHBoxLayout()
-        self.button_layout.setSpacing(0)
-        self.button_manage = QtGui.QPushButton(self.corpus_description_frame)
+        self.button_layout.setSpacing(8)
+
         self.validation_label = QtGui.QLabel("")
-        self.button_layout.addWidget(self.button_manage)
         self.button_layout.addWidget(self.validation_label)
         spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
         self.button_layout.addItem(spacerItem)
         self.verticalLayout_3.addLayout(self.button_layout)
-        #spacerItem1 = QtGui.QSpacerItem(20, 215, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
-        #self.verticalLayout_3.addItem(spacerItem1)
         
         self.verticalLayout_2.addWidget(self.corpus_description_frame)
+        self.verticalLayout_2.setContentsMargins(0, 0, 0, 0)
 
-    def setupInstallState(self, installed):
-        if installed:
-            self.button_manage.setText("Remove")
-            if self.stack:
-                self.button_manage.clicked.connect(lambda x: 
-                self.stack.removeCorpus.emit(self._name))
-        else:
-            self.button_manage.setText("Install")
+    def setup_buttons(self, installed, entry_widget):
+        self.button_install = QtGui.QPushButton(entry_widget)
+        self.button_layout.insertWidget(0, self.button_install)
+        if not installed:
+            self.button_install.setText("Install")
+
             self.validation_label.setText(
                 "<b>MD5 checksum:</b> {} ({})".format(
-                    self._checksum, self._validation))
+                self._checksum, self._validation))
+        else:
+            self.button_install.setText("Reinstall")
 
-            if self.stack:
-                self.button_manage.clicked.connect(self.safe_install)
-                #self.button_manage.clicked.connect(lambda x: 
-                #self.stack.installCorpus.emit(self._builder_class))
+            self.button_remove = QtGui.QPushButton(entry_widget)
+            self.button_remove.setIcon(QtGui.qApp.style().standardIcon(QtGui.QStyle.SP_DialogDiscardButton))
+            entry_widget.header_layout.addWidget(self.button_remove)
+
+            if self._stack:
+                self.button_remove.clicked.connect(lambda x: 
+                self._stack.removeCorpus.emit(self._name))
+
+        if self._stack:
+            self.button_install.clicked.connect(self.safe_install)
     
     def safe_install(self):
         if self._validation == "validated":
@@ -114,11 +124,11 @@ class CoqAccordionEntry(QtGui.QWidget):
             
         msg = msg.format(corpus=self._name)
 
-        response = box(None, 
-            "Unvalidated corpus installer – Coquery", 
-            msg, QtGui.QMessageBox.Yes| QtGui.QMessageBox.No, default)
-        if response == QtGui.QMessageBox.Yes:
-            self.stack.installCorpus.emit(self._builder_class)
+        #response = box(None, 
+            #"Unvalidated corpus installer – Coquery", 
+            #msg, QtGui.QMessageBox.Yes| QtGui.QMessageBox.No, default)
+        if True or response == QtGui.QMessageBox.Yes:
+            self._stack.installCorpus.emit(self._builder_class)
     
     def setReferences(self, ref):
         self._references = ref
@@ -188,95 +198,6 @@ class CoqAccordionEntry(QtGui.QWidget):
         self.corpus_description.setText(
             "".join(string_list))
 
-    #def setup_ui(self):
-        #self.corpus_entry = QtGui.QFrame(self)
-        
-        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.MinimumExpanding)
-        #sizePolicy.setHorizontalStretch(0)
-        #sizePolicy.setVerticalStretch(0)
-        #sizePolicy.setHeightForWidth(self.corpus_entry.sizePolicy().hasHeightForWidth())
-        #self.corpus_entry.setSizePolicy(sizePolicy)
-
-        
-        
-        #self.corpus_list_entry = QtGui.QVBoxLayout(self.corpus_entry)
-        #self.corpus_list_entry.setSpacing(0)
-        
-        ##self.header_box = QtGui.QFrame(self.corpus_entry)
-        ##self.header_box.setStyleSheet("background-color: lightgrey;")
-        ##self.header_box.setFrameShape(QtGui.QFrame.Box)
-
-        ##sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Minimum)
-        ##sizePolicy.setHorizontalStretch(0)
-        ##sizePolicy.setVerticalStretch(0)
-        ##sizePolicy.setHeightForWidth(self.header_box.sizePolicy().hasHeightForWidth())
-        ##self.header_box.setSizePolicy(sizePolicy)
-
-        
-        ##self.horizontalLayout_2 = QtGui.QHBoxLayout(self.header_box)
-        ##self.horizontalLayout_2.setSpacing(0)
-        ##self.horizontalLayout = QtGui.QHBoxLayout()
-        ##self.horizontalLayout.setSpacing(0)
-        ##self.horizontalLayout.setContentsMargins(10, -1, 10, -1)
-        ##self.horizontalLayout.setObjectName("horizontalLayout")
-        
-        ##self.expand_button = QtGui.QLabel(self.header_box)
-        ##self.expand_button.setText("+")
-        ##sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Fixed, QtGui.QSizePolicy.Minimum)
-        ##sizePolicy.setHorizontalStretch(0)
-        ##sizePolicy.setVerticalStretch(0)
-        ##sizePolicy.setHeightForWidth(self.expand_button.sizePolicy().hasHeightForWidth())
-        
-        ##self.expand_button.setSizePolicy(sizePolicy)
-        ##self.expand_button.setAlignment(QtCore.Qt.AlignCenter)
-        ##self.horizontalLayout.addWidget(self.expand_button)
-        ##self.horizontalLayout_2.addLayout(self.horizontalLayout)
-
-        ##self.label_title = QtGui.QLabel(self.header_box)
-        ##sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.MinimumExpanding, QtGui.QSizePolicy.Preferred)
-        ##sizePolicy.setHorizontalStretch(0)
-        ##sizePolicy.setVerticalStretch(0)
-        ##sizePolicy.setHeightForWidth(self.label_title.sizePolicy().hasHeightForWidth())
-
-        ##self.label_title.setSizePolicy(sizePolicy)
-        ##self.label_title.setFrameShape(QtGui.QFrame.NoFrame)
-        ##self.horizontalLayout_2.addWidget(self.label_title)
-        ##self.corpus_list_entry.addWidget(self.header_box)
-
-        #self.description_area = QtGui.QFrame(self.corpus_entry)
-        #self.description_area.setStyleSheet("QFrame {color: black; background-color: white;}")
-        #self.description_area.setFrameShape(QtGui.QFrame.Box)
-        #self.description_area.setFrameShadow(QtGui.QFrame.Raised)
-        #self.verticalLayout = QtGui.QVBoxLayout(self.description_area)
-        #self.verticalLayout.setSpacing(20)
-        #self.verticalLayout.setContentsMargins(45, 20, 20, 20)
-
-        #self.corpus_description = QtGui.QLabel(self.description_area)
-        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Expanding)
-        #sizePolicy.setHorizontalStretch(0)
-        #sizePolicy.setVerticalStretch(0)
-        #sizePolicy.setHeightForWidth(self.corpus_description.sizePolicy().hasHeightForWidth())
-        #self.corpus_description.setSizePolicy(sizePolicy)
-        #self.corpus_description.setWordWrap(True)
-        #self.verticalLayout.addWidget(self.corpus_description)
-
-        #self.layout_button = QtGui.QHBoxLayout()
-        #self.button_install = QtGui.QPushButton(self.description_area)
-        #self.button_install.setText("Install")
-        
-        #sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Minimum)
-        #sizePolicy.setHorizontalStretch(0)
-        #sizePolicy.setVerticalStretch(0)
-        #sizePolicy.setHeightForWidth(self.button_install.sizePolicy().hasHeightForWidth())
-        #self.button_install.setSizePolicy(sizePolicy)
-
-        #self.layout_button.addWidget(self.button_install)
-
-        #spacerItem = QtGui.QSpacerItem(40, 20, QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Minimum)
-        #self.layout_button.addItem(spacerItem)
-        #self.verticalLayout.addLayout(self.layout_button)
-        #self.corpus_list_entry.addWidget(self.description_area)
-
 class CorpusManager(QtGui.QDialog):
     removeCorpus = QtCore.Signal(object)
     installCorpus = QtCore.Signal(object)
@@ -285,7 +206,8 @@ class CorpusManager(QtGui.QDialog):
         super(CorpusManager, self).__init__(parent)
         self.ui = corpusManagerUi.Ui_corpusManager()
         self.ui.setupUi(self)
-        self.icon = QtGui.QIcon.fromTheme("server-database")
+
+        self.last_detail_box = None
         
         try:
             self.resize(self.width(), options.cfg.corpus_manager_view_height)
@@ -295,26 +217,49 @@ class CorpusManager(QtGui.QDialog):
             self.resize(options.cfg.corpus_manager_view_width, self.height())
         except AttributeError:
             pass
-
         
-        #self.corpus_form_layout = QtGui.QVBoxLayout()
-        #self.group_box = QtGui.QGroupBox()
+    def read(self, path, label=None):
+        """
+        Read the installers from the path, and add a widget for each to the 
+        installer list.
         
-        #self.list_model = QtGui.QStandardItemModel(self.ui.corpus_list)
-        #self.ui.corpus_list.setModel(self.list_model)
+        Parameters
+        ----------
+        path : str 
+            The directory in which to look for installer modules
+        label : str or None 
+            A label that can be added before the first installer widget.
+        """
+        # if a label is provided, add it to the list:
+        if label:
+            header = QtGui.QLabel(label)
+            height = header.sizeHint().height()
+            if self.ui.list_layout.count() == 0:
+                # no top margin if this is the first widget in the layout:
+                header.setContentsMargins(0, 0, 0, int(height * 0.25))
+            else:
+                # add spacing otherwise:
+                header.setContentsMargins(0, int(height * 0.75), 0, int(height * 0.25))
+            self.ui.list_layout.addWidget(header)
         
-    def read(self, path):
-        matches = []
-        row = 0
         for root, dirnames, filenames in os.walk(path):
             installer_list = sorted(fnmatch.filter(filenames, 'coq_install_*.py'), reverse=True)
+            
+            # The generic installer should not appear in the corpus manager 
+            # list: 
             try:
                 installer_list.remove("coq_install_generic.py")
             except ValueError:
                 pass
+            
             for fullpath in installer_list:
                 module_path = os.path.join(root, fullpath)
                 basename, ext = os.path.splitext(os.path.basename(fullpath))
+
+                # Validate the file, i.e. determine whether the file contains
+                # only those instructions that are allowed for installer 
+                # modules.
+                 
                 try:
                     hashsum = options.validate_module(module_path, 
                             expected_classes=["BuilderClass"], 
@@ -328,8 +273,6 @@ class CorpusManager(QtGui.QDialog):
                         None, "Corpus validation error – Coquery", 
                         msg_invalid_installer.format(name=basename, code=str(e)), QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
                     continue
-                try:
-                    module = imp.load_source(basename, module_path)
                 except (ImportError, SyntaxError) as e:
                     msg = msg_corpus_broken.format(
                         name=basename,
@@ -340,13 +283,20 @@ class CorpusManager(QtGui.QDialog):
                         None, "Corpus error – Coquery", 
                         msg, QtGui.QMessageBox.Ok, QtGui.QMessageBox.Ok)
                     continue
+                    
+                # load the module:
+                module = imp.load_source(basename, module_path)
+
                 try:
                     builder_class = module.BuilderClass
                 except AttributeError:
                     continue
-                entry = CoqAccordionEntry(stack=self)
 
+                # create a new accordion entry:
+                entry = CoqAccordionEntry(stack=self)
                 name = builder_class.get_name()
+                self.detail_box = classes.CoqDetailBox(name, entry)
+
                 entry.setName(name)
                 
                 entry.setChecksum(hashsum)
@@ -371,22 +321,29 @@ class CorpusManager(QtGui.QDialog):
                 if builder_class.get_modules():
                     entry.setModules(builder_class.get_modules())
 
-                entry.setupInstallState(name in options.cfg.current_resources)
+                entry.setup_buttons(name in options.cfg.current_resources, entry_widget=self.detail_box)
                 entry.setBuilderClass(builder_class)
-
-                self.ui.corpus_stack.addItem(entry, self.icon, name)
                 
-        self.ui.scroll_area_corpus.setWidgetResizable(True)
-        self.ui.corpus_stack.setCurrentIndex(self.ui.corpus_stack.count() - 1)
+                # add entry to installer list:
+                self.detail_box.clicked.connect(lambda x: self.update_accordion(x))
+                self.ui.list_layout.addWidget(self.detail_box)
+
+    def update_accordion(self, detail_box):
+        """
+        Closes the last installer entry, and remembers the current one.
+        """
+        if self.last_detail_box and self.last_detail_box.isExpanded() and self.last_detail_box != detail_box:
+            self.last_detail_box.toggle()
+        self.last_detail_box = detail_box            
+
+    def add_stretcher(self):
+        self.ui.list_layout.addItem(QtGui.QSpacerItem(0, 0, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding))
 
     def closeEvent(self, *args):
         options.cfg.corpus_manager_view_height = self.height()
         options.cfg.corpus_manager_view_width = self.width()
         options.set_current_server(options.cfg.current_server)
                     
-    def add_source_label(self, name, content):
-        pass
-
 def main():
     app = QtGui.QApplication(sys.argv)
     installer_list = CorpusManager.display("../installer", lambda x: x, lambda x: x)
