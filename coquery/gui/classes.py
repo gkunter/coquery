d2a68204da1ea69a046f274c45c57dcd3427ca85
gkunter <coquery@users.strandwall.de>
2016-01-10 16:02:21 +0100

    Update license note.

--- coquery/gui/classes.py
+++ coquery/gui/classes.py
@@ -3,14 +3,11 @@
 """
 classes.py is part of Coquery.
 
-Copyright (c) 2015 Gero Kunter (gero.kunter@coquery.org)
+Copyright (c) 2016 Gero Kunter (gero.kunter@coquery.org)
 
-Coquery is released under the terms of the GNU General Public License. A 
-Coquery exception applies under GNU GPL version 3 section 7.
-
+Coquery is released under the terms of the GNU General Public License (v3).
 For details, see the file LICENSE that you should have received along 
-with Coquery. If not, see <http://www.gnu.org/licenses/>. For the Coquery 
-exception, see <http://www.coquery.org/license/>.
+with Coquery. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from __future__ import unicode_literals
