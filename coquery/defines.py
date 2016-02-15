9b221dd02d2e888ae2aa9daf52fa7d509c44ff10
gkunter <coquery@users.strandwall.de>
2016-02-15 09:06:58 +0100

    Rename column

--- coquery/defines.py
+++ coquery/defines.py
@@ -68,7 +68,7 @@ COLUMN_NAMES = {
     "coq_conditional_probability": "Pcond",
 
     "coquery_query_token": "Query token",
-    "coquery_expanded_query_string": "Expanded query string",
+    "coquery_expanded_query_string": "Matched query string",
     "coquery_query_string": "Query string",
     
     "statistics_frequency": "Frequency", 
