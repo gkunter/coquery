743822ff0d3b21533f3b7504f82ac11417ab2dd0
gkunter <gero.kunter@uni-duesseldorf.de>
2016-02-22 19:47:34 +0100

    Update icons

--- coquery/icons/svg2png.sh
+++ coquery/icons/svg2png.sh
@@ -1,1 +1,1 @@
-for i in small-n-flat/SVG/*.svg; do convert -density 300x300 -resize x96 -background none "$i" "small-n-flat/PNG/$(basename ${i%.*}).png" ; done
+for i in small-n-flat/SVG/*.svg; do convert -resize x32 -background none "$i" "small-n-flat/PNG/$(basename ${i%.*}).png" ; done
