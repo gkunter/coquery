e2cfc0e1656793b704cc00467b859e2b93f4a2c6
gkunter <coquery@users.strandwall.de>
2016-02-28 19:40:19 +0100

    Add path extensions

--- coquery/__init__.py
+++ coquery/__init__.py
@@ -10,6 +10,14 @@ For details, see the file LICENSE that you should have received along
 with Coquery. If not, see <http://www.gnu.org/licenses/>.
 """
 
+import sys
+import os
+
+base_path, _ = os.path.split(os.path.realpath(__file__))
+sys.path.append(base_path)
+sys.path.append(os.path.join(base_path, "gui"))
+
 __version__ = "0.9"
 NAME = "Coquery"
 DATE = "2016"
+
