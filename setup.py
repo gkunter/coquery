04e68633c6cf084808c8287cf4a1f8054ff6a9c7
gkunter <coquery@users.strandwall.de>
2016-03-05 17:27:06 +0100

    Sreamline setup

--- setup.py
+++ setup.py
@@ -9,62 +9,12 @@ except ImportError:
 
 import re
 
-def has_module(name):
-    """
-    Check if the Python module 'name' is available.
-    
-    Parameters
-    ----------
-    name : str 
-        The name of the Python module, as used in an import instruction.
-        
-    This function uses ideas from this Stack Overflow question:
-    http://stackoverflow.com/questions/14050281/
-        
-    Returns
-    -------
-    b : bool
-        True if the module exists, or False otherwise.
-    """
-
-    if sys.version_info > (3, 3):
-        import importlib.util
-        return importlib.util.find_spec(name) is not None
-    elif sys.version_info > (2, 7, 99):
-        import importlib
-        return importlib.find_loader(name) is not None
-    else:
-        import pkgutil
-        return pkgutil.find_loader(name) is not None
-
-
-with open('coquery/__init__.py', "rt") as f:
-    version = re.search('^__version__\s*=\s*"(.*)"', f.read(), re.M).group(1)
+from coquery.defines import VERSION as version
 
 with open("README.md", "rb") as f:
     long_descr = f.read().decode("utf-8")
 
 DESCRIPTION = "Coquery: a free corpus query tool"
-LONG_DESCRIPTION = """\
-Coquery is a free corpus query tool for linguistis, lexicographers, 
-translators, and anybody who wishes to search and analyse text corpora.
-It is available for Windows, Linux, and Mac OS X computers.
-
-You can either build your own corpus from a collection of text files
-or PDF documents in a directory on your computer, or install a corpus 
-module for one of the supported corpora (the corpus data files are not
-provided by Coquery).
-
-A corpus can then be queried using a simple, yet expressive query syntax.
-The query produces a list of matching tokens that can be flexibly arranged
-in the form of token tables, frequency tables, contingency tables, and
-collocation tables. Coquery can also calculate different statistics such as 
-normalized frequencies, Shannon entropy, or mutual information scores.
-
-Several diagram modules are available to create visualizations of frequency
-distributions, categorical differences, or diachronic changes.
-"""
-
 
 
 if __name__ == "__main__":
