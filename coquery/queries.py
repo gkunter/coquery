b8310118865a186e835fb49a50e76649bf1c5e33
gkunter <gero.kunter@uni-duesseldorf.de>
2016-03-07 19:56:12 +0100

    Only execute ATTACH DATABASE on SQLite connections

--- coquery/queries.py
+++ coquery/queries.py
@@ -316,10 +316,11 @@ class TokenQuery(object):
                         logger.info(query_string)
                     
                     # SQLite: attach external databases
-                    for db_name in self.Resource.attach_list:
-                        path = os.path.join(options.cfg.database_path, "{}.db".format(db_name))
-                        S = "ATTACH DATABASE '{}' AS {}".format(path, db_name)
-                        connection.execute(S)
+                    if self.Resource.db_type == SQL_SQLITE:
+                        for db_name in self.Resource.attach_list:
+                            path = os.path.join(options.cfg.database_path, "{}.db".format(db_name))
+                            S = "ATTACH DATABASE '{}' AS {}".format(path, db_name)
+                            connection.execute(S)
 
                     try:
                         results = connection.execution_options(stream_results=True).execute(query_string.replace("%", "%%"))
