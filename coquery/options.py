1eb7f09ed9b9643c2cd1a4d9b66b9e67017d3be6
gkunter <gero.kunter@uni-duesseldorf.de>
2016-03-16 19:00:42 +0100

    Make command line help and argument parser Unicode compatible

--- coquery/options.py
+++ coquery/options.py
@@ -50,6 +50,120 @@ from .unicode import utf8
 from .defines import *
 from .errors import *
 
+# Define a HelpFormatter class that works with Unicode corpus names both in 
+# Python 2.7 and Python 3.x:
+if sys.version_info < (3, 0):
+    class CoqHelpFormatter(argparse.HelpFormatter):
+        """
+        A HelpFormatter class that is able to handle Unicode argument options.
+        
+        The code for _format_actions_usage() is a copy from 
+        python2.7/argparse.py, with the addition of the utf8() conversion in
+        one space.
+        """
+        def _format_actions_usage(self, actions, groups):
+            # find group indices and identify actions in groups
+            group_actions = set()
+            inserts = {}
+            for group in groups:
+                try:
+                    start = actions.index(group._group_actions[0])
+                except ValueError:
+                    continue
+                else:
+                    end = start + len(group._group_actions)
+                    if actions[start:end] == group._group_actions:
+                        for action in group._group_actions:
+                            group_actions.add(action)
+                        if not group.required:
+                            if start in inserts:
+                                inserts[start] += ' ['
+                            else:
+                                inserts[start] = '['
+                            inserts[end] = ']'
+                        else:
+                            if start in inserts:
+                                inserts[start] += ' ('
+                            else:
+                                inserts[start] = '('
+                            inserts[end] = ')'
+                        for i in range(start + 1, end):
+                            inserts[i] = '|'
+
+            # collect all actions format strings
+            parts = []
+            for i, action in enumerate(actions):
+
+                # suppressed arguments are marked with None
+                # remove | separators for suppressed arguments
+                if action.help is argparse.SUPPRESS:
+                    parts.append(None)
+                    if inserts.get(i) == '|':
+                        inserts.pop(i)
+                    elif inserts.get(i + 1) == '|':
+                        inserts.pop(i + 1)
+
+                # produce all arg strings
+                elif not action.option_strings:
+                    part = self._format_args(action, action.dest)
+
+                    # if it's in a group, strip the outer []
+                    if action in group_actions:
+                        if part[0] == '[' and part[-1] == ']':
+                            part = part[1:-1]
+
+                    # add the action string to the list
+                    parts.append(part)
+
+                # produce the first way to invoke the option in brackets
+                else:
+                    option_string = action.option_strings[0]
+
+                    # if the Optional doesn't take a value, format is:
+                    #    -s or --long
+                    if action.nargs == 0:
+                        part = '%s' % option_string
+
+                    # if the Optional takes a value, format is:
+                    #    -s ARGS or --long ARGS
+                    else:
+                        default = action.dest.upper()
+                        args_string = self._format_args(action, default)
+                        part = '%s %s' % (option_string, args_string)
+
+                    # make it look optional if it's not required or in a group
+                    if not action.required and action not in group_actions:
+                        part = '[%s]' % part
+
+                    # add the action string to the list
+                    parts.append(part)
+
+            # insert things at the necessary indices
+            for i in sorted(inserts, reverse=True):
+                parts[i:i] = [inserts[i]]
+
+            # join all the action items with spaces
+            text = u' '.join([item.decode("utf-8") for item in parts if item is not None])
+
+            # clean up separators for mutually exclusive groups
+            open = r'[\[(]'
+            close = r'[\])]'
+            text = argparse._re.sub(r'(%s) ' % open, r'\1', text)
+            text = argparse._re.sub(r' (%s)' % close, r'\1', text)
+            text = argparse._re.sub(r'%s *%s' % (open, close), r'', text)
+            text = argparse._re.sub(r'\(([^|]*)\)', r'\1', text)
+            text = text.strip()
+
+            # return the text
+            return text
+
+        def _join_parts(self, part_strings):
+            part_strings = [utf8(x) for x in part_strings if utf8(x) and x is not argparse.SUPPRESS]
+            return "".join(part_strings)
+else:
+    class CoqHelpFormatter(argparse.HelpFormatter):
+        pass
+
 class Options(object):
     def __init__(self):
         self.args = argparse.Namespace()
@@ -59,7 +173,7 @@ class Options(object):
         self.prog_name = NAME
         self.config_name = "%s.cfg" % NAME.lower()
         self.version = VERSION
-        self.parser = argparse.ArgumentParser(prog=self.prog_name, add_help=False)
+        self.parser = argparse.ArgumentParser(prog=self.prog_name, add_help=False, formatter_class=CoqHelpFormatter)
 
         self.args.config_path = os.path.join(self.args.coquery_home, self.config_name)
         self.args.disabled_columns = set([])
@@ -135,6 +249,9 @@ class Options(object):
         
     def setup_parser(self):
         self.parser.add_argument("MODE", help="determine the query mode (default: TOKEN)", choices=(QUERY_MODE_TOKENS, QUERY_MODE_FREQUENCIES, QUERY_MODE_DISTINCT, QUERY_MODE_STATISTICS, QUERY_MODE_CONTINGENCY, QUERY_MODE_COLLOCATIONS), type=str, nargs="?")
+        if sys.version_info < (3, 0):
+            l = [x.encode("utf-8") for x in self.corpus_argument_dict["choices"]]
+            self.corpus_argument_dict["choices"] = l
         self.parser.add_argument("corpus", nargs="?", **self.corpus_argument_dict)
         
         # If Qt is available, the GUI is used by default. The command line 
@@ -235,6 +352,7 @@ class Options(object):
                 self.args.corpus = ""
         except AttributeError:
             self.args.corpus = ""
+        self.args.corpus = utf8(self.args.corpus)
         # if no corpus is selected and no GUI is requested, display the help
         # and exit.
         if not self.args.corpus and not (self.args.gui):
