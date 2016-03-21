f872a81f0ba8031ab6bd55a927e7a96392a4f489
gkunter <gk_bitbucket@users.strandwall.de>
2016-03-22 00:15:15 +0100

    Add module information to About dialog

--- coquery/gui/app.py
+++ coquery/gui/app.py
@@ -150,10 +150,7 @@ class CoqueryApp(QtGui.QMainWindow):
         if sys.platform == "win32":
             import ctypes
             CoqId = 'Coquery.Coquery.{}'.format(VERSION)
-            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(CoqId)
-        
-        # self.showMessage("Seaborn: {} MySQL: {} NLTK: {} tgt: {} pdfminer: {}".format(options._use_seaborn, options._use_mysql, options._use_nltk, options._use_tgt, options._use_pdfminer))
-        
+            ctypes.windll.shell32.SetCurrentProcessExplicitAppUserModelID(CoqId)        
         
     def setup_app(self):
         """ Initialize all widgets with suitable data """
@@ -2200,8 +2197,16 @@ class CoqueryApp(QtGui.QMainWindow):
         dialog.ui.label_pixmap.setPixmap(QtGui.QPixmap.fromImage(image))
         dialog.ui.label_pixmap.setAlignment(QtCore.Qt.AlignCenter)
 
-        dialog.ui.label_description.setText(
-            unicode(dialog.ui.label_description.text()).format(version=VERSION, date=DATE))
+        dialog.ui.label_description.setText("{}<p>Optional modues:<br/>{}</p>".format(
+            unicode(dialog.ui.label_description.text()).format(version=VERSION, date=DATE),
+            "Seaborn: {} &nbsp;&nbsp;&nbsp; MySQL: {} &nbsp;&nbsp;&nbsp; NLTK: {} &nbsp;&nbsp;&nbsp; tgt: {} &nbsp;&nbsp;&nbsp; pdfminer: {} &nbsp;&nbsp;&nbsp; chardet: {}".format(
+                "yes" if options._use_seaborn else "no", 
+                "yes" if options._use_mysql else "no",
+                "yes" if options._use_nltk else "no", 
+                "yes" if options._use_tgt else "no", 
+                "yes" if options._use_pdfminer else "no", 
+                "yes" if options._use_chardet else "no")))
+
         dialog.exec_()
 
     def setGUIDefaults(self):
