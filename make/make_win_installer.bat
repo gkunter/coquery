31d039e5459289061c05bbf5eaf6087f4e121c5c
gkunter <coquery@users.strandwall.de>
2016-05-01 15:43:41 +0200

    Use %HOMEPATH%

--- make/make_win_installer.bat
+++ make/make_win_installer.bat
@@ -1,4 +1,4 @@
-rmdir C:\Users\dadasd\coquery\make\build /s /q
-rmdir C:\Users\dadasd\coquery\make\dist /s /q
+rmdir %HOMEPATH%\coquery\make\build /s /q
+rmdir %HOMEPATH%\coquery\make\dist /s /q
 pyinstaller coquery_win.spec
 "C:\Program Files\Inno Setup 5\Compil32.exe" /cc coquery.iss
