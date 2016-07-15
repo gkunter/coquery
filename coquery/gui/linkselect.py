fa0d3b2b28d2ed38658a4b6341c9ad9702543a52
gkunter <gero.kunter@uni-duesseldorf.de>
2016-07-15 17:37:35 +0200

    Revert previous commit

--- coquery/gui/linkselect.py
+++ coquery/gui/linkselect.py
@@ -1,9 +1,6 @@
-# -*- coding: utf-8 -*-
 """
 linkselect.py is part of Coquery.
-
 Copyright (c) 2016 Gero Kunter (gero.kunter@coquery.org)
-
 Coquery is released under the terms of the GNU General Public License (v3).
 For details, see the file LICENSE that you should have received along 
 with Coquery. If not, see <http://www.gnu.org/licenses/>.
@@ -15,39 +12,28 @@ from __future__ import unicode_literals
 import sys
 
 from coquery import options
+from coquery.corpus import BaseResource
 from coquery.defines import *
+from coquery.unicode import utf8
+from coquery.links import Link
 
 from .classes import CoqTreeItem
 from .pyqt_compat import QtCore, QtGui
 from .ui.linkselectUi import Ui_LinkSelect
 
-class Link(object):
-    def __init__(self, tree_item, ignore_case=True):
-        self.resource = tree_item.parent().parent().objectName()
-        resource = options.cfg.current_resources[self.resource][0] 
-
-        self.rc_feature = tree_item.objectName()
-        self.table = "{}".format(tree_item.parent().objectName())
-        self.db_name = resource.db_name
-        self.case = ignore_case
-        
-        self.table_name = getattr(resource, "{}_table".format(self.table))
-        self.feature_name = getattr(resource, self.rc_feature)
-        self.label = ".".join([self.resource, self.table_name, self.feature_name])
-
 class LinkSelect(QtGui.QDialog):
-    def __init__(self, feature, corpus, corpus_omit = [], parent=None):
-        
+    def __init__(self, res_from, rc_from, corpus_omit=[], parent=None):
         super(LinkSelect, self).__init__(parent)
-
-        # the following tables will not be shown as possible candidates for 
-        # linked tables:
-        self.omit_tables = ["coquery", "statistics", "corpusngram"]
         self.corpus_omit = corpus_omit
+        self.res_from = res_from
+        self.rc_from = rc_from
+        
         self.ui = Ui_LinkSelect()
         self.ui.setupUi(self)
         self.ui.label.setText(str(self.ui.label.text()).format(
-            resource_feature=feature, corpus=corpus))
+            resource_feature=getattr(res_from, rc_from), 
+            corpus=res_from.name))
+
         self.insert_data()
         self.ui.treeWidget.itemActivated.connect(self.selected)
         self.ui.buttonBox.button(QtGui.QDialogButtonBox.Ok).setEnabled(False)
@@ -60,6 +46,25 @@ class LinkSelect(QtGui.QDialog):
     def closeEvent(self, event):
         options.settings.setValue("linkselect_size", self.size())
         
+    @staticmethod
+    def pick(res_from, rc_from, corpus_omit, parent=None):
+        dialog = LinkSelect(res_from, rc_from, corpus_omit, parent=parent)        
+        dialog.setVisible(True)
+        result = dialog.exec_()
+        if result == QtGui.QDialog.Accepted:
+            selected_column_item = dialog.ui.treeWidget.selectedItems()[0]
+            selected_table_item = selected_column_item.parent()
+            selected_resource_item = selected_table_item.parent()
+
+            link = Link(res_from=res_from.name, 
+                        rc_from=rc_from, 
+                        res_to=selected_resource_item.objectName(), 
+                        rc_to=selected_column_item.objectName(),
+                        case=bool(dialog.ui.checkBox.checkState()))
+            return link
+        else:
+            return None
+
     def selected(self):
         item = self.ui.treeWidget.selectedItems()[0]
         if item.childCount():
@@ -77,19 +82,21 @@ class LinkSelect(QtGui.QDialog):
             corpusItem = CoqTreeItem()
             corpusItem.setText(0, corpus)
             corpusItem.setObjectName(corpus)
-            corpusItem.setIcon(0, self.parent().get_icon("database"))
-            for table in [x for x in table_dict if x not in self.omit_tables]:
+            if self.parent():
+                corpusItem.setIcon(0, self.parent().get_icon("database"))
+            for table in [x for x in table_dict if x not in self.res_from.special_table_list]:
                 table_string = getattr(resource, "{}_table".format(table))
                 tableItem = CoqTreeItem()
                 tableItem.setText(0, table_string)
                 tableItem.setObjectName(table)
-                tableItem.setIcon(0, self.parent().get_icon("table"))
+                if self.parent():
+                    tableItem.setIcon(0, self.parent().get_icon("table"))
                 for feature in [x for x in table_dict[table] if not x.rpartition("_")[-1] in ("table", "id")]:
                     featureItem = CoqTreeItem()
                     featureItem.setText(0, getattr(resource, feature))
                     featureItem.setObjectName(feature)
                     tableItem.addChild(featureItem)
-                    if feature in list(tag_list.values()):
+                    if feature in list(tag_list.values()) and self.parent():
                         featureItem.setIcon(0, self.parent().get_icon("tag"))
                     
                 if tableItem.childCount():
@@ -101,24 +108,3 @@ class LinkSelect(QtGui.QDialog):
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_Escape:
             self.reject()
-            
-    @staticmethod
-    def display(feature, corpus, corpus_omit, parent=None):
-        dialog = LinkSelect(feature, corpus, corpus_omit, parent=parent)        
-        dialog.setVisible(True)
-        result = dialog.exec_()
-        if result == QtGui.QDialog.Accepted:
-            selected = dialog.ui.treeWidget.selectedItems()[0]
-            return Link(selected, dialog.ui.checkBox.checkState())
-        else:
-            return None
-        
-
-def main():
-    app = QtGui.QApplication(sys.argv)
-    viewer = LinkSelect()
-    viewer.exec_()
-    
-if __name__ == "__main__":
-    main()
-    
