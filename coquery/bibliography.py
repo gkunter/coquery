210c4a438ad3d5be17fcf74177a11194c647bf40
gkunter <gero.kunter@uni-duesseldorf.de>
2016-09-28 21:38:07 +0200

    Add docstring with license note

--- coquery/bibliography.py
+++ coquery/bibliography.py
@@ -1,3 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+bibliography.py is part of Coquery.
+
+Copyright (c) 2016 Gero Kunter (gero.kunter@coquery.org)
+
+Coquery is released under the terms of the GNU General Public License (v3).
+For details, see the file LICENSE that you should have received along 
+with Coquery. If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from __future__ import unicode_literals
 import warnings
 
