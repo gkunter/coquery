8b7f6499289efc3eec82b92ca205ce0315d2ee34
gkunter <gero.kunter@uni-duesseldorf.de>
2016-10-12 19:08:40 +0200

    Add stopwords module

--- coquery/gui/availablemodules.py
+++ coquery/gui/availablemodules.py
@@ -45,7 +45,9 @@ class AvailableModulesDialog(QtGui.QDialog):
                 ("PDFMiner" if sys.version_info < (3, 0) else "pdfminer3k", options.use_pdfminer),
                 ("python-docx", options.use_docx),
                 ("odfpy", options.use_odfpy),
-                ("BeautifulSoup", options.use_bs4))
+                ("BeautifulSoup", options.use_bs4),
+                ("stop-words", options.use_stopwords_module),
+                )
         
         self.ui.table_modules.setRowCount(len(modules))
         
