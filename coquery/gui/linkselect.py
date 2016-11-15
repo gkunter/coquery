0bc82d59bc0fd5199acda7d8911233e2b8a3e38e
gkunter <gero.kunter@uni-duesseldorf.de>
2016-11-15 18:58:59 +0100

    Support corpus selection

--- coquery/gui/linkselect.py
+++ coquery/gui/linkselect.py
@@ -22,7 +22,7 @@ from .pyqt_compat import QtCore, QtGui
 from .ui.linkselectUi import Ui_LinkSelect
 
 class LinkSelect(QtGui.QDialog):
-    def __init__(self, res_from, rc_from, corpus_omit=[], parent=None):
+    def __init__(self, res_from=None, rc_from=None, corpus_omit=[], only_resources=False, parent=None):
         super(LinkSelect, self).__init__(parent)
         self.corpus_omit = corpus_omit
         self.res_from = res_from
@@ -30,11 +30,14 @@ class LinkSelect(QtGui.QDialog):
         
         self.ui = Ui_LinkSelect()
         self.ui.setupUi(self)
-        self.ui.label.setText(str(self.ui.label.text()).format(
-            resource_feature=getattr(res_from, rc_from), 
-            corpus=res_from.name))
+        if res_from and res_to:
+            self.ui.label.setText(str(self.ui.label.text()).format(
+                resource_feature=getattr(res_from, rc_from), 
+                corpus=res_from.name))
+        else:
+            self.ui.label.setText("Select a corpus:")
 
-        self.insert_data()
+        self.insert_data(only_resources)
         self.ui.treeWidget.itemActivated.connect(self.selected)
         self.ui.buttonBox.button(QtGui.QDialogButtonBox.Ok).setEnabled(False)
 
@@ -65,6 +68,18 @@ class LinkSelect(QtGui.QDialog):
         else:
             return None
 
+    @staticmethod
+    def get_resource(corpus_omit, parent=None):
+        dialog = LinkSelect(res_from=None, rc_from=None, corpus_omit=corpus_omit, only_resources=True, parent=parent)        
+        dialog.setVisible(True)
+        result = dialog.exec_()
+        if result == QtGui.QDialog.Accepted:
+            x = dialog.ui.treeWidget.selectedItems()[0]
+            print(x)
+            return x.objectName()
+        else:
+            return None
+
     def selected(self):
         item = self.ui.treeWidget.selectedItems()[0]
         if item.childCount():
@@ -73,7 +88,7 @@ class LinkSelect(QtGui.QDialog):
         else:
             self.ui.buttonBox.button(QtGui.QDialogButtonBox.Ok).setEnabled(True)
         
-    def insert_data(self):
+    def insert_data(self, only_resources=False):
         for corpus in [x for x in options.cfg.current_resources if x not in self.corpus_omit]:
             resource = options.cfg.current_resources[corpus][0]
             tag_list = resource.get_query_item_map()
@@ -84,24 +99,27 @@ class LinkSelect(QtGui.QDialog):
             corpusItem.setObjectName(corpus)
             if self.parent():
                 corpusItem.setIcon(0, self.parent().get_icon("database"))
-            for table in [x for x in table_dict if x not in self.res_from.special_table_list]:
-                table_string = getattr(resource, "{}_table".format(table))
-                tableItem = CoqTreeItem()
-                tableItem.setText(0, table_string)
-                tableItem.setObjectName(table)
-                if self.parent():
-                    tableItem.setIcon(0, self.parent().get_icon("table"))
-                for feature in [x for x in table_dict[table] if not x.rpartition("_")[-1] in ("table", "id")]:
-                    featureItem = CoqTreeItem()
-                    featureItem.setText(0, getattr(resource, feature))
-                    featureItem.setObjectName(feature)
-                    tableItem.addChild(featureItem)
-                    if feature in list(tag_list.values()) and self.parent():
-                        featureItem.setIcon(0, self.parent().get_icon("tag"))
-                    
-                if tableItem.childCount():
-                    corpusItem.addChild(tableItem)
-            if corpusItem.childCount():
+            if not only_resources:
+                for table in [x for x in table_dict if x not in self.res_from.special_table_list]:
+                    table_string = getattr(resource, "{}_table".format(table))
+                    tableItem = CoqTreeItem()
+                    tableItem.setText(0, table_string)
+                    tableItem.setObjectName(table)
+                    if self.parent():
+                        tableItem.setIcon(0, self.parent().get_icon("table"))
+                    for feature in [x for x in table_dict[table] if not x.rpartition("_")[-1] in ("table", "id")]:
+                        featureItem = CoqTreeItem()
+                        featureItem.setText(0, getattr(resource, feature))
+                        featureItem.setObjectName(feature)
+                        tableItem.addChild(featureItem)
+                        if feature in list(tag_list.values()) and self.parent():
+                            featureItem.setIcon(0, self.parent().get_icon("tag"))
+                        
+                    if tableItem.childCount():
+                        corpusItem.addChild(tableItem)
+                if corpusItem.childCount():
+                    self.ui.treeWidget.addTopLevelItem(corpusItem)
+            else:
                 self.ui.treeWidget.addTopLevelItem(corpusItem)
         self.ui.treeWidget.sortItems(0, QtCore.Qt.AscendingOrder)
         
