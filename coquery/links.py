56d5557246719656b634c24648f500392812c5de
gkunter <gero.kunter@uni-duesseldorf.de>
2016-11-23 19:02:02 +0100

    Use CoqObject as base class. Add PEP8

--- coquery/links.py
+++ coquery/links.py
@@ -5,7 +5,7 @@ links.py is part of Coquery.
 Copyright (c) 2016 Gero Kunter (gero.kunter@coquery.org)
 
 Coquery is released under the terms of the GNU General Public License (v3).
-For details, see the file LICENSE that you should have received along 
+For details, see the file LICENSE that you should have received along
 with Coquery. If not, see <http://www.gnu.org/licenses/>.
 """
 from __future__ import unicode_literals
@@ -13,14 +13,16 @@ from __future__ import unicode_literals
 import hashlib
 
 from .defines import *
+from .general import CoqObject
 
-class Link(object):
+
+class Link(CoqObject):
     """
     The Link class is used to link a table from one corpus to another corpus.
     """
-    
-    def __init__(self, res_from, rc_from, res_to, rc_to, join="LEFT JOIN", case=False,
-                 one_to_many=False):
+
+    def __init__(self, res_from, rc_from, res_to, rc_to, join="LEFT JOIN",
+                 case=False, one_to_many=False):
         """
         Parameters
         ----------
@@ -28,16 +30,16 @@ class Link(object):
             The name of the resource from which the link starts
         rc_from : str
             The resource feature from which the link starts
-        res_to : str 
+        res_to : str
             The name of the resource where the link ends
         rc_to : str
             The resource feature where the link ends
         join : str
             The SQL join type
         one_to_many : bool
-            True if all entries in the linked table are returned, or False if 
+            True if all entries in the linked table are returned, or False if
             only the first entry is returned
-        case : bool 
+        case : bool
             Determine whether the the join will be case sensitive
         """
         self.res_from = res_from
@@ -47,36 +49,34 @@ class Link(object):
         self.join_type = join
         self.case = case
         self.one_to_many = one_to_many
-        
-    def get_hash(self):
-        l = []
-        for x in sorted(dir(self)):
-            if not x.startswith("_") and not hasattr(getattr(self, x), "__call__"):
-                l.append(str(getattr(self, x)))
-        return hashlib.md5(u"".join(l).encode()).hexdigest()
-        
+
     def __repr__(self):
-        return "Link(res_from='{}', rc_from='{}', res_to='{}', rc_to='{}', join='{}', case={})".format(
-            self.res_from, self.rc_from, self.res_to, self.rc_to, self.join_type, self.case)
+        S = ("Link(res_from='{}', rc_from='{}', res_to='{}', rc_to='{}',"
+             "join='{}', case={})")
+        return S.format(
+            self.res_from, self.rc_from, self.res_to, self.rc_to,
+            self.join_type, self.case)
+
 
 def get_link_by_hash(link_list, hash):
     for link in link_list:
         if link.get_hash() == hash:
             return link
 
+
 def get_by_hash(hashed, link_list=None):
     """
     Return the link and the linked resource for the hash.
-    
+
     Parameters
     ----------
-    hashed : str 
+    hashed : str
         A hash string that has been produced by the get_hash() method.
-    
+
     link_list : list of Link objects
         A list of links that is used to lookup the hash. If not provided,
         the link list for the current server and resource is used.
-    
+
     Returns
     -------
     tup : tuple
@@ -86,7 +86,9 @@ def get_by_hash(hashed, link_list=None):
     from . import options
     if not link_list:
         link_list = options.cfg.table_links[options.cfg.current_server]
-    
+
     link = get_link_by_hash(link_list, hashed)
+    if not link:
+        raise ValueError(hashed, link_list)
     res = options.get_resource(link.res_to)[0]
     return (link, res)
