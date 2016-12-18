34f80f71f675168e3dddd62367a13ec97c02c342
gkunter <coquery@users.strandwall.de>
2016-12-18 15:32:20 +0100

    Add CoqListSelect class

---
+++ coquery/gui/listselect.py
@@ -0,0 +1,136 @@
+# -*- coding: utf-8 -*-
+"""
+listselect.py is part of Coquery.
+
+Copyright (c) 2016 Gero Kunter (gero.kunter@coquery.org)
+
+Coquery is released under the terms of the GNU General Public License (v3).
+For details, see the file LICENSE that you should have received along
+with Coquery. If not, see <http://www.gnu.org/licenses/>.
+"""
+
+from __future__ import unicode_literals
+
+from coquery.unicode import utf8
+
+from .pyqt_compat import QtCore, QtGui, get_toplevel_window
+from . import classes
+
+
+class CoqListSelect(QtGui.QWidget):
+    """
+    A QWidget that presents two exclusive list (a list of available and a
+    list of selected items), with controls to move between the two.
+    """
+    itemSelectionChanged = QtCore.Signal()
+
+    def __init__(self, *args, **kwargs):
+        from .ui import coqListSelectUi
+        super(CoqListSelect, self).__init__(*args, **kwargs)
+        self.ui = coqListSelectUi.Ui_CoqListSelect()
+        self.ui.setupUi(self)
+
+        self.ui.button_add.clicked.connect(self.add_selected)
+        self.ui.button_remove.clicked.connect(self.remove_selected)
+        self.ui.button_up.clicked.connect(self.selected_up)
+        self.ui.button_down.clicked.connect(self.selected_down)
+
+        self.ui.button_up.setIcon(get_toplevel_window().get_icon("Circled Chevron Up"))
+        self.ui.button_down.setIcon(get_toplevel_window().get_icon("Circled Chevron Down"))
+        self.ui.button_add.setIcon(get_toplevel_window().get_icon("Circled Chevron Left"))
+        self.ui.button_remove.setIcon(get_toplevel_window().get_icon("Circled Chevron Right"))
+
+        self.ui.list_selected.itemSelectionChanged.connect(self.check_buttons)
+        self.ui.list_available.itemSelectionChanged.connect(self.check_buttons)
+        self._minimum = 0
+
+    @staticmethod
+    def _fill_list_widget(w, l, translate):
+        for x in l:
+            if not isinstance(x, QtGui.QListWidgetItem):
+                item = QtGui.QListWidgetItem(translate(x))
+                item.setData(QtCore.Qt.UserRole, x)
+            else:
+                item = translate(x)
+            w.addItem(item)
+
+    def minimumItems(self):
+        return self._minimum
+
+    def setMinimumItems(self, i):
+        self._minimum = i
+
+    def selectedItems(self):
+        return [self.ui.list_selected.item(i) for i
+                in range(self.ui.list_selected.count())]
+
+    def setAvailableList(self, l, translate=lambda x: x):
+        self._fill_list_widget(self.ui.list_available, l, translate)
+
+    def setSelectedList(self, l, translate=lambda x: x):
+        self._fill_list_widget(self.ui.list_selected, l, translate)
+        self.ui.list_selected.setCurrentItem(
+            self.ui.list_selected.item(0))
+
+    def add_selected(self):
+        for x in self.ui.list_available.selectedItems():
+            row = self.ui.list_available.row(x)
+            item = self.ui.list_available.takeItem(row)
+            self.ui.list_selected.addItem(item)
+            self.ui.list_selected.setCurrentItem(item)
+            self.itemSelectionChanged.emit()
+
+    def remove_selected(self):
+        for x in self.ui.list_selected.selectedItems():
+            if self.ui.list_selected.count() > self.minimumItems():
+                row = self.ui.list_selected.row(x)
+                item = self.ui.list_selected.takeItem(row)
+                self.ui.list_available.addItem(item)
+                self.ui.list_available.setCurrentItem(item)
+                self.itemSelectionChanged.emit()
+
+    def selected_up(self):
+        self.move_selected(up=True)
+
+    def selected_down(self):
+        self.move_selected(up=False)
+
+    def move_selected(self, up):
+        pos_first = min([self.ui.list_selected.row(x) for x
+                         in self.ui.list_selected.selectedItems()])
+        if up:
+            new_pos = pos_first - 1
+        else:
+            new_pos = pos_first + 1
+        selected = [self.ui.list_selected.takeItem(pos_first) for _
+                    in self.ui.list_selected.selectedItems()]
+        for x in selected:
+            self.ui.list_selected.insertItem(new_pos, x)
+            x.setSelected(True)
+            self.ui.list_selected.setCurrentItem(x)
+
+    def check_buttons(self):
+        row = self.ui.list_selected.currentRow()
+        self.ui.button_up.setEnabled(row > 0)
+        self.ui.button_down.setEnabled(row + 1 < self.ui.list_selected.count())
+
+        self.ui.button_remove.setEnabled(row > self.minimumItems())
+        available_row = self.ui.list_available.currentRow()
+        self.ui.button_add.setEnabled(available_row != -1)
+
+    def keyPressEvent(self, event):
+        if event.key() == QtCore.Qt.Key_Left:
+            self.add_selected()
+        elif event.key() == QtCore.Qt.Key_Right:
+            self.remove_selected()
+        elif (self.ui.list_selected.hasFocus() and
+              event.key() == QtCore.Qt.Key_Up and
+              event.modifiers() == QtCore.Qt.ShiftModifier):
+            self.selected_up()
+        elif (self.ui.list_selected.hasFocus() and
+              event.key() == QtCore.Qt.Key_Down and
+              event.modifiers() == QtCore.Qt.ShiftModifier):
+            self.selected_down()
+        else:
+            super(CoqListSelect, self).keyPressEvent(event)
+
---
+++ coquery/gui/ui/coqListSelectUi.py
@@ -0,0 +1,100 @@
+# -*- coding: utf-8 -*-
+
+# Form implementation generated from reading ui file 'coqListSelect.ui'
+#
+# Created by: PyQt4 UI code generator 4.11.4
+#
+# WARNING! All changes made in this file will be lost!
+
+from coquery.gui.pyqt_compat import QtCore, QtGui, frameShadow, frameShape
+
+try:
+    _fromUtf8 = QtCore.QString.fromUtf8
+except AttributeError:
+    def _fromUtf8(s):
+        return s
+
+try:
+    _encoding = QtGui.QApplication.UnicodeUTF8
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig, _encoding)
+except AttributeError:
+    def _translate(context, text, disambig):
+        return QtGui.QApplication.translate(context, text, disambig)
+
+class Ui_CoqListSelect(object):
+    def setupUi(self, CoqListSelect):
+        CoqListSelect.setObjectName(_fromUtf8("CoqListSelect"))
+        self.verticalLayout_2 = QtGui.QVBoxLayout(CoqListSelect)
+        self.verticalLayout_2.setObjectName(_fromUtf8("verticalLayout_2"))
+        self.layout = QtGui.QGridLayout()
+        self.layout.setObjectName(_fromUtf8("layout"))
+        self.verticalLayout_5 = QtGui.QVBoxLayout()
+        self.verticalLayout_5.setObjectName(_fromUtf8("verticalLayout_5"))
+        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem)
+        self.horizontalLayout_6 = QtGui.QHBoxLayout()
+        self.horizontalLayout_6.setObjectName(_fromUtf8("horizontalLayout_6"))
+        self.button_add = QtGui.QToolButton(CoqListSelect)
+        self.button_add.setText(_fromUtf8(""))
+        self.button_add.setObjectName(_fromUtf8("button_add"))
+        self.horizontalLayout_6.addWidget(self.button_add)
+        self.verticalLayout = QtGui.QVBoxLayout()
+        self.verticalLayout.setObjectName(_fromUtf8("verticalLayout"))
+        self.button_up = QtGui.QToolButton(CoqListSelect)
+        self.button_up.setText(_fromUtf8(""))
+        self.button_up.setObjectName(_fromUtf8("button_up"))
+        self.verticalLayout.addWidget(self.button_up)
+        self.button_down = QtGui.QToolButton(CoqListSelect)
+        self.button_down.setText(_fromUtf8(""))
+        self.button_down.setObjectName(_fromUtf8("button_down"))
+        self.verticalLayout.addWidget(self.button_down)
+        self.horizontalLayout_6.addLayout(self.verticalLayout)
+        self.button_remove = QtGui.QToolButton(CoqListSelect)
+        self.button_remove.setText(_fromUtf8(""))
+        self.button_remove.setObjectName(_fromUtf8("button_remove"))
+        self.horizontalLayout_6.addWidget(self.button_remove, QtCore.Qt.AlignVCenter)
+        self.verticalLayout_5.addLayout(self.horizontalLayout_6)
+        spacerItem1 = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        self.verticalLayout_5.addItem(spacerItem1)
+        self.layout.addLayout(self.verticalLayout_5, 1, 1, 1, 1)
+        self.list_available = QtGui.QListWidget(CoqListSelect)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.list_available.sizePolicy().hasHeightForWidth())
+        self.list_available.setSizePolicy(sizePolicy)
+        self.list_available.setDragDropMode(QtGui.QAbstractItemView.DragOnly)
+        self.list_available.setObjectName(_fromUtf8("list_available"))
+        self.layout.addWidget(self.list_available, 1, 2, 1, 1)
+        self.label_available = QtGui.QLabel(CoqListSelect)
+        self.label_available.setObjectName(_fromUtf8("label_available"))
+        self.layout.addWidget(self.label_available, 0, 2, 1, 1)
+        self.label_select_list = QtGui.QLabel(CoqListSelect)
+        self.label_select_list.setObjectName(_fromUtf8("label_select_list"))
+        self.layout.addWidget(self.label_select_list, 0, 0, 1, 1)
+        self.list_selected = QtGui.QListWidget(CoqListSelect)
+        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Expanding, QtGui.QSizePolicy.Preferred)
+        sizePolicy.setHorizontalStretch(0)
+        sizePolicy.setVerticalStretch(0)
+        sizePolicy.setHeightForWidth(self.list_selected.sizePolicy().hasHeightForWidth())
+        self.list_selected.setSizePolicy(sizePolicy)
+        self.list_selected.setDragDropMode(QtGui.QAbstractItemView.InternalMove)
+        self.list_selected.setObjectName(_fromUtf8("list_selected"))
+        self.layout.addWidget(self.list_selected, 1, 0, 1, 1)
+        self.verticalLayout_2.addLayout(self.layout)
+        self.list_selected.raise_()
+        self.list_available.raise_()
+        self.list_available.raise_()
+        self.label_available.setBuddy(self.list_available)
+        self.label_select_list.setBuddy(self.list_selected)
+
+        self.retranslateUi(CoqListSelect)
+        QtCore.QMetaObject.connectSlotsByName(CoqListSelect)
+
+    def retranslateUi(self, CoqListSelect):
+        CoqListSelect.setWindowTitle(_translate("CoqListSelect", "Form", None))
+        self.label_available.setText(_translate("CoqListSelect", "&Available", None))
+        self.label_select_list.setText(_translate("CoqListSelect", "&Selected:", None))
+
+
