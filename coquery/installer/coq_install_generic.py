da63531cc7942cec7dc82cb1f898fa057622c0f0
gkunter <gero.kunter@uni-duesseldorf.de>
2017-02-21 16:23:17 +0100

    Catch all exceptions during pdf_to_str(). PEP8

--- coquery/installer/coq_install_generic.py
+++ coquery/installer/coq_install_generic.py
@@ -3,10 +3,10 @@
 """
 coq_install_generic.py is part of Coquery.
 
-Copyright (c) 2016 Gero Kunter (gero.kunter@coquery.org)
+Copyright (c) 2016, 2017 Gero Kunter (gero.kunter@coquery.org)
 
 Coquery is released under the terms of the GNU General Public License (v3).
-For details, see the file LICENSE that you should have received along 
+For details, see the file LICENSE that you should have received along
 with Coquery. If not, see <http://www.gnu.org/licenses/>.
 """
 
@@ -14,10 +14,17 @@ from __future__ import unicode_literals
 import string
 import re
 import pandas as pd
+import os
+import logging
 
-from coquery.corpusbuilder import *
 from coquery import options
-from coquery.documents import *
+from coquery.defines import NAME, FT_PDF, FT_DOCX, FT_ODT, FT_HTML, FT_PLAIN
+from coquery.corpusbuilder import BaseCorpusBuilder
+from coquery.corpusbuilder import (Column, Identifier, Link)
+from coquery.documents import (pdf_to_str, docx_to_str, odt_to_str,
+                               html_to_str, plain_to_str,
+                               detect_file_type)
+
 
 class BuilderClass(BaseCorpusBuilder):
     corpus_table = "Corpus"
@@ -48,7 +55,7 @@ class BuilderClass(BaseCorpusBuilder):
         # LemmaId
         # An int value containing the unique identifier of the lemma that
         # is associated with this word-form.
-        # 
+        #
         # Text
         # A text value containing the orthographic representation of this
         # word-form.
@@ -56,9 +63,9 @@ class BuilderClass(BaseCorpusBuilder):
         # Additionally, if NLTK is used to tag part-of-speech:
         #
         # Pos
-        # A text value containing the part-of-speech label of this 
+        # A text value containing the part-of-speech label of this
         # word-form.
-        
+
         if pos:
             self.word_pos = "POS"
             self.create_table_description(self.word_table,
@@ -80,10 +87,10 @@ class BuilderClass(BaseCorpusBuilder):
         #
         # FileId (Identifier)
         # An int value containing the unique identifier of this file.
-        # 
-        # File 
+        #
+        # File
         # A text value containing the base file name of this data file.
-        # 
+        #
         # Path
         # A text value containing the path that points to this data file.
 
@@ -92,9 +99,9 @@ class BuilderClass(BaseCorpusBuilder):
             Column(self.file_name, "VARCHAR(1024) NOT NULL"),
             Column(self.file_path, "VARCHAR(4048) NOT NULL")])
 
-        # Add the main corpus table. Each row in this table represents a 
+        # Add the main corpus table. Each row in this table represents a
         # token in the corpus. It has the following columns:
-        # 
+        #
         # TokenId (Identifier)
         # An int value containing the unique identifier of the token
         #
@@ -103,15 +110,15 @@ class BuilderClass(BaseCorpusBuilder):
         # entry associated with this token.
         #
         # FileId
-        # An int value containing the unique identifier of the data file 
+        # An int value containing the unique identifier of the data file
         # that contains this token.
-        
+
         self.create_table_description(self.corpus_table,
             [Identifier(self.corpus_id, "BIGINT UNSIGNED NOT NULL"),
-             Column(self.corpus_sentence, "INT UNSIGNED NOT NULL"), 
+             Column(self.corpus_sentence, "INT UNSIGNED NOT NULL"),
              Link(self.corpus_word_id, self.word_table),
              Link(self.corpus_file_id, self.file_table)])
-            
+
         self._sentence_id = 0
 
     @staticmethod
@@ -122,24 +129,24 @@ class BuilderClass(BaseCorpusBuilder):
     def _read_text(self, file_name):
         """
         Return the text content from the file as a string.
-        
+
         This method uses the function detect_file_type() from the documents.py
-        module in order to detect the text format type of the file. Currently 
-        supported file formats are: PDF, HTML, ODT, DOCX, and PLAIN. BINARY 
+        module in order to detect the text format type of the file. Currently
+        supported file formats are: PDF, HTML, ODT, DOCX, and PLAIN. BINARY
         files are ignored.
-        
-        If the file is a PLAIN text file, and if the 'chardet' module is 
-        installed, it is used to detect the encoding of the files. Otherwise, 
-        a PLAIN text file is first decoded assuming UTF-8, and if that fails, 
-        assuming Latin-1/ISO-8859-1. Note that this may cause wrong character 
+
+        If the file is a PLAIN text file, and if the 'chardet' module is
+        installed, it is used to detect the encoding of the files. Otherwise,
+        a PLAIN text file is first decoded assuming UTF-8, and if that fails,
+        assuming Latin-1/ISO-8859-1. Note that this may cause wrong character
         encodings in text files that use a different encoding! Using 'chardet'
         is therefore strongly recommended.
-        
+
         Parameters
         ----------
         file_name : str
-            The path to the file 
-            
+            The path to the file
+
         Returns
         -------
         raw_text : str
@@ -153,7 +160,7 @@ class BuilderClass(BaseCorpusBuilder):
             if options.use_pdfminer:
                 try:
                     raw_text = pdf_to_str(file_name)
-                except (UnicodeDecodeError, TypeError) as e:
+                except Exception as e:
                     logger.error("Error in PDF file {}: {}".format(file_name, e))
                     return ""
             else:
@@ -184,7 +191,7 @@ class BuilderClass(BaseCorpusBuilder):
                 logger.warn("Ignoring ODT file {} (the required Python module 'odtpy' is not available)".format(
                     file_name))
                 return ""
-                
+
         elif file_type == FT_HTML:
             if options.use_bs4:
                 try:
@@ -218,17 +225,17 @@ class BuilderClass(BaseCorpusBuilder):
             lemma = token_string
         else:
             try:
-                # use the current lemmatizer to assign the token to a lemma: 
+                # use the current lemmatizer to assign the token to a lemma:
                 lemma = self._lemmatize(token_string, self._pos_translate(token_pos)).lower()
-            except Exception as e:
+            except Exception:
                 lemma = token_string.lower()
-        
+
         # get word id, and create new word if necessary:
         word_dict = {self.word_lemma: lemma, self.word_label: token_string}
         if token_pos and self.arguments.use_nltk:
-            word_dict[self.word_pos] = token_pos 
+            word_dict[self.word_pos] = token_pos
         word_id = self.table(self.word_table).get_or_insert(word_dict, case=True)
-        
+
         # store new token in corpus table:
         return self.add_token_to_corpus(
             {self.corpus_word_id: word_id,
@@ -291,22 +298,22 @@ class BuilderClass(BaseCorpusBuilder):
             return False
 
     def process_file(self, file_name):
-        """ 
+        """
         Process a text file.
-        
-        This method reads the text content of the file. Several text file 
-        formats are supported (PDF, DOCX, ODT, HTML, PLAIN). 
-        
-        It first attempt to tokenize, lemmatize, and POS-tag the words in the 
-        text. If NLTK is available, it is used for that, otherwise the dumb 
-        default tokenizer/lemmatizer (which is *really* dumb, and does not 
+
+        This method reads the text content of the file. Several text file
+        formats are supported (PDF, DOCX, ODT, HTML, PLAIN).
+
+        It first attempt to tokenize, lemmatize, and POS-tag the words in the
+        text. If NLTK is available, it is used for that, otherwise the dumb
+        default tokenizer/lemmatizer (which is *really* dumb, and does not
         even attempt to create POS tags) is used.
-        
-        Then, if the token does not exist in the word table, add a new word 
-        with its lemma and POS tag to the word table. Finally, add the token 
-        with its word identifier to the corpus table, and proceed with the 
+
+        Then, if the token does not exist in the word table, add a new word
+        with its lemma and POS tag to the word table. Finally, add the token
+        with its word identifier to the corpus table, and proceed with the
         next token.
-        
+
         Parameters
         ----------
         file_name : string
@@ -317,7 +324,7 @@ class BuilderClass(BaseCorpusBuilder):
             return
 
         raw_text = self._read_text(file_name)
-            
+
         tokens = []
 
         # if possible, use NLTK for lemmatization, tokenization, and tagging:
@@ -326,16 +333,17 @@ class BuilderClass(BaseCorpusBuilder):
 
             # the WordNet lemmatizer will be used to obtain the lemma for a
             # given word:
-            self._lemmatize = lambda x,y: nltk.stem.wordnet.WordNetLemmatizer().lemmatize(x, pos=y)
-            
+            self._lemmatize = lambda x, y: nltk.stem.wordnet.WordNetLemmatizer().lemmatize(x, pos=y)
+
             # The NLTK POS tagger produces some labels that are different from
-            # the labels used in WordNet. In order to use the WordNet 
-            # lemmatizer for all words, we need a function that translates 
+            # the labels used in WordNet. In order to use the WordNet
+            # lemmatizer for all words, we need a function that translates
             # these labels:
-            self._pos_translate = lambda x: {'NN': nltk.corpus.wordnet.NOUN, 
+            self._pos_translate = lambda x: {
+                'NN': nltk.corpus.wordnet.NOUN,
                 'JJ': nltk.corpus.wordnet.ADJ,
                 'VB': nltk.corpus.wordnet.VERB,
-                'RB': nltk.corpus.wordnet.ADV} [x.upper()[:2]]
+                'RB': nltk.corpus.wordnet.ADV}[x.upper()[:2]]
 
             # Create a list of sentences from the content of the current file
             # and process this list one by one:
@@ -345,13 +353,13 @@ class BuilderClass(BaseCorpusBuilder):
                 # use NLTK tokenizer and POS tagger on this sentence:
                 tokens = nltk.word_tokenize(sentence)
                 pos_map = nltk.pos_tag(tokens)
-                    
+
                 for current_token, current_pos in pos_map:
                     # store each token:
                     self.add_token(current_token, current_pos)
         else:
-            # The default lemmatizer is pretty dumb and simply turns the 
-            # word-form to lower case so that at least 'Dogs' and 'dogs' are 
+            # The default lemmatizer is pretty dumb and simply turns the
+            # word-form to lower case so that at least 'Dogs' and 'dogs' are
             # assigned the same lemma -- which is a different lemma from the
             # one assigned to 'dog' and 'Dog'.
             #
@@ -359,14 +367,14 @@ class BuilderClass(BaseCorpusBuilder):
             # which will result in much better results.
             self._lemmatize = lambda x: x.lower()
             self._pos_translate = lambda x: x
-            
-            # use a dumb tokenizer that simply splits the file content by 
-            # spaces:            
-            
+
+            # use a dumb tokenizer that simply splits the file content by
+            # spaces:
+
             tokens = raw_text.replace("\n", " ").split(" ")
-            
+
             final_punctuation = []
-            
+
             for token in [x.strip() for x in tokens if x.strip()]:
                 # any punctuation at the beginning of the token is added to the
                 # corpus as a punctuation token, and is also stripped from the
@@ -376,18 +384,18 @@ class BuilderClass(BaseCorpusBuilder):
                     token = token[1:]
 
                 # Try to detect sentence boundaries.
-                # A sentence boundary is assumed if 
+                # A sentence boundary is assumed if
                 # (a) there is final punctuation
-                # (b) final_punctuation contains only the sentence-delimiting 
+                # (b) final_punctuation contains only the sentence-delimiting
                 #     punctuation marks .?!
                 # (c) the next word starts with a captial letter
-                
+
                 if (token and
                     final_punctuation and
                     not re.sub("[.!?'\"]", "", "".join(final_punctuation)) and
                     token[0] == token[0].upper()):
                     self._sentence_id += 1
-                    
+
                 # next, detect any word-final punctuation:
                 final_punctuation = []
                 for ch in reversed(token):
@@ -396,30 +404,26 @@ class BuilderClass(BaseCorpusBuilder):
                     else:
                         break
                 if final_punctuation == ["."]:
-                    # a single word-final full stop is considered to be a part 
-                    # of an abbreviation if there are more than one full stop 
+                    # a single word-final full stop is considered to be a part
+                    # of an abbreviation if there are more than one full stop
                     # in the token, e.g. in "U.S.A.". Otherwise, it is treated
                     # as sentence punctuation.
                     # This simple approach will also strip the full stop from
                     # abbreviations such as "Mr.".
                     if token.count(".") > 1:
                         final_punctuation = []
-                
+
                 # strip final punctuation from token:
                 if final_punctuation:
                     token = token[:-len(final_punctuation)]
-                
+
                 # add the token to the corpus:
                 if token:
                     self.add_token(token)
-                    
+
                 # add final punctuation:
                 for p in final_punctuation:
                     self.add_token(p, "PUNCT")
 
 
-def main():
-    BuilderClass().build()
-    
-if __name__ == "__main__":
-    main()
+logger = logging.getLogger(NAME)
