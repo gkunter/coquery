7cf2b5789506da3b0d0e1b371d7ec103729e5c0a
gkunter <coquery@users.strandwall.de>
2017-05-06 14:55:21 +0200

    Add test for issue #249

--- test/test_sessions.py
+++ test/test_sessions.py
@@ -11,6 +11,7 @@ import os
 from coquery.coquery import options
 from coquery.session import SessionInputFile
 from coquery.defines import QUERY_MODE_TOKENS
+from coquery.errors import TokenParseError
 
 class TestSessionInputFile(unittest.TestCase):
     def setUp(self):
@@ -50,6 +51,7 @@ class TestSessionInputFile(unittest.TestCase):
         self.write_to_temp_file(d)
 
         session = SessionInputFile()
+        session.prepare_queries()
 
         self.assertListEqual(session.header, [])
         self.assertEqual(options.cfg.query_label,
@@ -67,6 +69,7 @@ class TestSessionInputFile(unittest.TestCase):
         self.write_to_temp_file(d)
 
         session = SessionInputFile()
+        session.prepare_queries()
 
         self.assertListEqual(session.header, [])
         self.assertEqual(options.cfg.query_label,
@@ -89,6 +92,7 @@ class TestSessionInputFile(unittest.TestCase):
         self.write_to_temp_file(d)
 
         session = SessionInputFile()
+        session.prepare_queries()
 
         self.assertListEqual(session.header, [])
         self.assertEqual(options.cfg.query_label,
@@ -113,6 +117,7 @@ class TestSessionInputFile(unittest.TestCase):
         self.write_to_temp_file(d)
 
         session = SessionInputFile()
+        session.prepare_queries()
 
         self.assertListEqual(session.header, [])
         self.assertEqual(options.cfg.query_label,
@@ -133,6 +138,7 @@ class TestSessionInputFile(unittest.TestCase):
         self.write_to_temp_file(d)
 
         session = SessionInputFile()
+        session.prepare_queries()
 
         self.assertListEqual(session.header, ["DATA1", "DATA2"])
         self.assertEqual(options.cfg.query_label,
@@ -140,6 +146,21 @@ class TestSessionInputFile(unittest.TestCase):
         self.assertEqual(len(session.query_list), len(queries))
         self.assertListEqual(
             [x.query_string for x in session.query_list], queries)
+
+    def test_issue249(self):
+        options.cfg.file_has_headers = True
+        options.cfg.query_column_number = 2
+        options.cfg.skip_lines = 0
+
+        queries = ["QUERY1", "QUERY2"]
+        d = {"header": ["DATA1", "QUERY", "DATA2"],
+             "queries": ['tmp,\"#constitute .[v*]\",tmp']}
+
+        self.write_to_temp_file(d)
+
+        session = SessionInputFile()
+        with self.assertRaises(TokenParseError):
+            session.prepare_queries()
 
 def main():
     suite = unittest.TestSuite([
