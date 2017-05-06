0f7bcec88138f3617a1d1ab21b8037c37d15883f
gkunter <coquery@users.strandwall.de>
2017-05-06 13:23:49 +0200

    Fix several issues. Fix #246

--- coquery/gui/addfilters.py
+++ coquery/gui/addfilters.py
@@ -13,12 +13,18 @@ from __future__ import division
 from __future__ import unicode_literals
 
 from coquery import options
-from coquery.defines import *
+from coquery.defines import (
+    FILTER_STAGE_BEFORE_TRANSFORM, FILTER_STAGE_FINAL,
+    OPERATOR_LABELS, OP_MATCH, OP_NMATCH)
 from coquery.unicode import utf8
 from .pyqt_compat import QtCore, QtWidgets, get_toplevel_window
 from .ui.addFilterUi import Ui_FiltersDialog
 from .classes import CoqTableItem, CoqListItem
 from coquery.filters import Filter
+
+FeatureRole = QtCore.Qt.UserRole
+DtypeRole = QtCore.Qt.UserRole + 1
+FilterObjectRole = QtCore.Qt.UserRole + 2
 
 
 class FilterDialog(QtWidgets.QDialog):
@@ -27,56 +33,67 @@ class FilterDialog(QtWidgets.QDialog):
         self.ui = Ui_FiltersDialog()
         self.ui.setupUi(self)
 
-        self.ui.button_add.setIcon(get_toplevel_window().get_icon("sign-add"))
-        self.ui.button_remove.setIcon(get_toplevel_window().get_icon("sign-delete"))
-
-        for x in columns:
-            item = CoqListItem(session.translate_header(x))
-            item.setObjectName(x)
-            self.ui.list_columns.addItem(item)
-
-        self.columns = columns
-        self.dtypes = dtypes
-
-        self.ui.list_columns.setCurrentRow(0)
-
-        for filt in filter_list:
-            item = CoqTableItem(self.format_filter(filt))
-            item.setObjectName(filt)
-            i = self.ui.table_filters.rowCount()
-            self.ui.table_filters.insertRow(i)
-            self.ui.table_filters.setItem(0, i, item)
-
-        self.old_list = {filt.get_hash() for filt in filter_list}
-
-        new_filter = CoqTableItem("New filter...")
-        new_filter.setObjectName(None)
-        i = self.ui.table_filters.rowCount()
-        self.ui.table_filters.insertRow(i)
-        self.ui.table_filters.setItem(0, i, new_filter)
-
-        self.radio_operators = {
-            globals()[x.partition("_")[-1]]: x for x in dir(self.ui) if x.startswith("radio_OP_")}
-
-        self.ui.button_add.clicked.connect(self.add_filter)
-        self.ui.button_remove.clicked.connect(self.remove_filter)
-        self.ui.table_filters.clicked.connect(self.update_values)
-
-        self.ui.list_columns.currentRowChanged.connect(self.update_filter)
-        self.ui.edit_value.textChanged.connect(self.update_filter)
-        self.ui.check_before_transformation.toggled.connect(
-            self.update_filter)
-        for radio in [x for x in dir(self.ui) if x.startswith("radio_OP_")]:
-            getattr(self.ui, radio).toggled.connect(self.update_filter)
-
-        #self.ui.table_filters.selectRow(0)
-        self.ui.table_filters.selectRow(self.ui.table_filters.rowCount()-1)
-        self.update_values()
-
         try:
             self.resize(options.settings.value("filterdialog_size"))
         except TypeError:
             pass
+        self.ui.button_add.setIcon(
+            get_toplevel_window().get_icon("sign-add"))
+        self.ui.button_remove.setIcon(
+            get_toplevel_window().get_icon("sign-delete"))
+
+        assert len(columns) == len(dtypes)
+
+        self.columns = columns
+        self.dtypes = dtypes
+        self.filter_list = filter_list
+        self.session = session
+        self.old_list = {filt.get_hash() for filt in filter_list}
+
+        operator_dict = {x: globals()[x] for x in globals()
+                         if x.startswith("OP_")}
+        radio_widgets = [x for x in dir(self.ui)
+                         if x.startswith("radio_OP_")]
+        self.radio_operators = {
+            operator_dict[x.split("_", 1)[1]]: x for x in radio_widgets}
+
+        self.fill_lists()
+        self.change_filter()
+        self.setup_hooks()
+
+    def fill_lists(self):
+        for i, x in enumerate(self.columns):
+            item = CoqListItem(self.session.translate_header(x))
+            item.setData(FeatureRole, x)
+            item.setData(DtypeRole, self.dtypes[i])
+            self.ui.list_columns.addItem(item)
+        self.ui.list_columns.setCurrentRow(0)
+
+        for filt in self.filter_list:
+            item = CoqTableItem(self.format_filter(filt))
+            item.setData(FilterObjectRole, filt)
+            i = self.ui.table_filters.rowCount()
+            self.ui.table_filters.insertRow(i)
+            self.ui.table_filters.setItem(0, i, item)
+
+        new_filter = CoqTableItem("New filter...")
+        i = self.ui.table_filters.rowCount()
+        self.ui.table_filters.insertRow(i)
+        self.ui.table_filters.setItem(0, i, new_filter)
+
+        self.ui.table_filters.selectRow(self.ui.table_filters.rowCount()-1)
+
+    def setup_hooks(self):
+        self.ui.button_add.clicked.connect(self.add_filter)
+        self.ui.button_remove.clicked.connect(self.remove_filter)
+        self.ui.table_filters.clicked.connect(self.change_filter)
+
+        for signal in (self.ui.list_columns.currentRowChanged,
+                       self.ui.edit_value.textChanged,
+                       self.ui.check_before_transformation.toggled):
+            signal.connect(self.update_filter)
+        for radio in self.radio_operators.values():
+            getattr(self.ui, radio).toggled.connect(self.update_filter)
 
     def update_buttons(self, item):
         """
@@ -93,8 +110,7 @@ class FilterDialog(QtWidgets.QDialog):
         item : QTableItem
             The currently selected item
         """
-
-        filt = item.objectName()
+        filt = item.data(FilterObjectRole)
         if filt is None:
             self.ui.button_add.setEnabled(True)
             self.ui.button_remove.setDisabled(True)
@@ -109,13 +125,16 @@ class FilterDialog(QtWidgets.QDialog):
 
         new_list = set()
         for i in range(self.ui.table_filters.rowCount()):
-            filt = self.ui.table_filters.item(i, 0).objectName()
+            item = self.ui.table_filters.item(i, 0)
+            filt = item.data(FilterObjectRole)
             if filt is not None:
                 new_list.add(filt.get_hash())
+
+        ok_button = self.ui.buttonBox.button(QtWidgets.QDialogButtonBox.Ok)
         if self.old_list == new_list:
-            self.ui.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setDisabled(True)
+            ok_button.setDisabled(True)
         else:
-            self.ui.buttonBox.button(QtWidgets.QDialogButtonBox.Ok).setEnabled(True)
+            ok_button.setEnabled(True)
 
     def update_filter(self):
         """
@@ -127,19 +146,32 @@ class FilterDialog(QtWidgets.QDialog):
         the currently selected filter by a new filter initialized with the
         current dialog values.
         """
+
+        feature, dtype, operator, value, stage = self.get_values()
+        filt = Filter(feature, dtype, operator, value, stage)
+
+        # make sure that the current filter is legal:
+        try:
+            if operator not in (OP_MATCH, OP_NMATCH):
+                filt.get_filter_string()
+        except Exception as e:
+            self.ui.button_add.setDisabled(True)
+            return
+        else:
+            self.ui.button_add.setDisabled(False)
+
         row = self.ui.table_filters.currentRow()
         if row == self.ui.table_filters.rowCount() - 1:
             return
-        self.blockSignals(True)
         selected = self.ui.table_filters.item(row, 0)
-        feature, operator, value, stage = self.get_values()
-        filt = Filter(feature, operator, value, stage)
-        selected.setObjectName(filt)
+        self.blockSignals(True)
+        selected.setData(FilterObjectRole, filt)
         selected.setText(self.format_filter(filt))
         self.update_buttons(selected)
+
         self.blockSignals(False)
 
-    def update_values(self):
+    def change_filter(self):
         """
         Update the values from the dialog with the content of the currently
         selected filter.
@@ -148,35 +180,29 @@ class FilterDialog(QtWidgets.QDialog):
         is changed.
         """
         selected = self.ui.table_filters.currentItem()
+        selected_filter = selected.data(FilterObjectRole)
 
-        if selected.objectName() is not None:
-            filt = selected.objectName()
-            session = get_toplevel_window().Session
-            column_label = session.translate_header(filt.feature)
+        if selected_filter is not None:
+            # get correct operator radio button:
+            radio = getattr(self.ui,
+                            self.radio_operators[selected_filter.operator])
 
+            # get correct feature list entry:
             for i in range(self.ui.list_columns.count()):
-                if utf8(self.ui.list_columns.item(i).text()) == column_label:
-                    item = self.ui.list_columns.item(i)
+                item = self.ui.list_columns.item(i)
+                if item.data(FeatureRole) == selected_filter.feature:
                     row_number = i
 
-            radio = getattr(self.ui,
-                            self.radio_operators[filt.operator])
-            edit = self.ui.edit_value
+            self.blockSignals(True)
 
-            radio.blockSignals(True)
-            edit.blockSignals(True)
-            self.ui.list_columns.blockSignals(True)
-
+            # update GUI values:
             radio.setChecked(True)
-            edit.setText(utf8(filt.value))
+            self.ui.edit_value.setText(utf8(selected_filter.value))
+            self.ui.check_before_transformation.setChecked(
+                selected_filter.stage == FILTER_STAGE_BEFORE_TRANSFORM)
             self.ui.list_columns.setCurrentRow(row_number)
 
-            radio.blockSignals(False)
-            edit.blockSignals(False)
-            self.ui.list_columns.blockSignals(False)
-
-            self.ui.check_before_transformation.setChecked(
-                filt.stage == FILTER_STAGE_BEFORE_TRANSFORM)
+            self.blockSignals(False)
 
         self.update_buttons(selected)
 
@@ -203,23 +229,28 @@ class FilterDialog(QtWidgets.QDialog):
 
             'column' a string containing the resource feature name of the
             currently selected column.
+            'dtype' the dtype of the column
             'operator' is an integer corresponding to one of the values
             defined in ``defines.py``.
             'value' is a string containing the content of the value textedit.
             'stage' is either FILTER_STAGE_FINAL or
             FILTER_STAGE_BEFORE_TRANSFORM (defined in ``defines.py``)
         """
-        feature = utf8(self.ui.list_columns.currentItem().objectName())
+        feature = utf8(self.ui.list_columns.currentItem().data(FeatureRole))
+        dtype = self.ui.list_columns.currentItem().data(DtypeRole)
         value = utf8(self.ui.edit_value.text())
-        for x in dir(self.ui):
-            if x.startswith("radio_OP") and getattr(self.ui, x).isChecked():
-                op_label = x.partition("_")[-1]
-                operator = globals()[op_label]
+
+        # determine which operator radio button is checked:
+        for radio in self.radio_operators:
+            if getattr(self.ui, self.radio_operators[radio]).isChecked():
+                operator = globals()[radio]
+
         if self.ui.check_before_transformation.isChecked():
             stage = FILTER_STAGE_BEFORE_TRANSFORM
         else:
             stage = FILTER_STAGE_FINAL
-        return feature, operator, value, stage
+
+        return feature, dtype, operator, value, stage
 
     def add_filter(self):
         """
@@ -229,16 +260,19 @@ class FilterDialog(QtWidgets.QDialog):
         values. If the filter is added, the currently selected filter row is
         set to the 'New filter' row, and the button status is updated.
         """
-        feature, operator, value, stage = self.get_values()
-        filt = Filter(feature, operator, value, stage)
-        hashed = filt.get_hash()
-        S = set()
+        new_filt = Filter(*self.get_values())
+        hashed = new_filt.get_hash()
+
+        hashed_filters = set()
         rows = self.ui.table_filters.rowCount()
         for i in range(rows - 1):
-            S.add(self.ui.table_filters.item(0, i).objectName().get_hash())
-        if hashed not in S:
-            item = CoqTableItem(self.format_filter(filt))
-            item.setObjectName(filt)
+            item = self.ui.table_filters.item(i, 0)
+            existing_filt = item.data(FilterObjectRole)
+            hashed_filters.add(existing_filt.get_hash())
+
+        if hashed not in hashed_filters:
+            item = CoqTableItem(self.format_filter(new_filt))
+            item.setData(FilterObjectRole, new_filt)
             self.ui.table_filters.insertRow(rows - 1)
             self.ui.table_filters.setItem(rows - 1, 0, item)
             self.ui.table_filters.selectRow(rows)
@@ -272,7 +306,9 @@ class FilterDialog(QtWidgets.QDialog):
         if result == QtWidgets.QDialog.Accepted:
             l = []
             for i in range(self.ui.table_filters.rowCount() - 1):
-                filt = self.ui.table_filters.item(0, i).objectName()
+                item = self.ui.table_filters.item(i, 0)
+                print(i, item)
+                filt = item.data(FilterObjectRole)
                 ix = self.columns.tolist().index(filt.feature)
                 if self.dtypes[ix] == int:
                     filt.value = int(filt.value)
