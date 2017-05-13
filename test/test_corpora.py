d70c53d9cc259182544d422bc38411bc273072b5
gkunter <coquery@users.strandwall.de>
2017-05-13 12:29:51 +0200

    Add test for get_select_lisst()

--- test/test_corpora.py
+++ test/test_corpora.py
@@ -12,6 +12,8 @@ setup_module("options")
 from coquery.corpus import LexiconClass, BaseResource
 from coquery.coquery import options
 from coquery.defines import *
+from coquery.queries import TokenQuery
+from coquery.session import Session
 
 from coquery.gui.linkselect import Link
 
@@ -29,11 +31,11 @@ BaseResource.query_item_word = "word_label"
 
 class TestCorpus(unittest.TestCase):
     resource = BaseResource()
-    
+
     def setUp(self):
         options.cfg = argparse.Namespace()
         #options.cfg.external_links = [(Link(), "word_label")]
-    
+
     def test_is_lexical(self):
         self.assertTrue(self.resource.is_lexical("word_label"))
         self.assertTrue(self.resource.is_lexical("func.word_label"))
@@ -41,7 +43,7 @@ class TestCorpus(unittest.TestCase):
         #self.assertTrue(self.resource.is_lexical("func.cmudict.word_transcript"))
         self.assertFalse(self.resource.is_lexical("source_label"))
         self.assertFalse(self.resource.is_lexical("func.source_label"))
-        
+
     #def test_no_link(self):
         #table_structure = ice_ng.Resource.get_table_structure("word_table", ["word_label"])
         #self.assertEqual(table_structure["rc_features"], ['word_label', 'word_lemma_id', 'word_pos', 'word_transcript'])
@@ -64,6 +66,22 @@ class TestCorpus(unittest.TestCase):
         #self.assertEqual(table_structure["alias"], "COQ_CORPUS_TABLE")
         #self.assertEqual(table_structure["parent"], None)
 
+    def test_get_select_list(self):
+        options.cfg.corpus = ""
+        options.cfg.current_server = "MockConnection"
+        options.cfg.MODE = QUERY_MODE_TOKENS
+        options.cfg.context_mode = CONTEXT_NONE
+        options.cfg.selected_features = ["word_label"]
+        session = Session()
+
+        query = TokenQuery("this is a query", session)
+        session.get_max_token_count = lambda: 4
+        self.assertListEqual(
+            sorted(self.resource.get_select_list(query)),
+            sorted(["coq_word_label_1", "coq_word_label_2",
+                    "coq_word_label_3", "coq_word_label_4",
+                    "coquery_invisible_corpus_id",
+                    "coquery_invisible_number_of_tokens"]))
 
 def main():
     suite = unittest.TestSuite([
