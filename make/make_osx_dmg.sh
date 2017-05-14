7b11ee5d1a1a10fc8dad9752b87c785cfd7c9ae7
gkunter <coquery@users.strandwall.de>
2017-05-14 15:45:50 +0200

    Update output file name

--- make/make_osx_dmg.sh
+++ make/make_osx_dmg.sh
@@ -1,4 +1,4 @@
 rm -rf build dist
 pyinstaller coquery_osx.spec
 cp -a dist/Coquery/ dist/Coquery.app/Contents/MacOS/
-hdiutil create -srcfolder dist/Coquery.app dist/coquery-0.9.1-osx-el-capitan
+hdiutil create -srcfolder dist/Coquery.app dist/coquery-0.10.0-macos-sierra
