2dc9e31c91304646d0da6502e7c3ab3a0f2e8f4e
gkunter <gero.kunter@uni-duesseldorf.de>
2017-05-17 21:08:10 +0200

    Catch RuntimeError exceptions. Fix resource tree check states.

--- coquery/gui/app.py
+++ coquery/gui/app.py
@@ -1421,13 +1421,15 @@ class CoqMainWindow(QtWidgets.QMainWindow):
             pass
 
     def toggle_selected_feature(self, item):
-        is_checked = (item.checkState(0) == QtCore.Qt.Checked)
         rc_feature = utf8(item.objectName())
         if rc_feature and not rc_feature.endswith("_table"):
-            if is_checked:
+            if (item.checkState(0) == QtCore.Qt.Checked):
                 self.selected_features.add(rc_feature)
-            else:
-                self.selected_features.remove(rc_feature)
+            elif (item.checkState(0) == QtCore.Qt.Unchecked):
+                try:
+                    self.selected_features.remove(rc_feature)
+                except KeyError:
+                    pass
 
     def fill_combo_corpus(self):
         """
@@ -1765,6 +1767,12 @@ class CoqMainWindow(QtWidgets.QMainWindow):
 
     def exception_during_query(self):
         if not self.terminating:
+            if isinstance(self.exception, RuntimeError):
+                QtWidgets.QMessageBox.critical(
+                    self,
+                    "Error during execution – Coquery",
+                    str(self.exception),
+                    QtWidgets.QMessageBox.Ok, QtWidgets.QMessageBox.Ok)
             if isinstance(self.exception, UnsupportedQueryItemError):
                 QtWidgets.QMessageBox.critical(self, "Error in query string – Coquery", str(self.exception), QtWidgets.QMessageBox.Ok, QtWidgets.QMessageBox.Ok)
             else:
