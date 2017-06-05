2571576673672612ffa7710e6aa727370c24169e
gkunter <coquery@users.strandwall.de>
2017-06-05 20:20:48 +0200

    Make summary functions a session feature

--- coquery/session.py
+++ coquery/session.py
@@ -97,7 +97,7 @@ class Session(object):
         self.column_functions = functionlist.FunctionList()
         # Summary functions are functions that the user specified to be
         # applied after the summary
-        self.summary_group = managers.Summary("summary")
+        self.summary_group = options.cfg.summary_group[0]
 
         # row_visibility stores for each query type a pandas Series object
         # with the same index as the respective output object, and boolean
