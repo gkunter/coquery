dd07b77678ae0f5ebcb6f06bd5349d106fbbd693
gkunter <coquery@users.strandwall.de>
2017-09-30 22:00:49 +0200

    Revamp function selection interface. Fix #261

--- coquery/gui/groups.py
+++ coquery/gui/groups.py
@@ -17,9 +17,10 @@ from coquery.defines import FUNCTION_DESC
 from coquery.unicode import utf8
 from coquery.managers import Group, Summary
 from coquery.functions import (
-                    #FilteredRows, PassingRows,
+                    get_base_func,
                     Entropy,
-                    #SuperCondProb, ExternalCondProb,
+                    Min, Max, Mean, Median, StandardDeviation,
+                    InterquartileRange,
                     Freq, FreqNorm,
                     FreqPTW, FreqPMW,
                     ReferenceCorpusSize,
@@ -35,107 +36,77 @@ from coquery.functions import (
                     CorpusSize, SubcorpusSize)
 
 from .pyqt_compat import QtWidgets, QtGui, QtCore, get_toplevel_window
-from .classes import CoqClickableLabel
-from .ui.groupDialogUi import Ui_GroupDialog
 from .listselect import SelectionDialog
+from .ui import groupFunctionWidgetUi
 
 
-class FunctionWidget(QtWidgets.QWidget):
+class GroupFunctionWidget(QtWidgets.QWidget):
     """
-    A FunctionWidget is a widget that manages a group function.
+    A GroupFunctionWidget is a widget that manages a group function.
 
     It stores a check state, a function class type, and a list of columns.
     """
+    dataChanged = QtCore.Signal()
+    columnsClicked = QtCore.Signal()
+    lostFocus = QtCore.Signal()
 
-    def __init__(self, cls, checked, columns, available, *args, **kwargs):
-        super(FunctionWidget, self).__init__(*args, **kwargs)
+    def __init__(self, cls, *args, **kwargs):
+        super(GroupFunctionWidget, self).__init__(*args, **kwargs)
+        self.ui = groupFunctionWidgetUi.Ui_GroupFunctionWidget()
+        self.ui.setupUi(self)
+
+        for signal in (self.ui.checkbox.stateChanged, ):
+            signal.connect(self.fireDataChanged)
+
+        self.ui.button_columns.clicked.connect(self.selectColumns)
+
+        margins = self.ui.gridLayout.contentsMargins()
+        margins.setRight(
+            margins.right() +
+            QtWidgets.QScrollBar(QtCore.Qt.Vertical).sizeHint().width())
+        self.ui.gridLayout.setContentsMargins(margins)
+
+        self._columns = []
+        self._available_columns = []
         self._function_class = cls
-        self._columns = columns
-        self._available_columns = available
-        self.setupUi()
-        self._update_text()
-        self.setCheckState(checked)
-        self.checkbox.toggled.connect(self.change_highlight)
-        self.change_highlight()
 
-    def setupUi(self):
-        self.main_layout = QtWidgets.QGridLayout(self)
-        self.main_layout.setSpacing(4)
+        desc = FUNCTION_DESC.get(cls._name, "(no description available)")
+        self.ui.label_title.setText(
+            "{}<br><span style='font-size: small;'>{}</span>".format(
+                cls.get_name(), desc))
 
-        self.column_text = QtWidgets.QLabel()
+        self.hideArguments()
 
-        # use smaller font for the label that displays the selected columns:
-        font = self.column_text.font()
-        font.setPointSize(font.pointSize() * 0.8)
-        self.column_text.setFont(font)
+    def fireDataChanged(self, *args, **kwargs):
+        self.dataChanged.emit()
 
-        self.column_text.setWordWrap(True)
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding,
-                                           QtWidgets.QSizePolicy.Expanding)
-        self.column_text.setSizePolicy(sizePolicy)
-        self.column_text.setStyleSheet("padding-left: 3px; ")
+    def content(self):
+        return [self.functionClass(),
+                self.columns(),
+                self.availableColumns(),
+                self.checkState()]
 
-        self.checkbox = QtWidgets.QCheckBox()
-        desc = FUNCTION_DESC.get(self.functionClass()._name,
-                                 "no description available")
-        self.function_label = CoqClickableLabel(
-            "{} – <span style='font-size: small;'>{}</span>".format(
-                self.functionClass().get_name(), desc))
-        self.function_label.setWordWrap(True)
-        self.function_label.clicked.connect(self.checkbox.toggle)
+    def setContent(self, data):
+        cls, columns, available, checkstate = data
+        print(cls, cls.maximum_columns)
+        if (cls.maximum_columns == 0):
+            self.ui.button_columns.hide()
+            self.ui.label_columns.hide()
+        self.setColumns(columns)
+        self.ui.checkbox.setCheckState(checkstate)
+        self.ui.button_columns.setDisabled(len(columns + available) == 0)
 
-        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Expanding,
-                                           QtWidgets.QSizePolicy.Preferred)
-        self.function_label.setSizePolicy(sizePolicy)
+    def hideArguments(self):
+        self.ui.button_columns.hide()
 
-        self.main_layout.addWidget(self.checkbox, 0, 0)
-        self.main_layout.addWidget(self.function_label, 0, 1)
-        self.main_layout.addWidget(self.column_text, 1, 1, 1, 2)
+    def showArguments(self):
+        self.ui.button_columns.show()
 
-        if (self.functionClass().maximum_columns is None or
-                self.functionClass().maximum_columns > 0):
-            self.button = QtWidgets.QPushButton("Change columns...")
-            self.button.clicked.connect(self.selectColumns)
-            self.main_layout.addWidget(self.button, 0, 2)
-            l = self._columns + self._available_columns
-            if len(l) == 0:
-                self.button.setDisabled(True)
-
-        h = self.function_label.sizeHint().height() + 1
-        self.function_label.setMinimumHeight(h)
-        self.function_label.setMaximumHeight(h)
-
-    def change_highlight(self):
-        palette = QtWidgets.QApplication.instance().palette()
-
-        if self.isChecked():
-            style_sheet = """
-                CoqClickableLabel {{ background-color: {0};
-                                     color: {1};
-                                     padding: 2px;
-                                     border: 1px inset {2}; }}
-                CoqClickableLabel::hover {{
-                        padding: 2px;
-                        border: 1px solid {2}; }}
-                """.format(
-                    palette.color(QtGui.QPalette.Highlight).name(),
-                    palette.color(QtGui.QPalette.HighlightedText).name(),
-                    palette.color(QtGui.QPalette.HighlightedText).name())
-        else:
-            style_sheet = """
-                CoqClickableLabel {{ padding: 2px; }}
-                CoqClickableLabel::hover {{
-                        padding: 2px;
-                        border: 1px solid {0}; }}
-                """.format(
-                    palette.color(QtGui.QPalette.Highlight).name())
-        self.setStyleSheet(style_sheet)
-
-    def isChecked(self):
-        return self.checkbox.isChecked()
+    def checkState(self):
+        return self.ui.checkbox.checkState()
 
     def setCheckState(self, check):
-        self.checkbox.setCheckState(check)
+        self.ui.checkbox.setCheckState(check)
 
     def functionClass(self):
         return self._function_class
@@ -150,40 +121,139 @@ class FunctionWidget(QtWidgets.QWidget):
         return self._available_columns
 
     def setColumns(self, columns):
-        new_avail = []
-        new_columns = []
-
-        for col in self._columns + self._available_columns:
-            if col in columns:
-                new_columns.append(col)
-            else:
-                new_avail.append(col)
-
-        self._columns = new_columns
-        self._available_columns = new_avail
-        self._update_text()
-
-    def _update_text(self):
+        self._columns = columns
+        template = "<span style='font-size: small;'>{}</span>"
         if self.columns():
-            cols = [get_toplevel_window().Session.translate_header(x)
-                    for x in self.columns()]
-            S = "Columns: {}".format(", ".join(cols))
+            try:
+                cols = [get_toplevel_window().Session.translate_header(x)
+                        for x in columns]
+            except AttributeError:
+                cols = columns
+            S = template.format(", ".join(cols))
         else:
-            S = ""
-        self.column_text.setText(S)
+            S = "(none)"
+        self.ui.label_columns.setText(S)
+
+    def setAvailableColumns(self, columns):
+        self._available_columns = columns
 
     def selectColumns(self):
-        selected = SelectionDialog.show(
+        try:
+            translator = get_toplevel_window().Session.translate_header
+        except AttributeError:
+            translator = None
+
+        available = self.availableColumns()
+        columns = self.columns()
+        selected, available = SelectionDialog.show(
             "Column selection – Coquery",
-            self.columns(), self.availableColumns(),
-            translator=get_toplevel_window().Session.translate_header,
+            columns, available,
+            translator=translator,
             parent=self)
         self.setColumns(selected)
+        self.setAvailableColumns(available)
+        self.fireDataChanged()
+
+    def event(self, ev, *args, **kwargs):
+        return super(GroupFunctionWidget, self).event(ev, *args, **kwargs)
+
+
+class GroupFunctionDelegate(QtWidgets.QItemDelegate):
+    """
+    This delegate is used to display a GroupFunctionWidget in a QListView or
+    similar views.
+    """
+    def __init__(self, *args, **kwargs):
+        super(GroupFunctionDelegate, self).__init__(*args, **kwargs)
+        self._editor = None
+
+    def editor(self):
+        return self._editor
+
+    def paint(self, painter, option, index):
+        painter.save()
+        data = self._get_data(index)
+        cls, columns, available, checkstate = data
+
+        widget = GroupFunctionWidget(cls)
+        widget.setContent(data)
+        widget.setMaximumWidth(self.parent().size().width())
+        widget.setMinimumWidth(self.parent().size().width())
+
+        rect = option.rect
+        rect.adjust(0, 0, 0, 0)
+        if (option.state & QtWidgets.QStyle.State_Selected):
+            painter.fillRect(rect, option.palette.highlight())
+
+        paint_widget = True
+        if self._editor is not None:
+            paint_widget = False
+            try:
+                if self._editor.isHidden():
+                    paint_widget = True
+            except RuntimeError:
+                paint_widget = True
+
+        if (not (option.state & QtWidgets.QStyle.State_Selected) or
+                paint_widget):
+            painter.translate(rect.topLeft())
+            widget.render(painter, QtCore.QPoint(), QtGui.QRegion(),
+                          widget.DrawChildren)
+
+        painter.restore()
+
+    def createEditor(self, parent, option, index):
+        data = self._get_data(index)
+        cls, columns, available, checkstate = data
+        editor = GroupFunctionWidget(cls, parent=parent)
+        editor.setMaximumWidth(self.parent().size().width())
+        editor.setMinimumWidth(self.parent().size().width())
+        editor.setContent(data)
+        editor.showArguments()
+        self._editor = editor
+        self._index = index
+        self._editor.dataChanged.connect(self.pushData)
+        return editor
+
+    def pushData(self):
+        item = self.parent().model().item(self._index.row(),
+                                          self._index.column())
+        item.setData(self._editor.content(), QtCore.Qt.UserRole)
+
+    def setEditorData(self, editor, index):
+        model = self.parent().model()
+        data = model.data(index, QtCore.Qt.UserRole)
+        editor.setContent(data)
+
+    def setModelData(self, editor, model, index):
+        cls = editor.functionClass()
+        columns = editor.columns()
+        available = editor.availableColumns()
+        checkstate = editor.checkState()
+        data = [cls, columns, available, checkstate]
+        model.itemFromIndex(index).setData(data, QtCore.Qt.UserRole)
+        self._editor = None
+
+    def _get_data(self, index):
+        data = self.parent().model().data(index, QtCore.Qt.UserRole)
+        return data
+
+    def sizeHint(self, item, index):
+        data = self._get_data(index)
+        cls, columns, available, checkstate = data
+        widget = GroupFunctionWidget(cls, parent=self.parent())
+        widget.setContent(data)
+        widget.showArguments()
+        widget.setMaximumWidth(self.parent().sizeHint().width())
+        widget.setMinimumWidth(self.parent().sizeHint().width())
+        return widget.sizeHint()
 
 
 class GroupDialog(QtWidgets.QDialog):
     function_list = (Freq, FreqNorm, FreqPTW, FreqPMW,
                      RowNumber, Tokens, Types, TypeTokenRatio,
+                     Min, Max, Mean, Median, StandardDeviation,
+                     InterquartileRange,
                      ReferenceCorpusLLKeyness,
                      ReferenceCorpusDiffKeyness,
                      SubcorpusSize,
@@ -191,14 +261,9 @@ class GroupDialog(QtWidgets.QDialog):
 
     def __init__(self, group, all_columns, parent=None):
         super(GroupDialog, self).__init__(parent)
+        from .ui.groupDialogUi import Ui_GroupDialog
         self.ui = Ui_GroupDialog()
         self.ui.setupUi(self)
-
-        self.ui.scroll_layout.setSpacing(0)
-        palette = QtWidgets.QApplication.instance().palette()
-        self.ui.scroll_content.setStyleSheet("""
-            background-color: {}; """.format(
-                palette.color(QtGui.QPalette.Base).name()))
 
         # Remove group function columns as they may not be available yet
         # when the group is formed.
@@ -222,25 +287,67 @@ class GroupDialog(QtWidgets.QDialog):
         function_columns = {fnc_class: columns
                             for fnc_class, columns in group.functions}
 
-        # add function widgets:
-        for x in sorted(self.function_list, key=lambda x: x.get_name()):
-            # check if this function needs and uses columns:
-            if (x.maximum_columns is None or x.maximum_columns > 0):
-                cols = function_columns.get(x, selected_columns)
-            else:
-                cols = []
+        delegate = GroupFunctionDelegate()
+        self.ui.linked_functions.setListDelegate(delegate)
 
-            function_widget = FunctionWidget(
-                x, False,
-                cols,
-                [x for x in all_columns if x not in cols])
+        function_groups = self.get_function_groups()
 
-            self.ui.scroll_layout.addWidget(function_widget)
-            if x in function_columns:
-                function_widget.setCheckState(QtCore.Qt.Checked)
+        for fun_base in sorted(function_groups,
+                               key=lambda x: x.get_description()):
+
+            group_item = QtWidgets.QListWidgetItem(
+                fun_base.get_description())
+            function_items = []
+            for fnc in function_groups[fun_base]:
+                list_item = QtGui.QStandardItem()
+                if fnc in function_columns:
+                    columns = function_columns[fnc]
+                    available = [x for x in all_columns if x not in columns]
+                    check = QtCore.Qt.Checked
+                else:
+                    columns = all_columns
+                    available = []
+                    check = QtCore.Qt.Unchecked
+                list_item.setData([fnc, columns, available, check],
+                                  QtCore.Qt.UserRole)
+                function_items.append(list_item)
+
+            self.ui.linked_functions.addList(group_item, function_items)
+
+        self.ui.linked_functions.setCurrentCategoryRow(0)
 
     def setup_values(self, group):
         self.ui.edit_label.setText(group.name)
+
+    def get_function_groups(self):
+        """
+        Return a dictionary with base function classes as keys and lists of
+        function classes as values. This dictionary can be used to populate
+        the class list and function list of the dialog.
+        """
+        function_groups = {}
+        for fun in self.function_list:
+            fun_base = get_base_func(fun)
+            if fun_base not in function_groups:
+                function_groups[fun_base] = []
+            function_groups[fun_base].append(fun)
+        for fun_base in function_groups:
+            function_groups[fun_base] = sorted(function_groups[fun_base],
+                                               key=lambda x: x._name)
+        return function_groups
+
+    def get_selected_items(self):
+        selected = []
+        for i in range(self.ui.linked_functions.listCount()):
+            sublist = self.ui.linked_functions.list_(i)
+            for row in range(sublist.rowCount()):
+                item = sublist.item(row, 0)
+                data = item.data(QtCore.Qt.UserRole)
+                cls, columns, available, checkstate = data
+                print(cls, checkstate)
+                if checkstate == QtCore.Qt.Checked:
+                    selected.append((cls, columns))
+        return selected
 
     def exec_(self, *args, **kwargs):
         result = super(GroupDialog, self).exec_(*args, **kwargs)
@@ -248,11 +355,7 @@ class GroupDialog(QtWidgets.QDialog):
             name = utf8(self.ui.edit_label.text())
             columns = [x.data(QtCore.Qt.UserRole)
                        for x in self.ui.widget_selection.selectedItems()]
-            functions = []
-            for i in range(self.ui.scroll_layout.count()):
-                item = self.ui.scroll_layout.itemAt(i).widget()
-                if item.isChecked():
-                    functions.append((item.functionClass(), item.columns()))
+            functions = self.get_selected_items()
             show_distinct = bool(self.ui.radio_remove_duplicates.isChecked())
 
             group = Group(name, columns, functions, show_distinct)
@@ -275,11 +378,10 @@ class GroupDialog(QtWidgets.QDialog):
         result = dialog.exec_()
         return result
 
+
 class SummaryDialog(GroupDialog):
     function_list = (
-                    #FilteredRows, PassingRows,
                     Entropy,
-                    #SuperCondProb, ExternalCondProb,
                     Freq, FreqNorm,
                     FreqPTW, FreqPMW,
                     ReferenceCorpusSize,
@@ -294,7 +396,6 @@ class SummaryDialog(GroupDialog):
                     TypeTokenRatio,
                     CorpusSize, SubcorpusSize)
 
-
     def __init__(self, group, all_columns, parent=None):
         super(SummaryDialog, self).__init__(group, all_columns, parent)
         self.ui.label.hide()
@@ -303,12 +404,11 @@ class SummaryDialog(GroupDialog):
         self.ui.radio_keep_duplicates.hide()
         self.ui.widget_selection.hide()
         self.ui.edit_label.hide()
-        scroll_area = self.ui.verticalLayout.takeAt(0).widget()
+        function_list = self.ui.horizontalLayout_2.takeAt(0).widget()
         self.ui.tabWidget.hide()
-        self.ui.gridLayout.addWidget(scroll_area, 0, 0)
+        self.ui.gridLayout.addWidget(function_list, 0, 0)
         self.ui.gridLayout.setColumnStretch(0, 1)
         self.ui.gridLayout.setColumnStretch(1, 0)
-
 
     @staticmethod
     def edit(group, all_columns, parent=None):
@@ -319,4 +419,3 @@ class SummaryDialog(GroupDialog):
         if group:
             return Summary(name=group.name, functions=group.functions,
                            columns=all_columns, distinct=group.show_distinct)
-
