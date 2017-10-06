25867fc1c31158fd2a62b72c3a44f3860aaffe24
gkunter <gero.kunter@uni-duesseldorf.de>
2017-10-06 19:58:14 +0200

    Add CoqWidgetFader class

--- coquery/gui/classes.py
+++ coquery/gui/classes.py
@@ -12,6 +12,7 @@ with Coquery. If not, see <http://www.gnu.org/licenses/>.
 from __future__ import unicode_literals
 from __future__ import print_function
 from __future__ import absolute_import
+from __future__ import division
 
 import logging
 import os
@@ -405,6 +406,45 @@ class CoqClickableLabel(QtWidgets.QLabel):
     def setText(self, s):
         super(CoqClickableLabel, self).setText(s)
         self.textChanged.emit()
+
+class CoqWidgetFader(QtCore.QObject):
+    def __init__(self, widget, duration=250):
+        def blend(left, right, i, steps):
+            x = (steps - i - 1) / steps * left
+            y = i / steps * right
+            return int(x + y)
+
+        self._widget = widget
+
+        app = QtWidgets.QApplication.instance()
+        start = app.palette().color(QtGui.QPalette.Normal,
+                                    QtGui.QPalette.Highlight)
+        end = widget.palette().color(widget.backgroundRole())
+        print(widget, end.name())
+        print(widget.styleSheet())
+        self._pal = []
+
+        steps = 128
+
+        for i in range(steps):
+            blended = (blend(start.red(), end.red(), i, steps),
+                       blend(start.green(), end.green(), i, steps),
+                       blend(start.blue(), end.blue(), i, steps))
+            self._pal.append("#{:02x}{:02x}{:02x}".format(*blended))
+
+        self._delay = duration / steps
+
+    def fade(self):
+        self.fade_widget(0)
+
+    def fade_widget(self, step):
+        s = "background-color: '{}'".format(self._pal[step])
+        self._widget.setStyleSheet(s)
+        if step < len(self._pal) - 1:
+            QtCore.QTimer.singleShot(self._delay,
+                                     lambda: self.fade_widget(step + 1))
+        else:
+            self._widget.setStyleSheet("")
 
 
 class CoqSwitch(QtWidgets.QWidget):
