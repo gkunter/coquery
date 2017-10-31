d090d823aec36c864afce1f504a229beb5eaf86e
gkunter <coquery@users.strandwall.de>
2017-10-31 20:20:36 +0100

    Switch to branch 'preview'

--- make/make_win_installer.bat
+++ make/make_win_installer.bat
@@ -1,4 +1,4 @@
-rmdir "%HOMEPATH%\coquery-release\make\build" /s /q
-rmdir "%HOMEPATH%\coquery-release\make\dist" /s /q
+rmdir "%HOMEPATH%\coquery-preview\make\build" /s /q
+rmdir "%HOMEPATH%\coquery-preview\make\dist" /s /q
 pyinstaller coquery_win.spec
 "C:\Program Files\Inno Setup 5\Compil32.exe" /cc coquery.iss
