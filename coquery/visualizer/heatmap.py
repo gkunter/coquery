25198c3773b0747e1215bc2b5317c2f7aa7381fc
gkunter <coquery@users.strandwall.de>
2018-01-06 13:44:35 +0100

    Remove obsolete code

--- coquery/visualizer/heatmap.py
+++ coquery/visualizer/heatmap.py
@@ -2,7 +2,7 @@
 """
 heatmap.py is part of Coquery.
 
-Copyright (c) 2016, 2017 Gero Kunter (gero.kunter@coquery.org)
+Copyright (c) 2016-2018 Gero Kunter (gero.kunter@coquery.org)
 
 Coquery is released under the terms of the GNU General Public License (v3).
 For details, see the file LICENSE that you should have received along
@@ -11,8 +11,6 @@ with Coquery. If not, see <http://www.gnu.org/licenses/>.
 
 import seaborn as sns
 import pandas as pd
-import matplotlib.pyplot as plt
-from matplotlib.colors import ListedColormap
 
 from coquery.visualizer import visualizer as vis
 from coquery.gui.pyqt_compat import QtWidgets
@@ -35,15 +33,11 @@ def _annotate_heatmap(self, ax, mesh):
                                        self.annot_data.flat):
             print(m, pd.np.ma.masked)
             if m is not pd.np.ma.masked:
-                print(1)
                 _, l, _ = colorsys.rgb_to_hls(*color[:3])
                 text_color = ".15" if l > .408 else "w"
-                print(l, text_color)
                 annotation = ("{:" + self.fmt + "}").format(val)
-                print(annotation)
                 text_kwargs = dict(color=text_color, ha="center", va="center")
                 text_kwargs.update(self.annot_kws)
-                print(text_kwargs)
                 ax.text(x, y, annotation, **text_kwargs)
     except Exception as e:
         print(e)
@@ -52,74 +46,6 @@ def _annotate_heatmap(self, ax, mesh):
 if sns.__version__ < "0.7.0" or True:
     sns.matrix._HeatMapper._annotate_heatmap = _annotate_heatmap
 
-
-class Visualizer(vis.BaseVisualizer):
-    dimensionality=2
-
-    def setup_figure(self):
-        with sns.axes_style("white"):
-            super(Visualizer, self).setup_figure()
-
-    def set_defaults(self):
-        self.options["color_palette"] = "RdPu"
-        super(Visualizer, self).set_defaults()
-
-        if len(self._groupby) == 2:
-            self.options["label_y_axis"] = self._groupby[0]
-            self.options["label_legend"] = self._groupby[1]
-        else:
-            self.options["label_legend"] = self._groupby[0]
-
-    def draw(self):
-        """ Draw a heat map. """
-
-        def get_crosstab(data, row_fact,col_fact, row_names, col_names):
-            ct = pd.crosstab(data[row_fact], data[col_fact])
-            ct = ct.reindex_axis(row_names, axis=0).fillna(0)
-            ct = ct.reindex_axis(col_names, axis=1).fillna(0)
-            return ct
-
-        def plot(data, color):
-            ct = get_crosstab(
-                    data,
-                    self._groupby[0],
-                    self._groupby[1],
-                    self._levels[0],
-                    self._levels[1])
-
-            sns.heatmap(ct,
-                robust=True,
-                annot=True,
-                cbar=False,
-                cmap=cmap,
-                fmt="g",
-                vmax=vmax,
-                #ax=plt.gca(),
-                linewidths=1)
-
-        if len(self._groupby) < 2:
-            # create a dummy cross tab with one dimension containing empty
-            # values:
-            data_column = self._table[self._groupby[0]].reset_index(drop=True)
-            tab = pd.crosstab(
-                pd.Series([""] * len(data_column), name=""),
-                data_column)
-            plot_facet = lambda data, color: sns.heatmap(
-                tab,
-                robust=True,
-                annot=True,
-                cbar=False,
-                cmap=cmap,
-                fmt="g",
-                linewidths=1)
-        else:
-            plot_facet = plot
-            vmax = pd.crosstab(
-                [self._table[x] for x in [self._row_factor, self._groupby[0]] if x != None],
-                [self._table[x] for x in [self._col_factor, self._groupby[1]] if x != None]).values.max()
-
-        cmap = ListedColormap(self.options["color_palette_values"])
-        self.map_data(plot_facet)
 
 class Heatmap(vis.Visualizer):
     normalization = 0
@@ -215,13 +141,12 @@ class Heatmap(vis.Visualizer):
             fmt = "g"
 
         sns.heatmap(ct.fillna(0),
-            robust=True,
-            annot=True,
-            cbar=False,
-            cmap=cmap,
-            fmt=fmt,
-            #vmax=vmax,
-            linewidths=1)
+                    robust=True,
+                    annot=True,
+                    cbar=False,
+                    cmap=cmap,
+                    fmt=fmt,
+                    linewidths=1)
 
     def get_custom_widgets(self, *args, **kwargs):
         layout = QtWidgets.QHBoxLayout()
