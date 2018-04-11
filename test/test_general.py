19e747f5fe7dde25191c795a768e6a26957e0fde
gkunter <coquery@users.strandwall.de>
2018-04-11 20:22:46 +0200

    Add pretty() tests

--- test/test_general.py
+++ test/test_general.py
@@ -14,8 +14,9 @@ import unittest
 import sys
 import tempfile
 import os
+import numpy as np
 
-from coquery.general import check_fs_case_sensitive
+from coquery.general import check_fs_case_sensitive, pretty
 
 
 class TestGeneral(unittest.TestCase):
@@ -28,8 +29,126 @@ class TestGeneral(unittest.TestCase):
         else:
             raise NotImplementedError
 
+class TestPretty(unittest.TestCase):
+    def assertListEqual(self, l1, l2):
+        if (any([type(x) == object for x in l1]) or
+                any([type(x) == object for x in l2])):
+            return super(TestPretty, self).assertListEqual(l1, l2)
+        else:
+            if not all(np.equal(l1, l2)):
+                raise self.failureException(
+                    "Lists not equal enough:\n\t{}\n\t{}".format(l1, l2))
+            return all(np.equal(l1, l2))
 
-provided_tests = [TestGeneral]
+    def test_pretty_1(self):
+        vrange = (0, 100)
+        bins = pretty(vrange, 5)
+        self.assertListEqual(list(bins),
+                             [0, 20, 40, 60, 80])
+
+    def test_pretty_1a(self):
+        vrange = (5, 95)
+        bins = pretty(vrange, 5)
+        self.assertListEqual(list(bins),
+                             [0, 20, 40, 60, 80])
+
+    def test_pretty_2(self):
+        vrange = (0, 100)
+        bins = pretty(vrange, 4)
+        self.assertListEqual(list(bins),
+                             [0, 25, 50, 75])
+
+    def test_pretty_2a(self):
+        vrange = (5, 95)
+        bins = pretty(vrange, 4)
+        self.assertListEqual(list(bins),
+                             [0, 25, 50, 75])
+
+    def test_pretty_3(self):
+        vrange = (0, 1000)
+        bins = pretty(vrange, 10)
+        self.assertListEqual(list(bins),
+                             [0, 100, 200, 300, 400, 500, 600, 700, 800, 900])
+
+    def test_pretty_3a(self):
+        vrange = (5, 995)
+        bins = pretty(vrange, 10)
+        self.assertListEqual(list(bins),
+                             [0, 100, 200, 300, 400, 500, 600, 700, 800, 900])
+
+    def test_pretty_3b(self):
+        vrange = (50, 950)
+        bins = pretty(vrange, 10)
+        self.assertListEqual(list(bins),
+                             [0, 100, 200, 300, 400, 500, 600, 700, 800, 900])
+
+    def test_pretty_4(self):
+        vrange = (-200, 200)
+        bins = pretty(vrange, 4)
+        self.assertListEqual(list(bins),
+                             [-200, -100, 0, 100])
+
+    def test_pretty_4a(self):
+        vrange = (-195, 195)
+        bins = pretty(vrange, 4)
+        self.assertListEqual(list(bins),
+                             [-200, -100, 0, 100])
+
+    def test_pretty_5(self):
+        vrange = (-200, -100)
+        bins = pretty(vrange, 4)
+        self.assertListEqual(list(bins),
+                             [-200, -175, -150, -125])
+
+    def test_pretty_6(self):
+        vrange = (100, 200)
+        bins = pretty(vrange, 4)
+        self.assertListEqual(list(bins),
+                             [100, 125, 150, 175])
+
+    def test_pretty_7(self):
+        vrange = (0, 10)
+        bins = pretty(vrange, 5)
+        self.assertListEqual(list(bins),
+                             [0, 2, 4, 6, 8])
+
+    def test_pretty_8(self):
+        vrange = (0, 10)
+        bins = pretty(vrange, 4)
+        self.assertListEqual(list(bins),
+                             [0, 2.5, 5, 7.5])
+
+    def test_pretty_9(self):
+        vrange = (0, 1)
+        bins = pretty(vrange, 4)
+        self.assertListEqual(list(bins),
+                             [0, 0.25, 0.5, 0.75])
+
+    def test_pretty_10(self):
+        vrange = (0.5, 1)
+        bins = pretty(vrange, 4)
+        self.assertListEqual(list(bins),
+                             [0.5, 0.625, 0.75, 0.875])
+
+    def test_pretty_10a(self):
+        vrange = (0.05, 0.10)
+        bins = pretty(vrange, 4)
+        self.assertListEqual(list(bins),
+                             [0.0, 0.025, 0.05, 0.075])
+
+    def test_pretty_11(self):
+        vrange = (13452, 32787)
+        bins = pretty(vrange, 2)
+        self.assertListEqual(list(bins),
+                             [13000, 23000])
+
+    def test_pretty_12(self):
+        vrange = (0, 136)
+        bins = pretty(vrange, 2)
+        self.assertListEqual(list(bins),
+                             [0, 75])
+
+provided_tests = [TestGeneral, TestPretty]
 
 
 def main():
