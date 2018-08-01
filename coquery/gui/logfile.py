b94423f29d91f4fbb9b60ee5fc9117c384bd1a06
gkunter <github@strandwall.de>
2018-08-01 18:07:54 +0200

    PEP8

--- coquery/gui/logfile.py
+++ coquery/gui/logfile.py
@@ -2,7 +2,7 @@
 """
 logfile.py is part of Coquery.
 
-Copyright (c) 2016, 2017 Gero Kunter (gero.kunter@coquery.org)
+Copyright (c) 2016-2018 Gero Kunter (gero.kunter@coquery.org)
 
 Coquery is released under the terms of the GNU General Public License (v3).
 For details, see the file LICENSE that you should have received along
@@ -18,18 +18,20 @@ from . import classes
 from .pyqt_compat import QtCore, QtWidgets
 from .ui.logfileUi import Ui_logfileDialog
 
-button_map = {"ERROR": "check_errors", "WARNING": "check_warnings", "INFO": "check_info"}
+button_map = {"ERROR": "check_errors",
+              "WARNING": "check_warnings",
+              "INFO": "check_info"}
 
 class LogfileViewer(QtWidgets.QDialog):
     def __init__(self, parent=None):
-        
+
         super(LogfileViewer, self).__init__(parent)
-        
+
         self.ui = Ui_logfileDialog()
         self.ui.setupUi(self)
-        
+
         self.setWindowIcon(options.cfg.icon)
-        
+
         self.log_table = classes.LogTableModel(self)
         self.log_proxy = classes.LogProxyModel()
         self.log_proxy.setSourceModel(self.log_table)
@@ -37,10 +39,11 @@ class LogfileViewer(QtWidgets.QDialog):
         self.ui.log_table.setModel(self.log_proxy)
 
         for x in button_map:
-            getattr(self.ui, button_map[x]).setChecked(x in options.cfg.show_log_messages)
-            getattr(self.ui, button_map[x]).stateChanged.connect(self.changeFilters)
+            button = getattr(self.ui, button_map[x])
+            button.setChecked(x in options.cfg.show_log_messages)
+            button.stateChanged.connect(self.changeFilters)
 
-        #self.ui.log_table.horizontalHeader().setStretchLastSection(True)        
+        #self.ui.log_table.horizontalHeader().setStretchLastSection(True)
         #self.log_table.setVisible(False)
         self.ui.log_table.resizeColumnsToContents()
         #self.log_table.setVisible(True)
@@ -53,29 +56,30 @@ class LogfileViewer(QtWidgets.QDialog):
     def changeFilters(self):
         options.cfg.show_log_messages = []
         for filt, checkbox in button_map.items():
-            if getattr(self.ui, checkbox).isChecked():
+            button = getattr(self.ui, checkbox)
+            if button.isChecked():
                 options.cfg.show_log_messages.append(filt)
         self.log_proxy.updateFilter()
 
     def closeEvent(self, event):
         options.settings.setValue("logfile_size", self.size())
-        
+
     def keyPressEvent(self, e):
         if e.key() == QtCore.Qt.Key_Escape:
             self.accept()
-            
+
     @staticmethod
     def view(parent=None):
         dialog = LogfileViewer(parent)
         result = dialog.exec_()
         return None
-    
-            
+
+
 def main():
     app = QtWidgets.QApplication(sys.argv)
     viewer = LogfileViewer("")
     viewer.exec_()
-    
+
 if __name__ == "__main__":
     main()
-    
+
