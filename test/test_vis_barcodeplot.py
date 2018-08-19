76964efb5ace375ab08def6c4e75e158c5a653d2
gkunter <coquery@users.strandwall.de>
2018-08-19 09:17:42 +0200

    Update tests

--- test/test_vis_barcodeplot.py
+++ test/test_vis_barcodeplot.py
@@ -149,7 +149,7 @@ class TestBarcodePlotAxisArguments(TestBarcodePlotArguments):
                                               x=category, y=None, z=None,
                                               levels_x=levels, levels_y=None)
         target = {"xticklabels": levels,
-                  "xticks": [0.5, 1.5],
+                  "xticks": pd.np.array([0.5, 1.5]),
                   "xlim": (0, 2)}
 
         self.assertDictEqual(ax_args, target)
@@ -166,7 +166,7 @@ class TestBarcodePlotAxisArguments(TestBarcodePlotArguments):
                                               x=None, y=category, z=None,
                                               levels_x=None, levels_y=levels)
         target = {"yticklabels": levels[::-1],
-                  "yticks": [0.5, 1.5],
+                  "yticks": pd.np.array([0.5, 1.5]),
                   "ylim": (0, 2)}
 
         self.assertDictEqual(ax_args, target)
@@ -192,10 +192,69 @@ class TestHeatbarPlotArguments(CoqTestCase):
         self.df = pd.DataFrame({self.NUM_COLUMN: N, "X": X})
         self.vis = HeatbarPlot(None, None)
 
-    def test_horizontal_no_subgroup(self):
 
-        # Bins:      00000111112222233333444445555566666777778888899999
-        # Counts:        2    2    1    0    3    0    1    1    1    3
+    def test_increment_bins(self):
+        """
+        Test the increment_bins() method, which is responsible for
+        distributing a token presence across one or two bins depending on
+        whether the location of the token relative to the bin margins.
+
+        The following table summarizes the expected behavior for different
+        values of `p`, the relative position of a token id to the beginning
+        of a bin. A value of p=0.0 indicates that the token is located at the
+        left edge of the target bin, and a value of p=1.0 indicates that the
+        token is located at the right edge of the target bin.
+
+        The columns B(-1), B(0), B(+1) give the increments for the target bin
+        B(0), the bin preceding the target bin B(-1) and the bin folloowing
+        the target bin B(+1).
+
+        The largest increment is added when p=0.5, i.e. when the token is
+        located at the center of the bin. In this case, the whole increment is
+        given to the target bin, and no spread to a neighboring bin takes
+        place. If the token is located at the edge of a bin (p=0.0), the
+        increment is spread equally across the preceding bin and the target
+        bin.
+
+        ------------------------
+          P  B(-1)   B(0)  B(+1)
+        ------------------------
+        0.0   +0.5   +0.5
+        0.1   +0.4   +0.6
+        0.2   +0.3   +0.7
+        0.3   +0.2   +0.8
+        0.4   +0.1   +0.9
+        0.5          +1.0
+        0.6          +0.9  +0.1
+        0.7          +0.8  +0.2
+        0.8          +0.7  +0.3
+        0.9          +0.6  +0.4
+        ------------------------
+
+        """
+
+        increments = [[0.5, 0.5, 0.0],
+                      [0.4, 0.6, 0.0],
+                      [0.3, 0.7, 0.0],
+                      [0.2, 0.8, 0.0],
+                      [0.1, 0.9, 0.0],
+                      [0.0, 1.0, 0.0],
+                      [0.0, 0.9, 0.1],
+                      [0.0, 0.8, 0.2],
+                      [0.0, 0.7, 0.3],
+                      [0.0, 0.6, 0.4]]
+
+        for i, p in enumerate(pd.np.arange(0, 1, 0.1)):
+            bins = [0, 0, 0]
+            pd.np.testing.assert_array_almost_equal(
+                increments[i],
+                self.vis.increment_bins(bins, 5 + p * 5, 5))
+
+    def test_horizontal_no_subgroup(self):
+        binned = pd.np.zeros(10)
+
+        for i in self.df[self.NUM_COLUMN]:
+            binned = self.vis.increment_bins(binned, i, 5)
 
         args = self.vis.prepare_im_arguments(
             data=self.df,
@@ -205,16 +264,17 @@ class TestHeatbarPlotArguments(CoqTestCase):
         target = {"aspect": "auto",
                   "interpolation": "gaussian",
                   "extent": (0, 49, None, None),
-                  "M": [[2, 2, 1, 0, 3, 0, 1, 1, 1, 3]]}
+                  "M": pd.np.array([binned])}
 
         self.assertDictEqual(args, target)
 
     def test_vertical_no_subgroup(self):
-
-        # Bins:      00000111112222233333444445555566666777778888899999
-        # Counts:        2    2    1    0    3    0    1    1    1    3
-
         self.vis.force_horizontal = False
+
+        binned = pd.np.zeros(10)
+
+        for i in self.df[self.NUM_COLUMN]:
+            binned = self.vis.increment_bins(binned, i, 5)
 
         args = self.vis.prepare_im_arguments(
             data=self.df,
@@ -224,15 +284,17 @@ class TestHeatbarPlotArguments(CoqTestCase):
         target = {"aspect": "auto",
                   "interpolation": "gaussian",
                   "extent": (None, None, 0, 49),
-                  "M": [[2, 2, 1, 0, 3, 0, 1, 1, 1, 3]]}
+                  "M": pd.np.array([binned])}
 
         self.assertDictEqual(args, target)
 
     def test_horizontal_subgroup(self):
+        M = pd.np.array(pd.np.zeros((2, 10)))
 
-        # Bins:      00000111112222233333444445555566666777778888899999
-        # A count:       2    2    1    0    0    0    1    1    1    0
-        # B count:       0    0    0    0    3    0    0    0    0    3
+        for ix in self.df.index:
+            row = 0 if self.df.iloc[ix]["X"] == "A" else 1
+            val = self.df.iloc[ix][self.NUM_COLUMN]
+            M[row] = self.vis.increment_bins(M[row], val, 5)
 
         args = self.vis.prepare_im_arguments(
             data=self.df,
@@ -242,8 +304,7 @@ class TestHeatbarPlotArguments(CoqTestCase):
         target = {"aspect": "auto",
                   "interpolation": "gaussian",
                   "extent": (0, 49, None, None),
-                  "M": [[2, 2, 1, 0, 0, 0, 1, 1, 1, 0],
-                        [0, 0, 0, 0, 3, 0, 0, 0, 0, 3]]}
+                  "M": M}
 
         self.assertDictEqual(args, target)
 
@@ -252,6 +313,13 @@ class TestHeatbarPlotArguments(CoqTestCase):
         Test vertical subgroups. Group counts are the same as in
         test_horizontal_subgroup(), but the `extent` argument differs.
         """
+        M = pd.np.array(pd.np.zeros((2, 10)))
+
+        for ix in self.df.index:
+            row = 0 if self.df.iloc[ix]["X"] == "A" else 1
+            val = self.df.iloc[ix][self.NUM_COLUMN]
+            M[row] = self.vis.increment_bins(M[row], val, 5)
+
         args = self.vis.prepare_im_arguments(
             data=self.df,
             x="X", y=None, z=None, levels_x=["A", "B"], levels_y=None,
@@ -260,15 +328,14 @@ class TestHeatbarPlotArguments(CoqTestCase):
         target = {"aspect": "auto",
                   "interpolation": "gaussian",
                   "extent": (None, None, 0, 49, ),
-                  "M": [[2, 2, 1, 0, 0, 0, 1, 1, 1, 0],
-                        [0, 0, 0, 0, 3, 0, 0, 0, 0, 3]]}
+                  "M": M}
 
         self.assertDictEqual(args, target)
 
 
 provided_tests = (
-    #TestBarcodePlotArguments,
-    #TestBarcodePlotAxisArguments,
+    TestBarcodePlotArguments,
+    TestBarcodePlotAxisArguments,
     TestHeatbarPlotArguments,
     )
 
