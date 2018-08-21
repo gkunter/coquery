c2cb25a3b77b5c21984254d4d8721fb096a11847
gkunter <github@strandwall.de>
2018-08-21 19:57:52 +0200

    Use z scaling as the normalization method for numeric color variables

--- coquery/visualizer/heatmap.py
+++ coquery/visualizer/heatmap.py
@@ -15,6 +15,7 @@ import pandas as pd
 
 from coquery.visualizer import visualizer as vis
 from coquery.gui.pyqt_compat import QtWidgets, QtCore, tr
+from coquery import options
 
 
 def _annotate_heatmap(self, ax, mesh):
@@ -68,11 +69,86 @@ class Heatmap(vis.Visualizer):
     NORMALIZATIONS = [NORM_BY_ROW, NORM_BY_COLUMN, NORM_ACROSS, NORM_NONE]
     normalization = NORM_NONE
 
+    fill = None
+
+    def get_custom_widgets(self, *args, **kwargs):
+        text_normalization = tr("HeatMap", "Normalization", None)
+        text_centering = tr("HeatMap", "Set center value:", None)
+        text_fill = tr("HeatMap", "Supply missing value:", None)
+
+        self.label_normalization = QtWidgets.QLabel(text_normalization)
+        self.combo_normalize = QtWidgets.QComboBox()
+        self.combo_normalize.addItems(
+            [tr("HeatMap", text, None) for text in self.NORMALIZATIONS])
+        self.combo_normalize.setCurrentIndex(
+            self.NORMALIZATIONS.index(self.normalization))
+
+        self.check_centering = QtWidgets.QCheckBox(text_centering)
+        self.spin_centering = QtWidgets.QDoubleSpinBox()
+
+        if self.center is None:
+            self.check_centering.setCheckState(QtCore.Qt.Unchecked)
+        else:
+            self.check_centering.setCheckState(QtCore.Qt.Checked)
+            self.spin_centering.setValue(self.center)
+        self.spin_centering.setDisabled(self.center is None)
+
+        self.check_fill = QtWidgets.QCheckBox(text_fill)
+        self.spin_fill = QtWidgets.QDoubleSpinBox()
+
+        if self.fill is None:
+            self.check_fill.setCheckState(QtCore.Qt.Unchecked)
+        else:
+            self.check_fill.setCheckState(QtCore.Qt.Checked)
+            self.spin_fill.setValue(self.fill)
+        self.spin_fill.setDisabled(self.fill is None)
+
+        norm_layout = QtWidgets.QHBoxLayout()
+        norm_layout.addWidget(self.label_normalization)
+        norm_layout.addWidget(self.combo_normalize)
+        norm_layout.setStretch(0, 1)
+        norm_layout.setStretch(1, 0)
+
+        center_layout = QtWidgets.QHBoxLayout()
+        center_layout.addWidget(self.check_centering)
+        center_layout.addWidget(self.spin_centering)
+
+        fill_layout = QtWidgets.QHBoxLayout()
+        fill_layout.addWidget(self.check_fill)
+        fill_layout.addWidget(self.spin_fill)
+
+        return ([fill_layout, norm_layout, center_layout],
+                [self.combo_normalize.currentIndexChanged,
+                 self.check_centering.stateChanged,
+                 self.spin_centering.valueChanged,
+                 self.check_fill.stateChanged,
+                 self.spin_fill.valueChanged],
+                [self.check_centering.stateChanged,
+                 self.check_fill.stateChanged])
+
+    def update_widgets(self):
+        self.spin_centering.setEnabled(self.check_centering.isChecked())
+        self.spin_fill.setEnabled(self.check_fill.isChecked())
+
+    def update_values(self):
+        if self.check_centering.isChecked():
+            self.center = float(self.spin_centering.value())
+        else:
+            self.center = None
+        if self.check_fill.isChecked():
+            self.fill = float(self.spin_fill.value())
+        else:
+            self.fill = None
+        ix = int(self.combo_normalize.currentIndex())
+        self.normalization = self.NORMALIZATIONS[ix]
+
     def plot_facet(self, data, color, **kwargs):
         def get_crosstab(data, row_fact, col_fact, row_names, col_names):
             ct = pd.crosstab(data[row_fact], data[col_fact])
-            ct = ct.reindex_axis(row_names, axis=0).fillna(0)
-            ct = ct.reindex_axis(col_names, axis=1).fillna(0)
+            ct = ct.reindex_axis(row_names, axis=0)
+            ct = ct.reindex_axis(col_names, axis=1)
+            if self.fill is not None:
+                ct = ct.fillna(self.fill)
             return ct
 
         x = kwargs.get("x")
@@ -147,22 +223,53 @@ class Heatmap(vis.Visualizer):
             self._xlab = "Frequency"
 
         fmt = ".1%"
-        if self.normalization == self.NORM_BY_ROW:
-            ct = ct.apply(lambda col: col / sum(col), axis="columns")
-        elif self.normalization == self.NORM_BY_COLUMN:
-            ct = ct.apply(lambda row: row / sum(row), axis="rows")
-        elif self.normalization == self.NORM_ACROSS:
-            ct = pd.DataFrame(ct.values / ct.values.sum(),
-                              columns=ct.columns, index=ct.index)
-        else:
-            fmt = "g"
 
-        if self.center is None or self.normalization == self.NORM_NONE:
-            center = None
-        else:
-            center = self.center / 100
+        ct = ct.fillna(self.fill) if self.fill is not None else ct
 
-        sns.heatmap(ct.fillna(0),
+        if self.z:
+            fmt = "+.{}f".format(options.cfg.digits)
+            if self.normalization == self.NORM_BY_ROW:
+                means = ct.mean(axis="columns", skipna=True)
+                std = ct.std(axis="columns", skipna=True)
+                ct = (ct.subtract(means, axis="rows")
+                        .divide(std, axis="rows"))
+            elif self.normalization == self.NORM_BY_COLUMN:
+                means = ct.mean(axis="rows", skipna=True)
+                std = ct.std(axis="rows", skipna=True)
+                ct = (ct.subtract(means, axis="columns")
+                        .divide(std, axis="columns"))
+            elif self.normalization == self.NORM_ACROSS:
+                ct = (ct - pd.np.nanmean(ct.values)) / pd.np.nanstd(ct.values)
+            else:
+                fmt = ".{}f".format(options.cfg.digits)
+
+            if self.center is None:
+                center = None
+            else:
+                center = self.center
+        else:
+            if self.normalization == self.NORM_BY_ROW:
+                ct = ct.divide(ct.sum(axis="columns", skipna=True),
+                               axis="rows", skipna=True)
+            elif self.normalization == self.NORM_BY_COLUMN:
+                ct = ct.divide(ct.sum(axis="rows", skipna=True),
+                               axis="columns", skipna=True)
+            elif self.normalization == self.NORM_ACROSS:
+                ct = pd.DataFrame(ct.values / ct.values.sum(),
+                                  columns=ct.columns, index=ct.index)
+            else:
+                fmt = "g"
+
+
+            if self.center is None:
+                center = None
+            else:
+                if self.normalization == self.NORM_NONE:
+                    center = self.center
+                else:
+                    center = self.center / 100
+
+        sns.heatmap(ct,
                     robust=True,
                     annot=True,
                     cbar=False,
@@ -170,56 +277,6 @@ class Heatmap(vis.Visualizer):
                     cmap=cmap,
                     fmt=fmt,
                     linewidths=1)
-
-    def get_custom_widgets(self, *args, **kwargs):
-        text_normalization = tr("HeatMap", "Normalization", None)
-        text_centering = tr("HeatMap", "Use centering value", None)
-
-        self.label_normalization = QtWidgets.QLabel(text_normalization)
-        self.combo_normalize = QtWidgets.QComboBox()
-        self.combo_normalize.addItems(
-            [tr("HeatMap", text, None) for text in self.NORMALIZATIONS])
-        self.combo_normalize.setCurrentIndex(
-            self.NORMALIZATIONS.index(self.normalization))
-
-        self.check_centering = QtWidgets.QCheckBox(text_centering)
-        self.spin_centering = QtWidgets.QDoubleSpinBox()
-
-        if self.center is None:
-            self.check_centering.setCheckState(QtCore.Qt.Unchecked)
-        else:
-            self.check_centering.setCheckState(QtCore.Qt.Checked)
-            self.spin_centering.setValue(self.center)
-        self.spin_centering.setDisabled(self.center is None)
-
-        norm_layout = QtWidgets.QHBoxLayout()
-        norm_layout.addWidget(self.label_normalization)
-        norm_layout.addWidget(self.combo_normalize)
-        norm_layout.setStretch(0, 1)
-        norm_layout.setStretch(1, 0)
-
-        center_layout = QtWidgets.QHBoxLayout()
-        center_layout.addWidget(self.check_centering)
-        center_layout.addWidget(self.spin_centering)
-        center_layout.setStretch(0, 1)
-        center_layout.setStretch(1, 0)
-
-        return ([norm_layout, center_layout],
-                [self.combo_normalize.currentIndexChanged,
-                 self.check_centering.stateChanged,
-                 self.spin_centering.valueChanged],
-                [self.check_centering.stateChanged])
-
-    def update_widgets(self):
-        self.spin_centering.setEnabled(self.check_centering.isChecked())
-
-    def update_values(self):
-        if self.check_centering.isChecked():
-            self.center = float(self.spin_centering.value())
-        else:
-            self.center = None
-        ix = int(self.combo_normalize.currentIndex())
-        self.normalization = self.NORMALIZATIONS[ix]
 
     @staticmethod
     def validate_data(data_x, data_y, data_z, df, session):
