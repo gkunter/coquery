e3305bcb40f1dc7d7425ff0501fdf46b59fd3055
gkunter <github@strandwall.de>
2018-08-29 18:09:32 +0200

    Revise bubble placement algorithm

--- coquery/visualizer/bubbleplot.py
+++ coquery/visualizer/bubbleplot.py
@@ -12,6 +12,7 @@ with Coquery. If not, see <http://www.gnu.org/licenses/>.
 from __future__ import unicode_literals
 from __future__ import division
 import math
+import time
 
 from coquery.visualizer import visualizer as vis
 import seaborn as sns
@@ -106,17 +107,26 @@ class Bubbleplot(vis.Visualizer):
             try:
                 return self._angle_cache[_key]
             except KeyError:
+                assert (b != 0.0,
+                        "Zero length detected (b={}, c={})".format(b, c))
+                assert (c != 0.0,
+                        "Zero length detected (b={}, c={})".format(b, c))
                 x = (b ** 2 + c ** 2 - a ** 2) / (2 * b * c)
-                if x > 2:
+                if x > 1 or x < 0:
+                    #print("\t\tget_angle({:.2}, {:.2}, {:.2}) = NONE ({:.2})".format(
+                        #a, b, c, x))
                     raise ValueError("Illegal angle")
                 # Allow angles > 180 degrees:
                 if x > 1:
                     self._angle_cache[_key] = math.acos(x - 1)
                 else:
                     self._angle_cache[_key] = math.acos(x)
-            return self._angle_cache[_key]
+            val = self._angle_cache[_key]
+            #print("\t\tget_angle({:.2}, {:.2}, {:.2}) = {:.2} ({:.2})".format(
+                #a, b, c, val, x))
+            return val
 
-        def get_position(i, a, n):
+        def get_position(r, a, n):
             """
             Return the position of the next bubble I.
 
@@ -138,11 +148,15 @@ class Bubbleplot(vis.Visualizer):
 
             Parameters
             ----------
-            i, a, n : int
-                The index of the next bubble (i), the anchor bubble (a),
-                and the neighboring bubble (n).
+            r : int
+                The radius of the next bubble (i)
+
+            a, n : int
+                The index of the anchor bubble (a) and the
+                neighboring bubble (n).
 
             """
+            #print("\tget_position(r={:.2}, a={}, n={})".format(r, a, n))
             A = self.pos[a]
             N = self.pos[n]
 
@@ -154,8 +168,8 @@ class Bubbleplot(vis.Visualizer):
             #       c
 
             # get side lengths:
-            l_a = radii[n] + radii[i]
-            l_b = radii[a] + radii[i]
+            l_a = radii[n] + r
+            l_b = radii[a] + r
 
             # l_c is actually the distance between the centers of the
             # anchor and the neighbor circles:
@@ -172,17 +186,18 @@ class Bubbleplot(vis.Visualizer):
 
             theta = get_angle(l_a, l_b, l_c) + start_angle
 
-            return (A[0] + l_b * math.cos(theta),
-                    A[1] + l_b * math.sin(theta))
+            val = (A[0] + l_b * math.cos(theta), A[1] + l_b * math.sin(theta))
+            #print("\tPosition = {:3.2f}, {:3.2f}".format(*val))
+            return val
 
-        def intersecting(p, q):
-            x_p, y_p = self.pos[p]
-            x_q, y_q = self.pos[q]
+        def intersecting(tup1, tup2):
+            ((x1, y1), r1) = tup1
+            ((x2, y2), r2) = tup2
 
-            lower = (radii[p] - radii[q]) ** 2
-            upper = (radii[p] + radii[q]) ** 2
+            lower = (r1 - r2) ** 2
+            upper = (r1 + r2) ** 2
 
-            val = (x_p - x_q) ** 2 + (y_p - y_q) ** 2
+            val = (x1 - x2) ** 2 + (y1 - y2) ** 2
 
             return lower <= val < upper - 0.01
 
@@ -190,58 +205,65 @@ class Bubbleplot(vis.Visualizer):
             row = df_freq.iloc[k]
             freq = row["Freq"]
             rad = radii[k]
-            label = " | ".join(list(row[[z for z in [self.x, self.y] if z]]))
 
-            #c = self._col_dict[row[self._groupby[-1]]]
-            c = (0.3, 0.6, 0.9)
+            group = row[[cat for cat in [self.x, self.y] if cat]].values
+            label = " | ".join(group)
+            #print("draw_circle({}, {}, {}, {:.3})".format(
+                #k, label, int(rad ** 2 * math.pi), rad))
+            c = self.palette[k]
             x, y = self.pos[k]
 
-            circ = plt.Circle((x, y), max(0, rad - 0.05), color=c)
+            circ = plt.Circle((x, y), rad, color=c)
             ax.add_artist(circ)
             circ.set_edgecolor("black")
 
-            try:
-                #font = self.options.get("font_x_axis",
-                                        #self.options["figure_font"])
-                ax.text(x, y,
-                        "{}: {}".format(label.replace(" | ", "\n"), freq),
-                        ha="center",
-                        va="center")
-                        #family=font.family(),
-                        #fontsize=font.pointSize())
-            except Exception as e:
-                print(e)
-                raise
+            #font = self.options.get("font_x_axis",
+                                    #self.options["figure_font"])
+            ax.text(x, y,
+                    #"{}: {}".format(label.replace(" | ", "\n"), freq),
+                    str(k),
+                    ha="center",
+                    va="center")
+                    #family=font.family(),
+                    #fontsize=font.pointSize())
+
             self.max_x = max(self.max_x, x + rad)
             self.max_y = max(self.max_y, y + rad)
             self.min_x = min(self.min_x, x - rad)
             self.min_y = min(self.min_y, y - rad)
             self.circles.append(((x, y), rad, "{}: {}".format(label, freq)))
 
-        def get_intersections(i, lower, upper):
-            for x in range(lower, upper):
-                if intersecting(i, x):
-                    return True
-            return False
+            return group
 
-        group_columns = [x for x in data.columns
-                         if not x.startswith("coquery_invisible")]
-        df_freq = data.fillna("").groupby(group_columns).count().reset_index()
-        columns = list(df_freq.columns)
-        columns[-1] = "Freq"
-        df_freq.columns = columns
-        #self.set_palette_values(len(set(df_freq[self._groupby[-1]])))
+        def get_intersections(circ, lower, upper):
+            for x, trial in enumerate(zip(self.pos[lower:upper],
+                                       radii[lower:upper])):
+                if intersecting(circ, trial):
+                    #print("\tget_intersections(from={}, to={}) = {}".format(
+                        #lower, upper, x))
+                    raise ValueError
+                    return x
+
+
+            #print("\tget_intersections(from={}, to={}): None".format(lower, upper))
+            return None
+
+
+        grouping = [cat for cat in [self.x, self.y] if cat]
+
+        aggregator = vis.Aggregator()
+        aggregator.add("coquery_invisible_corpus_id", "count", "Freq")
+        df_freq = aggregator.process(data, grouping)
+        self.palette = sns.color_palette("Purples", len(df_freq))
+
         if (self.x and self.y):
             df_freq = df_freq.sort_values(by=[self.y, "Freq"],
-                                          ascending=[False, False])
-            #self._col_dict = dict(
-                #zip(self._levels[-1],
-                    #self.options["color_palette_values"]))
+                                          ascending=[True, False])
         else:
             df_freq = df_freq.sort_values(by="Freq", ascending=False)
-            #self._col_dict = dict(
-                #zip(df_freq[self._groupby[-1]],
-                    #self.options["color_palette_values"]))
+
+        #df_freq = df_freq.sort_values(grouping + ["Freq"],
+                                      #ascending=[True] * len(grouping) + [False])
         radii = list(map(r, df_freq["Freq"].values))
         ax = plt.gca()
 
@@ -251,43 +273,67 @@ class Bubbleplot(vis.Visualizer):
         self.min_y = 0
         ax.set_aspect(1)
 
-        a = 0
+        # Algorithm:
+        #
+        # * place anchor bubble A at origin (0, 0)
+        # * add neighbor bubble N to the right of A so that they touch
+        # * For each additional bubble:
+        #   * Repeat until a position P is found:
+        #     * calculate the position P where I touches A and N
+        #     * check if I at P intersects with any preceding bubble
+        #       (ignoring any bubble earlier than A)
+        #       * YES: set A to the next bubble after current A
+        #       * NO: draw bubble at P
 
+        a = 0
         completed = 0
 
         self.pos = [(0, 0)] * len(df_freq)
         draw_circle(0)
 
         if len(df_freq) > 1:
-            self.pos[1] = (self.pos[0][0] + radii[0] + radii[1],
-                           self.pos[0][1])
-            draw_circle(1)
-
             n = 1
+            self.pos[1] = (self.pos[0][0] + radii[0] + radii[n],
+                           self.pos[0][1])
+            last_group = draw_circle(1)
+
             for i in range(n + 1, len(df_freq)):
-                self.pos[i] = get_position(i, a, n)
-                while get_intersections(i, completed, n) and a < i:
-                    a = a + 1
-                    # Intersections not only trigger an anchor bubble change,
-                    # they also indicate that a closed ring of bubbles has
-                    # been completed. All bubbles within the closed ring do
-                    # not have to be considered for collision detections, so
-                    # keeping track of the completed bubbles speeds up the
-                    # algorithm:
+                r = radii[i]
+                group = df_freq.iloc[i][grouping].values
+                #print("----", i)
 
-                    completed = a
-
-                    # If there is no position at which the next bubble can
-                    # be placed so that it is tangent to both the neighbor
-                    # and the anchor, get_position() raises a ValueError
-                    # exception.
+                while True:
                     try:
-                        self.pos[i] = get_position(i, a, n)
-                    except ValueError:
-                        continue
+                        pos = get_position(r, a, n)
+                        intersect = get_intersections((pos, r), 0, i - 1)
 
+                    except (ValueError, ZeroDivisionError):
+                        if a < i - 1:
+                            a = a + 1
+                            #print("\n\tOVERLAP, SET NEW ANCHOR={}".format(a))
+                        else:
+                            a = n
+                            n = n - 1
+                            while True:
+                                try:
+                                    new_pos = get_position(r, a, n)
+                                    get_intersections((new_pos, r), 0, i - 1)
+                                except (ValueError, ZeroDivisionError):
+                                    if n == 0:
+                                        a = a - 1
+                                        n = a - 1
+                                    else:
+                                        n = n - 1
+
+                                else:
+                                    #print("\n\tOUT OF CHOICES: NEW NEIGBOR={}, RESET ANCHOR={}".format(n, a))
+                                    break
+                    else:
+                        break
+                #completed = max(a, completed)
+
+                self.pos[i] = pos
                 draw_circle(i)
-                # The current bubble becomes the new neighbor bubble:
                 n = i
 
         ax.set_ylim(self.min_y * 1.01, self.max_y * 1.01)
