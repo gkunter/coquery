e66d3d2575c70a182bcf77c3c08d8e3ce62e50d5
gkunter <github@strandwall.de>
2021-10-14 17:00:23 +0200

    Rename BURSC installer

--- coquery/installer/coq_install_bursc.py
+++ coquery/installer/coq_install_bursc.py
@@ -5,13 +5,15 @@ import csv
 import os
 import collections, difflib, string
 
-from coquery import corpusbuilder
-
+from coquery.corpusbuilder import BaseCorpusBuilder
+from coquery.tables import (
+    Identifier, Column, Link,
+    varchar, enum, smallint, mediumint, real)
 # The class corpus_code contains the Python source code that will be
 # embedded into the corpus library. It provides the Python code that will
 # override the default class methods of CorpusClass by methods that are
-# tailored for the Buckeye corpus.
-#
+# tailored for the BURSC corpus.
+
 class corpus_code():
     def sql_string_get_time_info(self, token_id):
         return "SELECT {} FROM {} WHERE {} = {}".format(
@@ -23,148 +25,85 @@ class corpus_code():
     def get_time_info_header(self):
         return ["Time"]
 
-class BURSCBuilder(corpusbuilder.BaseCorpusBuilder):
+class BuilderClass(BaseCorpusBuilder):
+    file_filter = "*.txt"
+
+    word_table = "word"
+    word_id = "WordId"
+    word_label = "Word"
+    word_pos = "Pos"
+    word_columns = [
+        Identifier(word_id, mediumint(7, unsigned=True, not_null=True),
+        Column(word_label, varchar(43, not_null=True),
+        Column(word_pos, varchar(24, not_null=True)]
+
+    file_table = "file"
+    file_id = "FileId"
+    file_name = "Filename"
+    file_path = "Path"
+    file_columns = [
+        Identifier(file_id, "SMALLINT(3) UNSIGNED NOT NULL"),
+        Column(file_name, "CHAR(12) NOT NULL"),
+        Column(file_path, "VARCHAR(1024) NOT NULL")]
+
+    source_table = "Sources"
+    source_id = "SourceId"
+    source_story = "Story"
+    source_storytype = "Storytype"
+    source_paragraph = "Paragraph"
+    source_noisy = "IsNoisy"
+    source_columns = [
+        Identifier(source_id, "SMALLINT(3) UNSIGNED NOT NULL"),
+        Column(source_story, "CHAR(3) NOT NULL"),
+        Column(source_storytype =
+        Column(file_path, "TINYTEXT NOT NULL")]
+
+
+    speaker_table = "Speakers"
+    speaker_id = "SpeakerId"
+    speaker_gender = "Gender"
+    speaker_type = "Type"
+
+    segment_table = "Segments"
+    segment_id = "SegmentId"
+    segment_origin_id = "FileId"
+    segment_label = "Segment"
+    segment_starttime = "SegStart"
+    segment_endtime = "SegEnd"
+
+    corpus_table = "corpus"
+    corpus_id = "TokenId"
+    corpus_word_id = "WordId"
+    corpus_source_id = "SourceId"
+    corpus_file_id = "FileId"
+    corpus_speaker_id = "SpeakerId"
+    corpus_starttime = "Start"
+    corpus_endtime = "End"
+    corpus_columns = [
+        Identifier(corpus_id, "INT(9) UNSIGNED NOT NULL"),
+        Column(corpus_starttime, "REAL(11,6) NOT NULL"),
+        Column(corpus_endtime, "REAL(11,6) NOT NULL"),
+        Link(corpus_word_id, word_table),
+        Link(corpus_source_id, source_table),
+        Link(corpus_file_id, file_table),
+        Link(corpus_speaker_id, speaker_table)]
+
     def __init__(self):
        # all corpus builders have to call the inherited __init__ function:
-        super(BURSCBuilder, self).__init__()
+        super(BuilderClass, self).__init__()
         
         # Read only .txt files from the corpus path:
-        self.file_filter = "*.txt"
         
         # specify which features are provided by this corpus and lexicon:
         self.lexicon_features = ["LEX_WORDID", "LEX_LEMMA", "LEX_ORTH", "LEX_PHON", "LEX_POS"]
         self.corpus_features = ["CORP_CONTEXT", "CORP_FILENAME", "CORP_STATISTICS", "CORP_TIMING"]
         self.documentation_url = "https://catalog.ldc.upenn.edu/LDC96S36"
-        
-        # add table descriptions for the tables used in this database.
-        #
-        # A table description is a dictionary with at least a 'CREATE' key
-        # which takes a list of strings as its value. Each of these strings
-        # represents a MySQL instruction that is used to create the table.
-        # Typically, this instruction is a column specification, but you can
-        # also add other table options such as the primary key for this 
-        # table.
-        #
-        # Additionaly, the table description can have an 'INDEX' key which
-        # takes a list of tuples as its value. Each tuple has three 
-        # elements. The first element is a list of strings containing the
-        # column names that are to be indexed. The second element is an
-        # integer value specifying the index length for columns of Text
-        # types. The third element specifies the index type (e.g. 'HASH' or
-        # 'BTREE'). Note that not all MySQL storage engines support all 
-        # index types.
-        
-        # Add the main corpus table. Each row in this table represents a 
-        # token in the corpus. It has the following columns:
-        # 
-        # TokenId
-        # An int value containing the unique identifier of the token
-        #
-        # WordId
-        # An int value containing the unique identifier of the lexicon
-        # entry associated with this token.
-        #
-        # FileId
-        # An int value containing the unique identifier of the data file 
-        # that contains this token.
-        
-        self.corpus_table = "corpus"
-        self.corpus_id = "TokenId"
-        self.corpus_word_id = "WordId"
-        self.corpus_source_id = "FileId"
-        self.corpus_time = "Time"
 
-        self.add_table_description(self.corpus_table, self.corpus_id,
-            {"CREATE": [
-                "`{}` BIGINT(20) UNSIGNED NOT NULL".format(self.corpus_id),
-                "`{}` MEDIUMINT(7) UNSIGNED NOT NULL".format(self.corpus_word_id),
-                "`{}` MEDIUMINT(7) UNSIGNED NOT NULL".format(self.corpus_source_id),
-                "`{}` DECIMAL(11,6) UNSIGNED".format(self.corpus_time)],
-            "INDEX": [
-                ([self.corpus_word_id], 0, "HASH"),
-                ([self.corpus_source_id], 0, "HASH")]})
-        
-        # Add the main lexicon table. Each row in this table represents a
-        # word-form that occurs in the corpus. It has the following columns:
-        #
-        # WordId
-        # An int value containing the unique identifier of this word-form.
-        #
-        # Text
-        # A text value containing the orthographic representation of this
-        # word-form.
-        #
-        # LemmaId
-        # An int value containing the unique identifier of the lemma that
-        # is associated with this word-form.
-        # 
-        # Pos
-        # A text value containing the part-of-speech label of this 
-        # word-form.
-        #
-        # Transcript
-        # A text value containing the phonological transcription of this
-        # word-form.
+        self.add_time_feature(self.corpus_starttime)
+        self.add_time_feature(self.corpus_endtime)
 
-        self.word_table = "word"
-        self.word_id = "WordId"
-        self.word_label = "Text"
-        self.word_lemma_id = "LemmaId"
-        self.word_pos = "Pos"
-        self.word_transcript = "Transcript"
-        
-        self.add_table_description(self.word_table, self.word_id,
-            {"CREATE": [
-                "`{}` SMALLINT(5) UNSIGNED NOT NULL".format(self.word_id),
-                "`{}` TEXT NOT NULL".format(self.word_label),
-                "`{}` TEXT NOT NULL".format(self.word_lemma_id),
-                "`{}` VARCHAR(10) NOT NULL".format(self.word_pos),
-                "`{}` TINYTEXT NOT NULL".format(self.word_transcript)],
-            "INDEX": [
-                ([self.word_lemma_id], 0, "BTREE"),
-                ([self.word_pos], 0, "BTREE"),
-                ([self.word_label], 0, "BTREE"),
-                ([self.word_transcript], 0, "BTREE")]})
+        self.add_annotation(self.segment_table, self.corpus_table)
 
-        # Add the file table. Each row in this table represents a data file
-        # that has been incorporated into the corpus. Each token from the
-        # corpus table is linked to exactly one file from this table, and
-        # more than one token may be linked to each file in this table.
-        # The table contains the following columns:
-        #
-        # FileId
-        # An int value containing the unique identifier of this file.
-        # 
-        # Path
-        # A text value containing the path that points to this data file.
-        
-        self.file_table = "file"
-        self.file_id = "FileId"
-        self.file_label = "Path"
-
-        self.add_table_description(self.file_table, self.file_id,
-            {"CREATE": [
-                "`{}` SMALLINT(3) UNSIGNED NOT NULL".format(self.file_id),
-                "`{}` TINYTEXT NOT NULL".format(self.file_label)]})
-
-        # Any corpus that provides either CORP_CONTEXT, CORP_SOURCE or
-        # CORP_FILENAME also needs to specify a source table. Each row in
-        # this source table represents a corpus source, and it has to 
-        # contain at least the following column:
-        #
-        # SourceId
-        # An int value containing the unique identifier of this source.
-        # 
-        # Additional columns may also store further information such as 
-        # year or genre.
-        # 
-        # In this generic corpus, detailed information on the source texts
-        # is not available, so no separate source table is required. 
-        # Instead, the corpus uses the file table as the source table:
-        
-        self.source_table = "file"
-        self.source_id = "FileId"
-        
         # Specify that the corpus-specific code is contained in the dummy
         # class 'corpus_code' defined above:
         self._corpus_code = corpus_code
