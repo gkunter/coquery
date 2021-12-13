05dfcfb34b6542e05926e1e770fb25d8f2c5d209
gkunter <coquery@users.strandwall.de>
2021-12-13 01:13:15 +0100

    Make sure string functions are always available for string columns

--- coquery/gui/addfunction.py
+++ coquery/gui/addfunction.py
@@ -2,7 +2,7 @@
 """
 addfunction.py is part of Coquery.
 
-Copyright (c) 2016, 2017 Gero Kunter (gero.kunter@coquery.org)
+Copyright (c) 2016-2021 Gero Kunter (gero.kunter@coquery.org)
 
 Coquery is released under the terms of the GNU General Public License (v3).
 For details, see the file LICENSE that you should have received along
@@ -10,6 +10,8 @@ with Coquery. If not, see <http://www.gnu.org/licenses/>.
 """
 
 from __future__ import unicode_literals
+
+import pandas as pd
 
 from coquery import options
 from coquery import functions
@@ -361,14 +363,19 @@ class FunctionDialog(QtWidgets.QDialog):
                     self.ui.list_functions.setCurrentItem(item)
 
     def get_function_groups(self):
-        if all([x == object for x in self.df[self.columns].dtypes]):
+        # check if ALL selected columns are strings:
+        if all([pd.api.types.is_string_dtype(x)
+                for x in self.df[self.columns].dtypes]):
             function_groups = (functions.StringFunction,
                                functions.Comparison,
                                functions.BaseProportion,
                                functions.BaseFreq,
                                functions.LogicFunction,
                                functions.ConversionFunction)
-        elif all([x != object for x in self.df[self.columns].dtypes]):
+        # check if NONE of the selected columns is string:
+        elif all([not pd.api.types.is_string_dtype(x)
+                  for x in self.df[self.columns].dtypes]):
+        #elif all([x != object for x in self.df[self.columns].dtypes]):
             function_groups = (functions.OperatorFunction,
                                functions.StatisticalFunction,
                                functions.Comparison,
@@ -412,7 +419,7 @@ class FunctionDialog(QtWidgets.QDialog):
 
             fun_list = []
             for fun in all_functions:
-                if (issubclass(fun, fun_class) and fun != fun_class):
+                if issubclass(fun, fun_class) and fun != fun_class:
                     fun_list.append(fun)
 
             self.available_functions[i] = sorted(fun_list,
@@ -457,7 +464,7 @@ class FunctionDialog(QtWidgets.QDialog):
         if result == QtWidgets.QDialog.Accepted:
             func, values = self.get_function_values()
             columns = self.columns
-            return (func, columns, values)
+            return func, columns, values
         else:
             return None
 
