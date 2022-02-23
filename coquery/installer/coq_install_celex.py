b97d48f5d6b8afda621d4a9a85fdc0ca269669df
gkunter <github@coquery.org>
2022-02-23 21:10:59 +0100

    Remove obsolete Py2 compatiblity

--- coquery/installer/coq_install_celex.py
+++ coquery/installer/coq_install_celex.py
@@ -3,15 +3,12 @@
 """
 coq_install_celex.py is part of Coquery.
 
-Copyright (c) 2016-2018 Gero Kunter (gero.kunter@coquery.org)
+Copyright (c) 2016-2022 Gero Kunter (gero.kunter@coquery.org)
 
 Coquery is released under the terms of the GNU General Public License (v3).
 For details, see the file LICENSE that you should have received along
 with Coquery. If not, see <http://www.gnu.org/licenses/>.
 """
-from __future__ import unicode_literals
-from __future__ import print_function
-
 import unicodedata
 import os
 
