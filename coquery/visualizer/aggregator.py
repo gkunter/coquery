6c66b77c46683bb6e2f3f01ac9d334b3fa8423b8
gkunter <coquery@users.strandwall.de>
2022-04-08 12:07:58 +0200

    Clean up minor style issues and ambiguities

--- coquery/visualizer/aggregator.py
+++ coquery/visualizer/aggregator.py
@@ -9,24 +9,23 @@ For details, see the file LICENSE that you should have received along
 with Coquery. If not, see <http://www.gnu.org/licenses/>.
 """
 
-import collections
+from collections import defaultdict
 
 from PyQt5 import QtCore
 
-import pandas as pd
 from scipy import stats as st
 
 
 class Aggregator(QtCore.QObject):
     def __init__(self, id_column=None):
         super(Aggregator, self).__init__()
-        self._names_dict = None
-        self._aggs_dict = None
+        self._aggs_dict = defaultdict(list)
+        self._names_dict = defaultdict(list)
         self._id_column = id_column or "coquery_invisible_corpus_id"
 
     def reset(self):
-        self._aggs_dict = collections.defaultdict(list)
-        self._names_dict = collections.defaultdict(list)
+        self._aggs_dict = defaultdict(list)
+        self._names_dict = defaultdict(list)
 
     def add(self, column, fnc, name=None):
         if fnc == "mode":
@@ -49,7 +48,7 @@ class Aggregator(QtCore.QObject):
             self.add(self._id_column, "first")
 
         df = df.groupby(grouping).agg(self._aggs_dict)
-        agg_columns = pd.Index()
+        agg_columns = []
         for names in [self._names_dict[x] for x in df.columns.levels[0]]:
             agg_columns += names
         df.columns = agg_columns
