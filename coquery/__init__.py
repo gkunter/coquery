3d97e7d07c761a3c134c12638640c0a7b295587c
G Kunter <pavlovia@strandwall.de>
2026-06-09 18:55:52 +0200

    Version bump

--- coquery/__init__.py
+++ coquery/__init__.py
@@ -3,13 +3,15 @@
 """
 __init__.py is part of Coquery.
 
-Copyright (c) 2016, 2017 Gero Kunter (gero.kunter@coquery.org)
+Copyright (c) 2016-2026 Gero Kunter (gero.kunter@coquery.org)
 
 Coquery is released under the terms of the GNU General Public License (v3).
 For details, see the file LICENSE that you should have received along
 with Coquery. If not, see <http://www.gnu.org/licenses/>.
 """
 
-__version__ = "0.11.prev2"
+__version__ = "0.11"
 NAME = "Coquery"
-DATE = "2016-2021"
+DATE = "2016-2026"
+
+APP_NAME = f"{NAME} {__version__}"
